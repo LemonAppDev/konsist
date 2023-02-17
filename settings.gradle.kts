8e06ba6dffcbe50bd35d72a084b0825d0a92c0ac
Igor Wojda <igor.wojda@gmail.com>
2023-02-17 15:09:45 +0100

    Remove duplicated entries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,12 @@ dependencyResolutionManagement {
             plugin("spring-dependencyManagement", "io.spring.dependency-management").version("1.1.0")
             plugin("kotlin-plugin-spring", "org.jetbrains.kotlin.plugin.spring").version(kotlinVersion)
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").version(kotlinVersion)
+            plugin("spotless", "com.diffplug.spotless").version("6.15.0")
 
             library("kotlin-reflect", "org.jetbrains.kotlin", "kotlin-reflect").withoutVersion()
             library("kotlin-stdlib-jdk8", "org.jetbrains.kotlin", "kotlin-stdlib-jdk8").withoutVersion()
             library("spring-boot-starter-web", "org.springframework.boot", "spring-boot-starter-web").withoutVersion()
             library("jackson", "com.fasterxml.jackson.module", "jackson-module-kotlin").withoutVersion()
-            library("kotlin-reflect", "org.jetbrains.kotlin", "kotlin-reflect").withoutVersion()
-            library("kotlin-stdlib-jdk8", "org.jetbrains.kotlin", "kotlin-stdlib-jdk8").withoutVersion()
 
             library("spring-boot-starter-test", "org.springframework.boot", "spring-boot-starter-test").withoutVersion()
             library("junitJupiterEngine", "org.junit.jupiter", "junit-jupiter-engine").version("5.9.2")
