c0b7b014c260a52c97f5d1d41052e4983c984473
Igor Wojda <igor.wojda@gmail.com>
2023-02-17 14:12:10 +0100

    Add version catalog (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.2"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.spring") version "1.8.10"
+    alias(libs.plugins.springframework.boot)
+    alias(libs.plugins.spring.dependencyManagement)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.spring)
 }
 
 group = "com.example"
@@ -16,13 +16,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("org.amshove.kluent:kluent:1.72")
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.jackson)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlin.stdlib.jdk8)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.junitJupiterEngine)
+    testImplementation(libs.kluent)
 }
 
 tasks.withType<KotlinCompile> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
 rootProject.name = "mango"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            val kotlinVersion = "1.8.10"
+
+            plugin("springframework-boot", "org.springframework.boot").version("3.0.2")
+            plugin("spring-dependencyManagement", "io.spring.dependency-management").version("1.1.0")
+            plugin("kotlin-plugin-spring", "org.jetbrains.kotlin.plugin.spring").version(kotlinVersion)
+            plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").version(kotlinVersion)
+
+            library("kotlin-reflect", "org.jetbrains.kotlin", "kotlin-reflect").withoutVersion()
+            library("kotlin-stdlib-jdk8", "org.jetbrains.kotlin", "kotlin-stdlib-jdk8").withoutVersion()
+            library("spring-boot-starter-web", "org.springframework.boot", "spring-boot-starter-web").withoutVersion()
+            library("jackson", "com.fasterxml.jackson.module", "jackson-module-kotlin").withoutVersion()
+            library("kotlin-reflect", "org.jetbrains.kotlin", "kotlin-reflect").withoutVersion()
+            library("kotlin-stdlib-jdk8", "org.jetbrains.kotlin", "kotlin-stdlib-jdk8").withoutVersion()
+
+            library("spring-boot-starter-test", "org.springframework.boot", "spring-boot-starter-test").withoutVersion()
+            library("junitJupiterEngine", "org.junit.jupiter", "junit-jupiter-engine").version("5.9.2")
+            library("kluent", "org.amshove.kluent", "kluent").version("1.72")
+        }
+    }
+}
