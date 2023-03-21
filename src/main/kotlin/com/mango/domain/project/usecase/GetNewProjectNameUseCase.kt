f96413174ee65edc7d6aa5df713f5328fb2d5f83
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-03-21 13:15:00 +0100

    MNG-96 Add GetNewProjectNameUseCase() (#157)

---
+++ src/main/kotlin/com/mango/domain/project/usecase/GetNewProjectNameUseCase.kt
@@ -0,0 +1,27 @@
+package com.mango.domain.project.usecase
+
+import org.springframework.stereotype.Service
+
+@Service
+class GetNewProjectNameUseCase {
+    operator fun invoke(oldName: String): String {
+        val words = oldName
+            .split(" ")
+            .filter { it.isNotBlank() }
+
+        val lastWord = words.last().toIntOrNull()
+        val secondToLastWord = words.getOrElse(words.size - 2) { "" }
+
+        return if (words.last() == "copy" && !oldName.endsWith(" ")) {
+            "$oldName 2"
+        } else if (secondToLastWord == "copy" && lastWord != null) {
+            val counter = words.last().toInt()
+            val newCounter = (counter + 1)
+            oldName.replace(counter.toString(), newCounter.toString())
+        } else if (oldName.endsWith(" ")) {
+            "${oldName}copy"
+        } else {
+            "$oldName copy"
+        }
+    }
+}
