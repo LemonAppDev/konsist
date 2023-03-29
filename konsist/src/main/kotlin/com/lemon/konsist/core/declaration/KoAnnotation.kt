e751a43919e97572e2e915d46fae2d1aa3c3eaa5
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-03-29 21:03:00 +0200

    MNG-127 Add hasAnnotation to KoSecoundaryConstructor (#189)
    Co-authored-by: Igor Wojda <igor.wojda@gmail.com>

--- konsist/src/main/kotlin/com/lemon/konsist/core/declaration/KoAnnotation.kt
+++ konsist/src/main/kotlin/com/lemon/konsist/core/declaration/KoAnnotation.kt
@@ -6,4 +6,6 @@ open class KoAnnotation(
     private val ktObjectDeclaration: KtAnnotationEntry,
 ) : KoNamedDeclaration(ktObjectDeclaration) {
     val type = ktObjectDeclaration.typeReference?.text ?: ""
+
+    override val name by lazy { ktObjectDeclaration.shortName.toString() }
 }
--- konsist/src/test/kotlin/com/lemon/konsist/core/kosecondaryconstructor/KoSecondaryConstructorTest.kt
+++ konsist/src/test/kotlin/com/lemon/konsist/core/kosecondaryconstructor/KoSecondaryConstructorTest.kt
@@ -1,5 +1,7 @@
 package com.lemon.konsist.core.kosecondaryconstructor
 
+import SampleAnnotation
+import SampleAnnotation1
 import com.lemon.konsist.TestSnippetProvider
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
@@ -73,6 +75,39 @@ class KoSecondaryConstructorTest {
 
         // then
         sut.name shouldBeEqualTo "SampleClass"
+    }
+
+    @Test
+    fun `class-with-secondary-constructor-with-annotation`() {
+        // given
+        val sut = getSut("class-with-secondary-constructor-with-annotation")
+            .classes()
+            .first()
+            .secondaryConstructors
+            .first()
+
+        // then
+        sut.apply {
+            annotations.map { it.name } shouldBeEqualTo listOf("SampleAnnotation")
+            hasAnnotation(SampleAnnotation::class) shouldBeEqualTo true
+            hasAnnotation(SampleAnnotation1::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `class-with-secondary-constructor-without-annotation`() {
+        // given
+        val sut = getSut("class-with-secondary-constructor-without-annotation")
+            .classes()
+            .first()
+            .secondaryConstructors
+            .first()
+
+        // then
+        sut.apply {
+            annotations.isEmpty() shouldBeEqualTo true
+            hasAnnotation(SampleAnnotation::class) shouldBeEqualTo false
+        }
     }
 
     private fun getSut(fileName: String) = TestSnippetProvider.getSnippetKoScope("core/kosecondaryconstructor/snippet/$fileName.kt.txt")
