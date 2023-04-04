415c57e4505d0b222f97204684d46afba3469d53
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-04 14:29:18 +0200

    MNG-141 Add KoImport.alias property (#220)

--- konsist/src/main/kotlin/com/lemon/konsist/core/declaration/KoImport.kt
+++ konsist/src/main/kotlin/com/lemon/konsist/core/declaration/KoImport.kt
@@ -5,6 +5,8 @@ import org.jetbrains.kotlin.psi.KtImportDirective
 class KoImport private constructor(private val ktImportDirective: KtImportDirective) : KoNamedDeclaration(ktImportDirective) {
     override val name by lazy { ktImportDirective.importPath?.fqName.toString() }
 
+    val alias by lazy { ktImportDirective.alias?.name ?: name }
+
     companion object {
         private val cache = KoDeclarationCache<KoImport>()
         fun getInstance(ktImportDirective: KtImportDirective) = if (cache.hasKey(ktImportDirective)) {
