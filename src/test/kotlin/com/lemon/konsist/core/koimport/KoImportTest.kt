c0a058466cf8e2f2a0cd788ee0c8e594ecec0011
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-04 14:29:18 +0200

    MNG-141 Add KoImport.alias property (#220)

--- src/test/kotlin/com/lemon/konsist/core/koimport/KoImportTest.kt
+++ src/test/kotlin/com/lemon/konsist/core/koimport/KoImportTest.kt
@@ -9,11 +9,22 @@ class KoImportTest {
     @Test
     fun `import-name`() {
         // given
-        val sut = getSut("import-name")
-            .imports()
+        val sut = getSut("import-name").imports()
 
         // then
         sut.map { it.name } shouldBeEqualTo listOf("com.lemon.konsist.testdata.SampleClass")
+    }
+
+    @Test
+    fun `import-name-with-alias`() {
+        // given
+        val sut = getSut("import-name-with-alias").imports()
+
+        // then
+        with(sut) {
+            this[0].alias shouldBeEqualTo "com.lemon.konsist.testdata.SampleClass"
+            this[1].alias shouldBeEqualTo "AliasType"
+        }
     }
 
     private fun getSut(fileName: String) = TestSnippetProvider.getSnippetKoScope("core/koimport/snippet/$fileName.kttxt")
