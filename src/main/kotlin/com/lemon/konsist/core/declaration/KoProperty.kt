de0b5b899ce482b9556b91882119c52cbe5658be
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-04 12:38:20 +0200

    MNG-162 Add KoProperty.isVal, KoProperty.isVar, KoProperty.isConst (#218)
    * remove default values from `CreateProjectRequestModel`
    
    * remove default values from `CreateTaskRequestModel`
    
    * remove default values from `UpdateTaskRequestModel` and delete prefix `com.mango.domain.task.activity.`
    
    * delete prefix `com.mango.domain.task.model.`
    
    * fix integration tests
    
    * fix tests
    
    * resolve conflict
    
    * upd integration test
    
    * add `KoProperty.isConst`
    
    * add empty line
    
    * simplify KoProperty.isConst

--- src/main/kotlin/com/lemon/konsist/core/declaration/KoProperty.kt
+++ src/main/kotlin/com/lemon/konsist/core/declaration/KoProperty.kt
@@ -1,5 +1,19 @@
 package com.lemon.konsist.core.declaration
 
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtModifierList
 import org.jetbrains.kotlin.psi.KtProperty
 
-class KoProperty(private val ktProperty: KtProperty) : KoDeclaration(ktProperty)
+class KoProperty(private val ktProperty: KtProperty) : KoDeclaration(ktProperty) {
+    val isVar by lazy { ktProperty.isVar }
+
+    val isVal by lazy { !ktProperty.isVar }
+
+    val isConst by lazy {
+        val modifiers = ktProperty
+            .children
+            .filterIsInstance<KtModifierList>()
+
+        modifiers.firstOrNull()?.hasModifier(KtTokens.CONST_KEYWORD) ?: false
+    }
+}
