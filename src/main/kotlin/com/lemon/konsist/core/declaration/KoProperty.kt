c39e6dd729315f6f1a1bde8dfa9c7755b3c438b0
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-07 07:22:01 +0200

    MNG-168 KoProperty add missing modifiers - isX (#241)

--- src/main/kotlin/com/lemon/konsist/core/declaration/KoProperty.kt
+++ src/main/kotlin/com/lemon/konsist/core/declaration/KoProperty.kt
@@ -11,6 +11,16 @@ class KoProperty private constructor(private val ktProperty: KtProperty) : KoDec
 
     val isVal by lazy { !ktProperty.isVar }
 
+    val isLateinit by lazy { ktProperty.modifierList?.hasModifier(KtTokens.LATEINIT_KEYWORD) ?: false }
+
+    val isOverride by lazy { ktProperty.modifierList?.hasModifier(KtTokens.OVERRIDE_KEYWORD) ?: false }
+
+    val isAbstract by lazy { ktProperty.modifierList?.hasModifier(KtTokens.ABSTRACT_KEYWORD) ?: false }
+
+    val isOpen by lazy { ktProperty.modifierList?.hasModifier(KtTokens.OPEN_KEYWORD) ?: false }
+
+    val isFinal by lazy { ktProperty.modifierList?.hasModifier(KtTokens.FINAL_KEYWORD) ?: false }
+
     val isConst by lazy {
         ktProperty
             .children
