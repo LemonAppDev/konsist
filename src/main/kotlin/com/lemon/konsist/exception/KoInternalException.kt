3b64ee17f387063e2ca0ed6c2828ef64ebe94bb6
Igor Wojda <igor.wojda@gmail.com>
2023-04-12 01:51:13 +0200

    Display declaration content when internal error is thrown (#271)

--- src/main/kotlin/com/lemon/konsist/exception/KoInternalException.kt
+++ src/main/kotlin/com/lemon/konsist/exception/KoInternalException.kt
@@ -1,3 +1,6 @@
 package com.lemon.konsist.exception
 
-class KoInternalException(message: String? = null, cause: Throwable? = null) : KoException(message, cause)
+import com.lemon.konsist.core.declaration.KoBaseDeclaration
+
+class KoInternalException(message: String? = null, cause: Throwable? = null, koBaseDeclaration: KoBaseDeclaration? = null) :
+    KoException("$message, declaration:\n${koBaseDeclaration?.text}", cause)
