b24f867cbf07d8ffcbd18f9d780f7a74ec013dc1
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-12 10:09:12 +0200

    MNG-189 KoFile hasImport Allow to use package selection scope syntax (#264)

--- konsist/src/test/kotlin/com/lemon/konsist/core/declaration/kodeclaration/KoDeclarationForResideInPackagesTest.kt
+++ konsist/src/test/kotlin/com/lemon/konsist/core/declaration/kodeclaration/KoDeclarationForResideInPackagesTest.kt
@@ -6,124 +6,36 @@ import org.junit.jupiter.api.Test
 
 class KoDeclarationForResideInPackagesTest {
     @Test
-    fun `reside-in-packages-with-one-word`() {
+    fun `none-package-reside-in-file-package`() {
         // given
-        val sut = getSut("reside-in-packages-with-one-word")
+        val sut = getSut("none-package-reside-in-file-package")
             .classes()
             .first()
 
         // then
-        sut.apply {
-            resideInPackages("com") shouldBeEqualTo false
-            resideInPackages("..com..") shouldBeEqualTo true
-            resideInPackages("..com") shouldBeEqualTo false
-            resideInPackages("com..") shouldBeEqualTo true
-            resideInPackages("domain..") shouldBeEqualTo false
-            resideInPackages("..domain") shouldBeEqualTo false
-            resideInPackages("..domain..") shouldBeEqualTo true
-            resideInPackages("usecase..") shouldBeEqualTo false
-            resideInPackages("..usecase") shouldBeEqualTo true
-            resideInPackages("..usecase..") shouldBeEqualTo true
-            resideInPackages("..update") shouldBeEqualTo false
-            resideInPackages("update..") shouldBeEqualTo false
-            resideInPackages("..update..") shouldBeEqualTo true
-            resideInPackages("..nonexisting..") shouldBeEqualTo false
-        }
+        sut.resideInPackages("com", "..nonexisting..") shouldBeEqualTo false
     }
 
     @Test
-    fun `reside-in-packages-without-two-dots`() {
+    fun `all-packages-reside-in-file-package`() {
         // given
-        val sut = getSut("reside-in-packages-without-two-dots")
+        val sut = getSut("all-packages-reside-in-file-package")
             .classes()
             .first()
 
         // then
-        sut.apply {
-            resideInPackages("com.domain") shouldBeEqualTo false
-            resideInPackages("com.domain.update") shouldBeEqualTo false
-            resideInPackages("com.domain.update.usecase") shouldBeEqualTo true
-            resideInPackages("com.domain.usecase") shouldBeEqualTo false
-        }
+        sut.resideInPackages("com..", "com.domain.update.usecase") shouldBeEqualTo true
     }
 
     @Test
-    fun `reside-in-packages-with-first-and-last-word`() {
+    fun `one-package-reside-in-file-package`() {
         // given
-        val sut = getSut("reside-in-packages-with-first-and-last-word")
+        val sut = getSut("one-package-reside-in-file-package")
             .classes()
             .first()
 
         // then
-        sut.apply {
-            resideInPackages("com..usecase") shouldBeEqualTo true
-            resideInPackages("..com..usecase") shouldBeEqualTo true
-            resideInPackages("com..usecase..") shouldBeEqualTo true
-            resideInPackages("..com..usecase..") shouldBeEqualTo true
-        }
-    }
-
-    @Test
-    fun `reside-in-packages-with-dots-at-end`() {
-        // given
-        val sut = getSut("reside-in-packages-with-dots-at-end")
-            .classes()
-            .first()
-
-        // then
-        sut.apply {
-            resideInPackages("com.domain..") shouldBeEqualTo true
-            resideInPackages("com.domain.update..") shouldBeEqualTo true
-            resideInPackages("com.domain.update.usecase..") shouldBeEqualTo true
-            resideInPackages("com.domain.usecase..") shouldBeEqualTo false
-        }
-    }
-
-    @Test
-    fun `reside-in-packages-with-dots-in-few-places`() {
-        // given
-        val sut = getSut("reside-in-packages-with-dots-in-few-places")
-            .classes()
-            .first()
-
-        // then
-        sut.apply {
-            resideInPackages("domain..usecase..") shouldBeEqualTo false
-            resideInPackages("com.domain..usecase..") shouldBeEqualTo true
-            resideInPackages("..domain..usecase") shouldBeEqualTo true
-            resideInPackages("..domain..usecase..") shouldBeEqualTo true
-            resideInPackages("..domain..update..") shouldBeEqualTo true
-            resideInPackages("com..domain..update..usecase") shouldBeEqualTo true
-            resideInPackages("domain..update..usecase") shouldBeEqualTo false
-        }
-    }
-
-    @Test
-    fun `reside-in-packages-with-repeating-word`() {
-        // given
-        val sut = getSut("reside-in-packages-with-repeating-word")
-            .classes()
-            .first()
-
-        // then
-        sut.apply {
-            resideInPackages("..domain..domain..update.usecase") shouldBeEqualTo false
-            resideInPackages("com..domain..update..com..usecase") shouldBeEqualTo false
-        }
-    }
-
-    @Test
-    fun `reside-in-packages-with-the-wrong-order`() {
-        // given
-        val sut = getSut("reside-in-packages-with-the-wrong-order")
-            .classes()
-            .first()
-
-        // then
-        sut.apply {
-            resideInPackages("..update..domain..") shouldBeEqualTo false
-            resideInPackages("..usecase..domain..") shouldBeEqualTo false
-        }
+        sut.resideInPackages("com..", "com") shouldBeEqualTo true
     }
 
     private fun getSut(fileName: String) = getSnippetKoScope("kodeclaration/snippet/forresideinpackage/", fileName)
