2d229113f49c0cb5bc4cdca096f88575961bddde
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-12 12:54:41 +0200

    MNG-234 KoClass add hasParentClass, hasParentInterface, hasParent (#280)

--- src/test/kotlin/com/lemon/konsist/core/declaration/koclass/KoClassTest.kt
+++ src/test/kotlin/com/lemon/konsist/core/declaration/koclass/KoClassTest.kt
@@ -197,8 +197,15 @@ class KoClassTest {
         // then
         sut.run {
             parents shouldBeEqualTo listOf("SampleParentClass", "SampleParentInterface")
+            hasParent() shouldBeEqualTo true
             parentClass shouldBeEqualTo "SampleParentClass"
+            hasParentClass() shouldBeEqualTo true
+            hasParentClass("SampleParentClass") shouldBeEqualTo true
+            hasParentClass("OtherClass") shouldBeEqualTo false
             parentInterfaces shouldBeEqualTo listOf("SampleParentInterface")
+            hasParentInterface() shouldBeEqualTo true
+            hasParentInterface("SampleParentInterface") shouldBeEqualTo true
+            hasParentInterface("OtherInterface") shouldBeEqualTo false
         }
     }
 
@@ -212,8 +219,13 @@ class KoClassTest {
         // then
         sut.run {
             parents shouldBeEqualTo listOf()
+            hasParent() shouldBeEqualTo false
             parentClass shouldBeEqualTo null
+            hasParentClass() shouldBeEqualTo false
+            hasParentClass("SampleParentClass") shouldBeEqualTo false
             parentInterfaces shouldBeEqualTo listOf()
+            hasParentInterface() shouldBeEqualTo false
+            hasParentInterface("SampleInterface") shouldBeEqualTo false
         }
     }
 
