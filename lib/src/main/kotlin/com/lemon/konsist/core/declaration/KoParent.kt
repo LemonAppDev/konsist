c465c4a6dd1798f0edc606a898c3d5e861bbdbeb
natalia.peterwas <n.peterwas@gmail.com>
2023-04-17 11:11:44 +0200

    Add properties, methods and tests in KoParent

--- lib/src/main/kotlin/com/lemon/konsist/core/declaration/KoParent.kt
+++ lib/src/main/kotlin/com/lemon/konsist/core/declaration/KoParent.kt
@@ -1,17 +1,27 @@
 package com.lemon.konsist.core.declaration
 
 import com.lemon.konsist.core.cache.KoDeclarationCache
+import org.jetbrains.kotlin.psi.KtDelegatedSuperTypeEntry
 import org.jetbrains.kotlin.psi.KtSuperTypeListEntry
 
 class KoParent private constructor(private val ktSuperTypeListEntry: KtSuperTypeListEntry): KoNamedDeclaration(ktSuperTypeListEntry) {
 
     override val name: String by lazy {
-        ktSuperTypeListEntry.text.removeSuffix("()")
+        ktSuperTypeListEntry
+            .text
+            .removeSuffix("()")
+            .substringBefore(" by")
     }
 
     val delegateName by lazy {
-
+        if (ktSuperTypeListEntry is KtDelegatedSuperTypeEntry){
+            ktSuperTypeListEntry.text
+        } else null
     }
+
+    fun hasDelegate() = delegateName != null
+
+    fun hasDelegate(delegateName: String) = name == delegateName
 
     companion object {
         private val cache = KoDeclarationCache<KoParent>()
