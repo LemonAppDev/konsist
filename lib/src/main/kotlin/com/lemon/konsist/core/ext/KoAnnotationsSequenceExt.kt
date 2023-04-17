b1db262cd2a2f096e401594182ec00c2bcf19124
natalia.peterwas <n.peterwas@gmail.com>
2023-04-17 13:13:01 +0200

    upd logic in KoAnnotationsSequenceExt.kt

--- lib/src/main/kotlin/com/lemon/konsist/core/ext/KoAnnotationsSequenceExt.kt
+++ lib/src/main/kotlin/com/lemon/konsist/core/ext/KoAnnotationsSequenceExt.kt
@@ -4,15 +4,15 @@ import com.lemon.konsist.core.declaration.KoAnnotation
 
 fun Sequence<KoAnnotation>.withType(type: String) = filter { it.type == type }
 
-fun Sequence<KoAnnotation>.withoutType(type: String) = filterNot { it.type == type }
+fun Sequence<KoAnnotation>.withoutType(type: String) = this - withType(type).toSet()
 
 fun Sequence<KoAnnotation>.withName(name: String) = filter { it.name == name }
 
 inline fun <reified T> Sequence<KoAnnotation>.withName() = filter { it.name == T::class.simpleName }
 
-fun Sequence<KoAnnotation>.withoutName(name: String) = filterNot { it.name == name }
+fun Sequence<KoAnnotation>.withoutName(name: String) = this - withName(name).toSet()
 
-inline fun <reified T> Sequence<KoAnnotation>.withoutName() = filterNot { it.name == T::class.simpleName }
+inline fun <reified T> Sequence<KoAnnotation>.withoutName() = this - withName<T>().toSet()
 
 fun Sequence<KoAnnotation>.withFullyQualifiedClassName(name: String) =
     filter { it.fullyQualifiedName == name }
@@ -21,7 +21,7 @@ inline fun <reified T> Sequence<KoAnnotation>.withFullyQualifiedClassName() =
     filter { it.fullyQualifiedName == T::class.qualifiedName }
 
 fun Sequence<KoAnnotation>.withoutFullyQualifiedClassName(name: String) =
-    filterNot { it.fullyQualifiedName == name }
+    this - withFullyQualifiedClassName(name).toSet()
 
 inline fun <reified T> Sequence<KoAnnotation>.withoutFullyQualifiedClassName() =
-    filterNot { it.fullyQualifiedName == T::class.qualifiedName }
+    this - withFullyQualifiedClassName<T>().toSet()
