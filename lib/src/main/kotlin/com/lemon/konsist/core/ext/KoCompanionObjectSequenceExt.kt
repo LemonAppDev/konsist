ff1836178d3ea788368d33b80f90efeec173d34a
natalia.peterwas <n.peterwas@gmail.com>
2023-04-18 09:08:02 +0200

    upd KoCompanionObjectSequenceExt and its tests

--- lib/src/main/kotlin/com/lemon/konsist/core/ext/KoCompanionObjectSequenceExt.kt
+++ lib/src/main/kotlin/com/lemon/konsist/core/ext/KoCompanionObjectSequenceExt.kt
@@ -4,4 +4,4 @@ import com.lemon.konsist.core.declaration.KoCompanionObject
 
 fun Sequence<KoCompanionObject>.withExplicitName() = filter { it.hasExplicitName() }
 
-fun Sequence<KoCompanionObject>.withoutExplicitName() = this - withExplicitName().toSet()
+fun Sequence<KoCompanionObject>.withoutExplicitName() = filterNot { it.hasExplicitName() }
--- lib/src/test/kotlin/com/lemon/konsist/core/ext/KoCompanionObjectSequenceExtTest.kt
+++ lib/src/test/kotlin/com/lemon/konsist/core/ext/KoCompanionObjectSequenceExtTest.kt
@@ -10,10 +10,12 @@ class KoCompanionObjectSequenceExtTest {
     @Test
     fun `withExplicitName() returns companion object with explicit name`() {
         // given
-        val companionObject1: KoCompanionObject = mockk()
-        every { companionObject1.hasExplicitName() } returns true
-        val companionObject2: KoCompanionObject = mockk()
-        every { companionObject2.hasExplicitName() } returns false
+        val companionObject1: KoCompanionObject = mockk {
+            every { hasExplicitName() } returns true
+        }
+        val companionObject2: KoCompanionObject = mockk {
+            every { hasExplicitName() } returns false
+        }
         val companionObjects = sequenceOf(companionObject1, companionObject2)
 
         // when
@@ -24,28 +26,14 @@ class KoCompanionObjectSequenceExtTest {
     }
 
     @Test
-    fun `withExplicitName() returns empty list when none companion object has explicit type`() {
-        // given
-        val companionObject1: KoCompanionObject = mockk()
-        every { companionObject1.hasExplicitName() } returns false
-        val companionObject2: KoCompanionObject = mockk()
-        every { companionObject2.hasExplicitName() } returns false
-        val companionObjects = sequenceOf(companionObject1, companionObject2)
-
-        // when
-        val sut = companionObjects.withExplicitName()
-
-        // then
-        sut.toList() shouldBeEqualTo emptyList()
-    }
-
-    @Test
     fun `withoutExplicitName() returns companion object without explicit name`() {
         // given
-        val companionObject1: KoCompanionObject = mockk()
-        every { companionObject1.hasExplicitName() } returns true
-        val companionObject2: KoCompanionObject = mockk()
-        every { companionObject2.hasExplicitName() } returns false
+        val companionObject1: KoCompanionObject = mockk {
+            every { hasExplicitName() } returns true
+        }
+        val companionObject2: KoCompanionObject = mockk {
+            every { hasExplicitName() } returns false
+        }
         val companionObjects = sequenceOf(companionObject1, companionObject2)
 
         // when
@@ -53,37 +41,5 @@ class KoCompanionObjectSequenceExtTest {
 
         // then
         sut.toList() shouldBeEqualTo listOf(companionObject2)
-    }
-
-    @Test
-    fun `withoutExplicitName() returns empty list when all companion objects have explicit type`() {
-        // given
-        val companionObject1: KoCompanionObject = mockk()
-        every { companionObject1.hasExplicitName() } returns true
-        val companionObject2: KoCompanionObject = mockk()
-        every { companionObject2.hasExplicitName() } returns true
-        val companionObjects = sequenceOf(companionObject1, companionObject2)
-
-        // when
-        val sut = companionObjects.withoutExplicitName()
-
-        // then
-        sut.toList() shouldBeEqualTo emptyList()
-    }
-
-    @Test
-    fun `withoutExplicitName() returns list with companion objects when none companion object has explicit type`() {
-        // given
-        val companionObject1: KoCompanionObject = mockk()
-        every { companionObject1.hasExplicitName() } returns false
-        val companionObject2: KoCompanionObject = mockk()
-        every { companionObject2.hasExplicitName() } returns false
-        val companionObjects = sequenceOf(companionObject1, companionObject2)
-
-        // when
-        val sut = companionObjects.withoutExplicitName()
-
-        // then
-        sut.toList() shouldBeEqualTo listOf(companionObject1, companionObject2)
     }
 }
