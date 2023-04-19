145839c447f3d760a6573962a6ae79ef42af430f
Igor Wojda <igor.wojda@gmail.com>
2023-04-19 13:06:38 +0200

    Upd formatting

--- lib/src/main/kotlin/com/lemon/konsist/core/ext/KoImportSequenceExt.kt
+++ lib/src/main/kotlin/com/lemon/konsist/core/ext/KoImportSequenceExt.kt
@@ -5,8 +5,7 @@ import com.lemon.konsist.core.declaration.KoImport
 fun Sequence<KoImport>.withAlias(name: String? = null) = filter {
     when (name) {
         null -> it.alias != it.name
-        else ->
-            it.alias == name
+        else -> it.alias == name
     }
 }
 
