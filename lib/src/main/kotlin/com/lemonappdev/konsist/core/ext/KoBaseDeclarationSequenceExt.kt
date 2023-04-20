6ba85f45517fc72b9139c74cc412b596683d7b81
natalia.peterwas <n.peterwas@gmail.com>
2023-04-20 10:13:30 +0200

    add extensions withFilePaths and withoutFilePaths to KoBaseDeclaration

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/ext/KoBaseDeclarationSequenceExt.kt
@@ -0,0 +1,14 @@
+package com.lemonappdev.konsist.core.ext
+
+import com.lemonappdev.konsist.core.declaration.KoBaseDeclaration
+
+fun Sequence<KoBaseDeclaration>.withFilePaths(vararg path: String) = filter { koBaseDeclaration ->
+    path.all { koBaseDeclaration.hasFilePath(it) }
+}
+
+fun Sequence<KoBaseDeclaration>.withSomeFilePaths(vararg path: String) = filter { koBaseDeclaration ->
+    path.any { koBaseDeclaration.hasFilePath(it) } }
+
+
+fun Sequence<KoBaseDeclaration>.withoutFilePaths(vararg path: String) = filter { koBaseDeclaration ->
+    path.none { koBaseDeclaration.hasFilePath(it) } }
