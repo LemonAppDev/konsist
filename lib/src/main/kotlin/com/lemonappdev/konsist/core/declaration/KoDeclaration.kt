549e71bd222eb1b36b788f19b2847f8b752da12b
natalia.peterwas <n.peterwas@gmail.com>
2023-04-21 13:52:29 +0200

    fix spotless

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoDeclaration.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoDeclaration.kt
@@ -40,9 +40,7 @@ abstract class KoDeclaration(private val ktTypeParameterListOwner: KtTypeParamet
         val hasOtherVisibilityModifier =
             hasModifier(KtTokens.PRIVATE_KEYWORD) ||
                 hasModifier(KtTokens.PROTECTED_KEYWORD) ||
-                hasModifier(
-                    KtTokens.INTERNAL_KEYWORD,
-                )
+                hasModifier(KtTokens.INTERNAL_KEYWORD)
 
         hasOtherVisibilityModifier.not()
     }
