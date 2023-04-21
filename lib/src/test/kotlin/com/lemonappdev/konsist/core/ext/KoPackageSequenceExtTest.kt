2f39f4141a850e41639e6020389f14fc2457c29f
natalia.peterwas <n.peterwas@gmail.com>
2023-04-21 09:38:09 +0200

    add vararg to extensions accepting String in KoPackageSequenceExt

--- lib/src/test/kotlin/com/lemonappdev/konsist/core/ext/KoPackageSequenceExtTest.kt
+++ lib/src/test/kotlin/com/lemonappdev/konsist/core/ext/KoPackageSequenceExtTest.kt
@@ -8,42 +8,50 @@ import org.junit.jupiter.api.Test
 
 class KoPackageSequenceExtTest {
     @Test
-    fun `withQualifiedName() returns package1 with given name`() {
+    fun `withQualifiedName() returns packages with one of given names`() {
         // given
-        val qualifiedName1 = "com.samplepackage"
-        val qualifiedName2 = "com.otherpackage"
+        val qualifiedName1 = "com.samplepackage1"
+        val qualifiedName2 = "com.samplepackage2"
+        val qualifiedName3 = "com.samplepackage3"
         val package1: KoPackage = mockk {
             every { qualifiedName } returns qualifiedName1
         }
         val package2: KoPackage = mockk {
             every { qualifiedName } returns qualifiedName2
         }
-        val packages = sequenceOf(package1, package2)
+        val package3: KoPackage = mockk {
+            every { qualifiedName } returns qualifiedName3
+        }
+        val packages = sequenceOf(package1, package2, package3)
 
         // when
-        val sut = packages.withQualifiedName(qualifiedName1)
+        val sut = packages.withQualifiedName(qualifiedName1, qualifiedName2)
 
         // then
-        sut.toList() shouldBeEqualTo listOf(package1)
+        sut.toList() shouldBeEqualTo listOf(package1, package2)
     }
 
     @Test
-    fun `withoutQualifiedName() returns package without given name`() {
+    fun `withoutQualifiedName() returns package without given names`() {
         // given
-        val qualifiedName1 = "com.samplepackage"
-        val qualifiedName2 = "com.otherpackage"
+        val qualifiedName1 = "com.samplepackage1"
+        val qualifiedName2 = "com.samplepackage2"
+        val qualifiedName3 = "com.samplepackage3"
         val package1: KoPackage = mockk {
             every { qualifiedName } returns qualifiedName1
         }
         val package2: KoPackage = mockk {
             every { qualifiedName } returns qualifiedName2
         }
-        val packages = sequenceOf(package1, package2)
+        val package3: KoPackage = mockk {
+            every { qualifiedName } returns qualifiedName3
+        }
+        val packages = sequenceOf(package1, package2, package3)
 
         // when
-        val sut = packages.withoutQualifiedName("com.samplepackage")
+        val sut = packages.withoutQualifiedName(qualifiedName1, qualifiedName2)
 
         // then
-        sut.toList() shouldBeEqualTo listOf(package2)
+        sut.toList() shouldBeEqualTo listOf(package3)
     }
 }
