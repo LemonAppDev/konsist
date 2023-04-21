8802fbea158bdb0f0ca460a796adbc4d07d33795
natalia.peterwas <n.peterwas@gmail.com>
2023-04-21 10:02:15 +0200

    add vararg to extensions accepting String in KoParentSequenceExt

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/ext/KoParentSequenceExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/ext/KoParentSequenceExt.kt
@@ -2,6 +2,16 @@ package com.lemonappdev.konsist.core.ext
 
 import com.lemonappdev.konsist.core.declaration.KoParent
 
-fun Sequence<KoParent>.withDelegate(name: String? = null) = filter { it.hasDelegate(name) }
+fun Sequence<KoParent>.withDelegate(vararg names: String) = filter {
+    when {
+        names.isEmpty() -> it.hasDelegate()
+        else -> names.any { name -> it.hasDelegate(name) }
+    }
+}
 
-fun Sequence<KoParent>.withoutDelegate(name: String? = null) = filterNot { it.hasDelegate(name) }
+fun Sequence<KoParent>.withoutDelegate(vararg names: String) = filter {
+    when {
+        names.isEmpty() -> !it.hasDelegate()
+        else -> names.none { name -> it.hasDelegate(name) }
+    }
+}
