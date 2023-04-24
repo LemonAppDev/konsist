cfeb0dee44d17f532061ca8c176569023ea4d22c
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-04-24 13:22:32 +0200

    KON-65 KoComplexDeclaration Add Extensions For Providers (#72
    * add extensions for providers
    
    * fix spotless

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/ext/KoComplexDeclarationSequenceExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/ext/KoComplexDeclarationSequenceExt.kt
@@ -1,5 +1,6 @@
 package com.lemonappdev.konsist.core.ext
 
+import com.lemonappdev.konsist.core.const.KoModifier
 import com.lemonappdev.konsist.core.declaration.KoComplexDeclaration
 import kotlin.reflect.KClass
 
@@ -30,3 +31,42 @@ fun Sequence<KoComplexDeclaration>.withoutTypeOf(vararg types: KClass<*>) = filt
 inline fun <reified T> Sequence<KoComplexDeclaration>.withTypeOf() = filter { it.representsType<T>() }
 
 inline fun <reified T> Sequence<KoComplexDeclaration>.withoutTypeOf() = filterNot { it.representsType<T>() }
+
+fun Sequence<KoComplexDeclaration>.declarations(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+    includeLocal: Boolean = false,
+) = flatMap { it.declarations(modifiers, includeNested, includeLocal) }
+
+fun Sequence<KoComplexDeclaration>.classes(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+    includeLocal: Boolean = false,
+) = flatMap { it.classes(modifiers, includeNested, includeLocal) }
+
+fun Sequence<KoComplexDeclaration>.interfaces(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+) = flatMap { it.interfaces(modifiers, includeNested) }
+
+fun Sequence<KoComplexDeclaration>.objects(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+) = flatMap { it.objects(modifiers, includeNested) }
+
+fun Sequence<KoComplexDeclaration>.companionObjects(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+) = flatMap { it.companionObjects(modifiers, includeNested) }
+
+fun Sequence<KoComplexDeclaration>.properties(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+    includeLocal: Boolean = false,
+) = flatMap { it.properties(modifiers, includeNested, includeLocal) }
+
+fun Sequence<KoComplexDeclaration>.functions(
+    modifiers: List<KoModifier> = emptyList(),
+    includeNested: Boolean = false,
+    includeLocal: Boolean = false,
+) = flatMap { it.functions(modifiers, includeNested, includeLocal) }
