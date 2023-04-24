8a44321203bbacddccdbdd894c49dc010545dcbe
natalia.peterwas <n.peterwas@gmail.com>
2023-04-24 14:58:50 +0200

    add test

---
+++ lib/src/test/kotlin/com/lemonappdev/konsist/core/verify/assert/PrintDeclaration.kt
@@ -0,0 +1,21 @@
+package com.lemonappdev.konsist.core.verify.assert
+
+import com.lemonappdev.konsist.core.declaration.KoScope
+import com.lemonappdev.konsist.core.ext.withNamePrefix
+import com.lemonappdev.konsist.core.verify.printDeclarations
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class PrintDeclaration {
+    @Test
+    fun `print declaration from package`() {
+        // given
+        val scope = KoScope.fromPackage("com.lemonappdev.konsist.core.declaration.koscope")
+        val sut = scope
+            .classes()
+            .withNamePrefix("KoScopeForC")
+
+        // then
+        sut.printDeclarations() shouldBeEqualTo sut
+    }
+}
