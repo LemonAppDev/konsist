4f054f811970e8c84c905d60bb111db519a7b50e
Igor Wojda <igor.wojda@gmail.com>
2023-04-25 13:09:28 +0200

    Set konsist.jar Java 1.8

--- buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
+++ buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
@@ -3,6 +3,12 @@ plugins {
 }
 
 java {
+    // Make consist compatible with  Java 1.8
+    toolchain {
+        // Java bytecode version to 52.0
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+
     // Generated sources.jar for the library jar
     withSourcesJar()
 
