8d20b3e7ca20f5d7911be5c62b9e62ef7daa1b8c
Igor Wojda <igor.wojda@gmail.com>
2023-05-03 16:53:13 +0200

    Make internal

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoDocTagDeclarationImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoDocTagDeclarationImpl.kt
@@ -2,4 +2,4 @@ package com.lemonappdev.konsist.core.declaration
 
 import com.lemonappdev.konsist.api.KoTag
 
-open class KoDocTagDeclarationImpl(open val name: KoTag, open val description: String)
+internal open class KoDocTagDeclarationImpl(open val name: KoTag, open val description: String)
--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoValuedDocTagDeclarationImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoValuedDocTagDeclarationImpl.kt
@@ -1,7 +1,8 @@
 package com.lemonappdev.konsist.core.declaration
 
 import com.lemonappdev.konsist.api.KoTag
-open class KoValuedDocTagDeclarationImpl(
+
+internal open class KoValuedDocTagDeclarationImpl(
     override val name: KoTag,
     val value: String,
     override val description: String,
