d32ff363af3d15c52461f10bcfb8b412b3efc12d
Igor Wojda <igor.wojda@gmail.com>
2023-05-04 22:06:40 +0200

    Extract doka local plugin

--- buildSrc/src/main/kotlin/local.dokka.gradle.kts
+++ buildSrc/src/main/kotlin/local.dokka.gradle.kts
@@ -1,3 +1,26 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     id("org.jetbrains.dokka")
+}
+
+// Dokka config
+// https://kotlinlang.org/docs/dokka-gradle.html#package-options
+
+tasks.withType<DokkaTask>().configureEach {
+//    failOnWarning.set(false)
+
+    dokkaSourceSets.configureEach {
+        // Generate docs for 'api'
+        perPackageOption {
+            matchingRegex.set("com.lemonappdev.konsist.api.*")
+            suppress.set(false)
+        }
+
+        // Don't generate docs for all packages
+        perPackageOption {
+            matchingRegex.set(".*")
+            suppress.set(true)
+        }
+    }
 }
