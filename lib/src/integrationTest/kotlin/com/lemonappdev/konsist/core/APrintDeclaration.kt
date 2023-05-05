314399a1c95054f58a62c8cf594b10c5ed18dd2c
Igor Wojda <igor.wojda@gmail.com>
2023-05-05 11:13:18 +0200

    Test Rename

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/APrintDeclaration.kt
@@ -0,0 +1,15 @@
+package com.lemonappdev.konsist.core
+
+class APrintDeclaration {
+//    @Test
+//    fun `return value after printDeclaration() is equal to original value`() {
+//        // given
+//        val scope = Konsist.scopeFromPackage("com.lemonappdev.konsist.core.scope.koscope")
+//        val sut = scope
+//            .classes()
+//            .withNamePrefix("KoScopeForC")
+//
+//        // then
+//        sut.print() shouldBeEqualTo sut
+//    }
+}
