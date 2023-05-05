16442b778f4fad5bc9a0a500682084a7497251d0
Igor Wojda <igor.wojda@gmail.com>
2023-05-05 13:02:27 +0200

    Update names

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/util/PackageHelperTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/util/PackageHelperTest.kt
@@ -8,230 +8,230 @@ class PackageHelperTest {
     @Test
     fun `has given fragment with one word`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com", name) shouldBeEqualTo false
-            resideInPackage("..com..", name) shouldBeEqualTo true
-            resideInPackage("..com", name) shouldBeEqualTo false
-            resideInPackage("com..", name) shouldBeEqualTo true
-            resideInPackage("domain..", name) shouldBeEqualTo false
-            resideInPackage("..domain", name) shouldBeEqualTo false
-            resideInPackage("..domain..", name) shouldBeEqualTo true
-            resideInPackage("usecase..", name) shouldBeEqualTo false
-            resideInPackage("..usecase", name) shouldBeEqualTo true
-            resideInPackage("..usecase..", name) shouldBeEqualTo true
-            resideInPackage("..update", name) shouldBeEqualTo false
-            resideInPackage("update..", name) shouldBeEqualTo false
-            resideInPackage("..update..", name) shouldBeEqualTo true
-            resideInPackage("..nonexisting..", name) shouldBeEqualTo false
+            resideInPackage("com", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..com..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..com", desiredPackage) shouldBeEqualTo false
+            resideInPackage("com..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("domain..", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..domain", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..domain..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("usecase..", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..usecase", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..usecase..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..update", desiredPackage) shouldBeEqualTo false
+            resideInPackage("update..", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..update..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..nonexisting..", desiredPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with one word with given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com", name, '/') shouldBeEqualTo false
-            resideInPackage("..com..", name, '/') shouldBeEqualTo true
-            resideInPackage("..com", name, '/') shouldBeEqualTo false
-            resideInPackage("com..", name, '/') shouldBeEqualTo true
-            resideInPackage("domain..", name, '/') shouldBeEqualTo false
-            resideInPackage("..domain", name, '/') shouldBeEqualTo false
-            resideInPackage("..domain..", name, '/') shouldBeEqualTo true
-            resideInPackage("usecase..", name, '/') shouldBeEqualTo false
-            resideInPackage("..usecase", name, '/') shouldBeEqualTo true
-            resideInPackage("..usecase..", name, '/') shouldBeEqualTo true
-            resideInPackage("..update", name, '/') shouldBeEqualTo false
-            resideInPackage("update..", name, '/') shouldBeEqualTo false
-            resideInPackage("..update..", name, '/') shouldBeEqualTo true
-            resideInPackage("..nonexisting..", name, '/') shouldBeEqualTo false
+            resideInPackage("com", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("..com..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..com", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("com..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("domain..", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("..domain", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("..domain..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("usecase..", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("..usecase", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..usecase..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..update", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("update..", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("..update..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..nonexisting..", desiredPackage, '/') shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment without two dots`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com.domain", name) shouldBeEqualTo false
-            resideInPackage("com.domain.update", name) shouldBeEqualTo false
-            resideInPackage("com.domain.update.usecase", name) shouldBeEqualTo true
-            resideInPackage("com.domain.usecase", name) shouldBeEqualTo false
+            resideInPackage("com.domain", desiredPackage) shouldBeEqualTo false
+            resideInPackage("com.domain.update", desiredPackage) shouldBeEqualTo false
+            resideInPackage("com.domain.update.usecase", desiredPackage) shouldBeEqualTo true
+            resideInPackage("com.domain.usecase", desiredPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment without two dots with given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com/domain", name, '/') shouldBeEqualTo false
-            resideInPackage("com/domain/update", name, '/') shouldBeEqualTo false
-            resideInPackage("com/domain/update/usecase", name, '/') shouldBeEqualTo true
-            resideInPackage("com/domain/usecase", name, '/') shouldBeEqualTo false
+            resideInPackage("com/domain", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("com/domain/update", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("com/domain/update/usecase", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("com/domain/usecase", desiredPackage, '/') shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with first and last word`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com..usecase", name) shouldBeEqualTo true
-            resideInPackage("..com..usecase", name) shouldBeEqualTo true
-            resideInPackage("com..usecase..", name) shouldBeEqualTo true
-            resideInPackage("..com..usecase..", name) shouldBeEqualTo true
+            resideInPackage("com..usecase", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..com..usecase", desiredPackage) shouldBeEqualTo true
+            resideInPackage("com..usecase..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..com..usecase..", desiredPackage) shouldBeEqualTo true
         }
     }
 
     @Test
     fun `has given fragment with first and last word and given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com..usecase", name, '/') shouldBeEqualTo true
-            resideInPackage("..com..usecase", name, '/') shouldBeEqualTo true
-            resideInPackage("com..usecase..", name, '/') shouldBeEqualTo true
-            resideInPackage("..com..usecase..", name, '/') shouldBeEqualTo true
+            resideInPackage("com..usecase", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..com..usecase", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("com..usecase..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..com..usecase..", desiredPackage, '/') shouldBeEqualTo true
         }
     }
 
     @Test
     fun `has given fragment with dots at end`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com.domain..", name) shouldBeEqualTo true
-            resideInPackage("com.domain.update..", name) shouldBeEqualTo true
-            resideInPackage("com.domain.update.usecase..", name) shouldBeEqualTo true
-            resideInPackage("com.domain.usecase..", name) shouldBeEqualTo false
+            resideInPackage("com.domain..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("com.domain.update..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("com.domain.update.usecase..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("com.domain.usecase..", desiredPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with dots at end and given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("com/domain..", name, '/') shouldBeEqualTo true
-            resideInPackage("com/domain/update..", name, '/') shouldBeEqualTo true
-            resideInPackage("com/domain/update/usecase..", name, '/') shouldBeEqualTo true
-            resideInPackage("com/domain/usecase..", name, '/') shouldBeEqualTo false
+            resideInPackage("com/domain..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("com/domain/update..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("com/domain/update/usecase..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("com/domain/usecase..", desiredPackage, '/') shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with dots in few places`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("domain..usecase..", name) shouldBeEqualTo false
-            resideInPackage("com.domain..usecase..", name) shouldBeEqualTo true
-            resideInPackage("..domain..usecase", name) shouldBeEqualTo true
-            resideInPackage("..domain..usecase..", name) shouldBeEqualTo true
-            resideInPackage("..domain..update..", name) shouldBeEqualTo true
-            resideInPackage("com..domain..update..usecase", name) shouldBeEqualTo true
-            resideInPackage("domain..update..usecase", name) shouldBeEqualTo false
-            resideInPackage("..domain.update..", name) shouldBeEqualTo true
+            resideInPackage("domain..usecase..", desiredPackage) shouldBeEqualTo false
+            resideInPackage("com.domain..usecase..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..domain..usecase", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..domain..usecase..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("..domain..update..", desiredPackage) shouldBeEqualTo true
+            resideInPackage("com..domain..update..usecase", desiredPackage) shouldBeEqualTo true
+            resideInPackage("domain..update..usecase", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..domain.update..", desiredPackage) shouldBeEqualTo true
         }
     }
 
     @Test
     fun `has given fragment with dots in few places and given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("domain..usecase..", name, '/') shouldBeEqualTo false
-            resideInPackage("com/domain..usecase..", name, '/') shouldBeEqualTo true
-            resideInPackage("..domain..usecase", name, '/') shouldBeEqualTo true
-            resideInPackage("..domain..usecase..", name, '/') shouldBeEqualTo true
-            resideInPackage("..domain..update..", name, '/') shouldBeEqualTo true
-            resideInPackage("com..domain..update..usecase", name, '/') shouldBeEqualTo true
-            resideInPackage("domain..update..usecase", name, '/') shouldBeEqualTo false
-            resideInPackage("../domain/update..", name, '/') shouldBeEqualTo true
+            resideInPackage("domain..usecase..", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("com/domain..usecase..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..domain..usecase", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..domain..usecase..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("..domain..update..", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("com..domain..update..usecase", desiredPackage, '/') shouldBeEqualTo true
+            resideInPackage("domain..update..usecase", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("../domain/update..", desiredPackage, '/') shouldBeEqualTo true
         }
     }
 
     @Test
     fun `has given fragment with repeating word`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("..domain..domain..update.usecase", name) shouldBeEqualTo false
-            resideInPackage("com..domain..update..com..usecase", name) shouldBeEqualTo false
+            resideInPackage("..domain..domain..update.usecase", desiredPackage) shouldBeEqualTo false
+            resideInPackage("com..domain..update..com..usecase", desiredPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with repeating word and given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("..domain..domain..update/usecase", name, '/') shouldBeEqualTo false
-            resideInPackage("com..domain..update..com..usecase", name, '/') shouldBeEqualTo false
+            resideInPackage("..domain..domain..update/usecase", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("com..domain..update..com..usecase", desiredPackage, '/') shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with the wrong order`() {
         // given
-        val name = "com.domain.update.usecase"
+        val desiredPackage = "com.domain.update.usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("..update..domain..", name) shouldBeEqualTo false
-            resideInPackage("..usecase..domain..", name) shouldBeEqualTo false
+            resideInPackage("..update..domain..", desiredPackage) shouldBeEqualTo false
+            resideInPackage("..usecase..domain..", desiredPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with the wrong order and given separator`() {
         // given
-        val name = "com/domain/update/usecase"
+        val desiredPackage = "com/domain/update/usecase"
         val sut = PackageHelper
 
         // then
         assertSoftly(sut) {
-            resideInPackage("..update..domain..", name, '/') shouldBeEqualTo false
-            resideInPackage("..usecase..domain..", name, '/') shouldBeEqualTo false
+            resideInPackage("..update..domain..", desiredPackage, '/') shouldBeEqualTo false
+            resideInPackage("..usecase..domain..", desiredPackage, '/') shouldBeEqualTo false
         }
     }
 }
