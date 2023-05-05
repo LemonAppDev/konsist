7b223fbae5fd649efed2f43d8aa24833fb0ad9e4
Igor Wojda <igor.wojda@gmail.com>
2023-05-05 10:22:33 +0200

    Update `local.javalibrary.gradle.kts` comment

--- buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
+++ buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 java {
     // Make consist compatible with  Java 1.8
     toolchain {
-        // Java bytecode version to 52.0
+        // Java 8 == bytecode version 52.0
         languageVersion.set(JavaLanguageVersion.of(8))
     }
 
