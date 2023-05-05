e0a1fb424bc4467213ccd7031d66b54ab4d7629c
Igor Wojda <igor.wojda@gmail.com>
2023-05-05 13:01:06 +0200

    Upd Test name

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/verify/PrintDeclarationTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/verify/PrintDeclarationTest.kt
@@ -1,4 +1,4 @@
-package com.lemonappdev.konsist.core
+package com.lemonappdev.konsist.core.verify
 
 import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.ext.sequence.print
