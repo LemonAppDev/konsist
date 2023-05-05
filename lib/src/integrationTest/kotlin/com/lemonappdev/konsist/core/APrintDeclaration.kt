a9f58b407f36fc9ad75750a67c18d4199e7b9e40
Igor Wojda <igor.wojda@gmail.com>
2023-05-05 11:13:51 +0200

    Test Rename

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/APrintDeclaration.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/APrintDeclaration.kt
@@ -1,15 +1,21 @@
 package com.lemonappdev.konsist.core
 
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.sequence.print
+import com.lemonappdev.konsist.api.ext.sequence.withNamePrefix
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
 class APrintDeclaration {
-//    @Test
-//    fun `return value after printDeclaration() is equal to original value`() {
-//        // given
-//        val scope = Konsist.scopeFromPackage("com.lemonappdev.konsist.core.scope.koscope")
-//        val sut = scope
-//            .classes()
-//            .withNamePrefix("KoScopeForC")
-//
-//        // then
-//        sut.print() shouldBeEqualTo sut
-//    }
+    @Test
+    fun `return value after printDeclaration() is equal to original value`() {
+        // given
+        val scope = Konsist.scopeFromPackage("com.lemonappdev.konsist.core.scope.koscope")
+        val sut = scope
+            .classes()
+            .withNamePrefix("KoScopeForC")
+
+        // then
+        sut.print() shouldBeEqualTo sut
+    }
 }
