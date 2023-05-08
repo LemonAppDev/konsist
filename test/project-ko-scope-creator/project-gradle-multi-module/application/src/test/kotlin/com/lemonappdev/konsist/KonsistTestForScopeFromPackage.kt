4ba4fa18cbfee7f4b4748f9a8309226eab26c6fc
Igor Wojda <igor.wojda@gmail.com>
2023-05-08 22:33:05 +0200

    Update package tests

--- test/project-ko-scope-creator/project-gradle-multi-module/application/src/test/kotlin/com/lemonappdev/konsist/KonsistTestForScopeFromPackage.kt
+++ test/project-ko-scope-creator/project-gradle-multi-module/application/src/test/kotlin/com/lemonappdev/konsist/KonsistTestForScopeFromPackage.kt
@@ -154,146 +154,166 @@ class KonsistTestForScopeFromPackage {
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package`() {
+    fun `scopeFromPackage for any__sample__any package`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..")
+            .scopeFromPackage("..sample..")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$applicationMainSourceSetDirectory/sample/AppClass.kt",
                 "$applicationMainSourceSetDirectory/sample/data/AppDataClass.kt",
+                "$applicationTestSourceSetDirectory/sample/AppClassTest.kt",
                 "$applicationTestSourceSetDirectory/sample/data/AppDataClassTest.kt",
+                "$libraryMainSourceSetDirectory/sample/LibClass.kt",
                 "$libraryMainSourceSetDirectory/sample/data/LibDataClass.kt",
+                "$libraryTestSourceSetDirectory/sample/LibClassTest.kt",
                 "$libraryTestSourceSetDirectory/sample/data/LibDataClassTest.kt",
+                "$rootMainSourceSetDirectory/sample/RootClass.kt",
                 "$rootMainSourceSetDirectory/sample/data/RootDataClass.kt",
+                "$rootTestSourceSetDirectory/sample/RootClassTest.kt",
                 "$rootTestSourceSetDirectory/sample/data/RootDataClassTest.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, main source set`() {
+    fun `scopeFromPackage for any__sample__any package, main source set`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", sourceSetName = "main")
+            .scopeFromPackage("..sample..", sourceSetName = "main")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$applicationMainSourceSetDirectory/sample/AppClass.kt",
                 "$applicationMainSourceSetDirectory/sample/data/AppDataClass.kt",
+                "$libraryMainSourceSetDirectory/sample/LibClass.kt",
                 "$libraryMainSourceSetDirectory/sample/data/LibDataClass.kt",
+                "$rootMainSourceSetDirectory/sample/RootClass.kt",
                 "$rootMainSourceSetDirectory/sample/data/RootDataClass.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, test source set`() {
+    fun `scopeFromPackage for any__sample__any package, test source set`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", sourceSetName = "test")
+            .scopeFromPackage("..sample..", sourceSetName = "test")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$applicationTestSourceSetDirectory/sample/AppClassTest.kt",
                 "$applicationTestSourceSetDirectory/sample/data/AppDataClassTest.kt",
+                "$libraryTestSourceSetDirectory/sample/LibClassTest.kt",
                 "$libraryTestSourceSetDirectory/sample/data/LibDataClassTest.kt",
+                "$rootTestSourceSetDirectory/sample/RootClassTest.kt",
                 "$rootTestSourceSetDirectory/sample/data/RootDataClassTest.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, application module`() {
+    fun `scopeFromPackage for any__sample__any package, application module`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", moduleName = "application")
+            .scopeFromPackage("..sample..", moduleName = "application")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$applicationMainSourceSetDirectory/sample/AppClass.kt",
                 "$applicationMainSourceSetDirectory/sample/data/AppDataClass.kt",
+                "$applicationTestSourceSetDirectory/sample/AppClassTest.kt",
                 "$applicationTestSourceSetDirectory/sample/data/AppDataClassTest.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, application module, main source set`() {
+    fun `scopeFromPackage for any__sample__any package, application module, main source set`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", moduleName = "application", sourceSetName = "main")
+            .scopeFromPackage("..sample..", moduleName = "application", sourceSetName = "main")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$applicationMainSourceSetDirectory/sample/AppClass.kt",
                 "$applicationMainSourceSetDirectory/sample/data/AppDataClass.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, application module, test source set`() {
+    fun `scopeFromPackage for any__sample__any package, application module, test source set`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", moduleName = "application", sourceSetName = "test")
+            .scopeFromPackage("..sample..", moduleName = "application", sourceSetName = "test")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$applicationTestSourceSetDirectory/sample/AppClassTest.kt",
                 "$applicationTestSourceSetDirectory/sample/data/AppDataClassTest.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, library module`() {
+    fun `scopeFromPackage for any__sample__any package, library module`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", moduleName = "library")
+            .scopeFromPackage("..sample..", moduleName = "library")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$libraryMainSourceSetDirectory/sample/LibClass.kt",
                 "$libraryMainSourceSetDirectory/sample/data/LibDataClass.kt",
+                "$libraryTestSourceSetDirectory/sample/LibClassTest.kt",
                 "$libraryTestSourceSetDirectory/sample/data/LibDataClassTest.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, library module, main source set`() {
+    fun `scopeFromPackage for any__sample__any package, library module, main source set`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", moduleName = "library", sourceSetName = "main")
+            .scopeFromPackage("..sample..", moduleName = "library", sourceSetName = "main")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$libraryMainSourceSetDirectory/sample/LibClass.kt",
                 "$libraryMainSourceSetDirectory/sample/data/LibDataClass.kt",
             ),
         )
     }
 
     @Test
-    fun `scopeFromPackage for any__data__any package, library module, test source set`() {
+    fun `scopeFromPackage for any__sample__any package, library module, test source set`() {
         // given
         val sut = Konsist
-            .scopeFromPackage("..data..", moduleName = "library", sourceSetName = "test")
+            .scopeFromPackage("..sample..", moduleName = "library", sourceSetName = "test")
             .mapToFilePaths()
 
         // then
         sut.shouldBeEqualTo(
             listOf(
+                "$libraryTestSourceSetDirectory/sample/LibClassTest.kt",
                 "$libraryTestSourceSetDirectory/sample/data/LibDataClassTest.kt",
             ),
         )
