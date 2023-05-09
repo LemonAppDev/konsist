0240f0a23f420f3e85c1499611aac893814a5253
Igor Wojda <igor.wojda@gmail.com>
2023-05-09 09:36:39 +0200

    Add Test for KonsistTestForRootProjectPath

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/filesystem/ProjectRootDirProviderFactory.kt
@@ -0,0 +1,17 @@
+package com.lemonappdev.konsist.core.filesystem
+
+import com.lemonappdev.konsist.core.filesystem.rootprovider.GitProjectRootDirResolver
+import com.lemonappdev.konsist.core.filesystem.rootprovider.GradleProjectRootDirResolver
+import com.lemonappdev.konsist.core.filesystem.rootprovider.MavenProjectRootDirResolver
+
+class ProjectRootDirProviderFactory(
+    pathVerifier: PathVerifier,
+) {
+    private val providers = listOf(
+        GradleProjectRootDirResolver(pathVerifier),
+        MavenProjectRootDirResolver(pathVerifier),
+        GitProjectRootDirResolver(pathVerifier),
+    )
+
+    fun create() = providers
+}
