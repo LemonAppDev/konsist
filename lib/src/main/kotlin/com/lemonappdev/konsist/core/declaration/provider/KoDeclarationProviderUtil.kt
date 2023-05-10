9eacbf5174608a5e641754da3bae450c4f892f5b
natalia.peterwas <n.peterwas@gmail.com>
2023-05-10 09:37:31 +0200

    add more KoNamedDeclarations for getKoDeclaration method

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/provider/KoDeclarationProviderUtil.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/provider/KoDeclarationProviderUtil.kt
@@ -1,32 +1,48 @@
 package com.lemonappdev.konsist.core.declaration.provider
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
 import com.lemonappdev.konsist.api.declaration.KoBaseDeclaration
 import com.lemonappdev.konsist.api.declaration.KoDeclaration
 import com.lemonappdev.konsist.api.declaration.KoFunctionDeclaration
+import com.lemonappdev.konsist.api.declaration.KoNamedDeclaration
+import com.lemonappdev.konsist.core.declaration.KoAnnotationDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoClassDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoCompanionObjectDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoComplexDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoFunctionDeclarationImpl
+import com.lemonappdev.konsist.core.declaration.KoImportDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoInterfaceDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoObjectDeclarationImpl
+import com.lemonappdev.konsist.core.declaration.KoPackageDeclarationImpl
 import com.lemonappdev.konsist.core.declaration.KoPropertyDeclarationImpl
+import com.lemonappdev.konsist.core.declaration.KoTypeAliasDeclarationImpl
 import com.lemonappdev.konsist.core.exception.KoUnsupportedOperationException
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtDeclarationContainer
+import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtImportDirective
+import org.jetbrains.kotlin.psi.KtImportList
 import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.jetbrains.kotlin.psi.KtPackageDirective
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtTypeAlias
 import org.jetbrains.kotlin.psi.psiUtil.getTextWithLocation
 
 internal object KoDeclarationProviderUtil {
-    inline fun <reified T : KoDeclaration> getKoDeclarations(
-        ktDeclarationContainer: KtDeclarationContainer,
+    inline fun <reified T : KoNamedDeclaration> getKoDeclarations(
+        ktElement: KtElement,
         includeNested: Boolean = false,
         includeLocal: Boolean = false,
         parent: KoBaseDeclaration,
     ): Sequence<T> {
-        val declarations = ktDeclarationContainer
-            .declarations
+        val declarations = ktElement
+            .containingFile
+            .children
+            .filterNot { it is PsiWhiteSpace }
+            .flattenDeclarations()
             .mapNotNull {
                 if (it is KtClass && !it.isInterface()) {
                     KoClassDeclarationImpl.getInstance(it, parent)
@@ -40,6 +56,14 @@ internal object KoDeclarationProviderUtil {
                     KoPropertyDeclarationImpl.getInstance(it, parent)
                 } else if (it is KtFunction) {
                     KoFunctionDeclarationImpl.getInstance(it, parent)
+                } else if (it is KtImportDirective) {
+                    KoImportDeclarationImpl.getInstance(it, parent)
+                } else if (it is KtPackageDirective) {
+                    KoPackageDeclarationImpl.getInstance(it, parent)
+                } else if (it is KtAnnotationEntry){
+                    KoAnnotationDeclarationImpl.getInstance(it, parent)
+                }  else if (it is KtTypeAlias){
+                    KoTypeAliasDeclarationImpl.getInstance(it, parent)
                 } else {
                     throw KoUnsupportedOperationException("Unknown declaration type: ${it.getTextWithLocation()}")
                 }
@@ -49,8 +73,8 @@ internal object KoDeclarationProviderUtil {
         return getKoDeclarations(declarations, includeNested, includeLocal)
     }
 
-    inline fun <reified T : KoDeclaration> getKoDeclarations(
-        declarations: Sequence<KoDeclaration>,
+    inline fun <reified T : KoNamedDeclaration> getKoDeclarations(
+        declarations: Sequence<KoNamedDeclaration>,
         includeNested: Boolean = false,
         includeLocal: Boolean = false,
     ): Sequence<T> {
@@ -89,9 +113,9 @@ internal object KoDeclarationProviderUtil {
         return result.filterIsInstance<T>()
     }
 
-    fun localDeclarations(koFunctions: Sequence<KoFunctionDeclaration>): Sequence<KoDeclaration> {
-        val localDeclarations = mutableListOf<KoDeclaration>()
-        val nestedDeclarations = mutableListOf<KoDeclaration>()
+    fun localDeclarations(koFunctions: Sequence<KoFunctionDeclaration>): Sequence<KoNamedDeclaration> {
+        val localDeclarations = mutableListOf<KoNamedDeclaration>()
+        val nestedDeclarations = mutableListOf<KoNamedDeclaration>()
 
         koFunctions.forEach { koFunction ->
             koFunction.localDeclarations().forEach {
@@ -104,5 +128,13 @@ internal object KoDeclarationProviderUtil {
         }
 
         return localDeclarations.asSequence()
+    }
+
+    private fun List<PsiElement>.flattenDeclarations() = this.flatMap {
+        if (it is KtImportList) {
+            it.imports
+        } else {
+            listOf(it)
+        }
     }
 }
