9259e17bb86a043a53a1c59a8dc8d3ec966b5e6e
natalia.peterwas <n.peterwas@gmail.com>
2023-05-11 14:15:00 +0200

    add test in KonsistTestForScopeFromModule

--- test/konsist-scope-creation/app/src/integrationTest/kotlin/com/lemonappdev/konsist/KonsistTestForScopeFromModule.kt
+++ test/konsist-scope-creation/app/src/integrationTest/kotlin/com/lemonappdev/konsist/KonsistTestForScopeFromModule.kt
@@ -59,4 +59,40 @@ class KonsistTestForScopeFromModule {
             ),
         )
     }
+
+    @Test
+    fun `scopeFromModule for app and data modules`() {
+        // given
+        val sut = Konsist
+            .scopeFromModule("app", "data")
+            .mapToFilePaths()
+
+        // then
+        sut.shouldBeEqualTo(
+            listOf(
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForRootProjectPath.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromDirectory.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromFile.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromModule.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromPackage.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromProduction.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromProject.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromProjectDirectory.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromProjectFile.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromSourceSet.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForScopeFromTest.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/KonsistTestForToString.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/ext/KoScopeExt.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/util/PathProvider.kt",
+                "$appIntegrationTestSourceSetDirectory/sample/AppClassTest.kt",
+                "$appIntegrationTestSourceSetDirectory/sample/data/AppDataClassTest.kt",
+                "$appMainSourceSetDirectory/sample/AppClass.kt",
+                "$appMainSourceSetDirectory/sample/data/AppDataClass.kt",
+                "$dataMainSourceSetDirectory/sample/LibClass.kt",
+                "$dataMainSourceSetDirectory/sample/data/LibDataClass.kt",
+                "$dataTestSourceSetDirectory/sample/LibClassTest.kt",
+                "$dataTestSourceSetDirectory/sample/data/LibDataClassTest.kt",
+            ),
+        )
+    }
 }
