87c9e62ba37290d03b8ccf2f90cd8a1a4e7e0ba1
natalia.peterwas <n.peterwas@gmail.com>
2023-05-11 14:51:29 +0200

    remove unnecessary extensions

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/sequence/KoObjectDeclarationSequenceExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/sequence/KoObjectDeclarationSequenceExt.kt
@@ -21,29 +21,3 @@ fun Sequence<KoObjectDeclaration>.withCompanionModifier(): Sequence<KoObjectDecl
  * Sequence containing all declarations that don't have 'companion' modifier.
  */
 fun Sequence<KoObjectDeclaration>.withoutCompanionModifier(): Sequence<KoObjectDeclaration> = filterNot { it.hasCompanionModifier() }
-
-/**
- * Sequence containing declarations that have 'companion' modifier and not defaulted name.
- */
-fun Sequence<KoObjectDeclaration>.withNamedCompanionObject(): Sequence<KoObjectDeclaration> =
-    filter { it.hasCompanionModifier() && it.name != "Companion" }
-
-/**
- * Sequence containing declarations that have 'companion' modifier and defaulted name.
- */
-fun Sequence<KoObjectDeclaration>.withoutNamedCompanionObject(): Sequence<KoObjectDeclaration> =
-    filter { it.hasCompanionModifier() && it.name == "Companion" }
-
-/**
- * Sequence containing declarations that have 'companion' modifier and name.
- */
-fun Sequence<KoObjectDeclaration>.withNamedCompanionObjectAs(vararg names: String): Sequence<KoObjectDeclaration> = filter {
-    it.hasCompanionModifier() && names.any { name -> it.name == name }
-}
-
-/**
- * Sequence containing declarations that have 'companion' modifier and don't have name.
- */
-fun Sequence<KoObjectDeclaration>.withoutNamedCompanionObjectAs(vararg names: String): Sequence<KoObjectDeclaration> = filter {
-    it.hasCompanionModifier() && names.none { name -> it.name == name }
-}
