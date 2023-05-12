43d6f792f0195d65c1b5606b61fa3227f7650eb3
natalia.peterwas <n.peterwas@gmail.com>
2023-05-12 13:22:18 +0200

    KoPackageDeclaration

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kopackagedeclaration/KoPackageDeclarationForNameTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kopackagedeclaration/KoPackageDeclarationForNameTest.kt
@@ -4,7 +4,7 @@ import com.lemonappdev.konsist.TestSnippetProvider
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
-class KoPackageDeclarationTest {
+class KoPackageDeclarationForNameTest {
     @Test
     fun `package-has-name`() {
         // given
