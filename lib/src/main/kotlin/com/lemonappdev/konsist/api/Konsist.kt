2ac5cdb467b5ee0a6b8ab9906efcbf3e6691f95f
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-05-12 09:34:35 +0200

    KON-125 Check If KoPsiDeclarationImpl.projectFilePath Can Be Retrieved By Using PathProvider (#150)
    * change package for KoScopeCreatorImpl
    
    * add PathProvider.getInstance()

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/Konsist.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/Konsist.kt
@@ -1,6 +1,6 @@
 package com.lemonappdev.konsist.api
 
-import com.lemonappdev.konsist.core.KoScopeCreatorImpl
+import com.lemonappdev.konsist.core.scope.KoScopeCreatorImpl
 
 /**
  * Represents the Konsist API. This is the main entry point to the Konist library.
