156effb0521e61f861a12b5de7b174099816570b
natalia.peterwas <n.peterwas@gmail.com>
2023-05-16 18:10:06 +0200

    remove import

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koannotationdeclaration/KoAnnotationDeclarationForRepresentsTypeTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koannotationdeclaration/KoAnnotationDeclarationForRepresentsTypeTest.kt
@@ -5,7 +5,6 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
-import sun.jvm.hotspot.oops.CellTypeState.value
 
 class KoAnnotationDeclarationForRepresentsTypeTest {
     @ParameterizedTest
