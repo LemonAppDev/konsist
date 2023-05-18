8620586acad8100f40318f95f34d42be888b028c
natalia.peterwas <n.peterwas@gmail.com>
2023-05-18 11:32:37 +0200

    add tests for KoSecondaryConstructorForHasValidKDocTest

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kosecondaryconstructor/KoSecondaryConstructorForHasValidKDocTest.kt
@@ -0,0 +1,50 @@
+package com.lemonappdev.konsist.core.declaration.kosecondaryconstructor
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class KoSecondaryConstructorForHasValidKDocTest {
+    @ParameterizedTest
+    @MethodSource("provideValues")
+    fun `class-with-kdoc-with-param-tags`(
+        fileName: String,
+        verifyParamTag: Boolean,
+        value: Boolean,
+    ) {
+        // given
+        val sut = getSnippetFile(fileName)
+            .classes()
+            .first()
+            .secondaryConstructors
+            .first()
+
+        // then
+        sut
+            .hasValidKDoc(
+                verifyDescription = false,
+                verifyParamTag = verifyParamTag,
+            )
+            .shouldBeEqualTo(value)
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/kosecondaryconstructor/snippet/forhasvalidkdoc/", fileName)
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun provideValues() = listOf(
+            arguments("secondary-constructor-with-kdoc-with-complete-param-tags", true, true),
+            arguments("secondary-constructor-with-kdoc-with-complete-param-tags", false, true),
+            arguments("secondary-constructor-with-kdoc-with-not-complete-param-tags", true, false),
+            arguments("secondary-constructor-with-kdoc-with-not-complete-param-tags", false, true),
+            arguments("secondary-constructor-with-kdoc-without-param-tags", true, false),
+            arguments("secondary-constructor-with-kdoc-without-param-tags", false, true),
+        )
+    }
+}
