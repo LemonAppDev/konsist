89941be673370eff70a0109fbf5e10211d550aec
Igor Wojda <igor.wojda@gmail.com>
2023-05-18 13:15:34 +0200

    Upd docs

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/Konsist.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/Konsist.kt
@@ -4,5 +4,7 @@ import com.lemonappdev.konsist.core.scope.KoScopeCreatorImpl
 
 /**
  * Represents the Konsist API. This is the main entry point to the Konist library.
+ *
+ * It allows to create a [KoScope] instance from the given set of files such as all project files, single module, path etc.
  */
 object Konsist : KoScopeCreator by KoScopeCreatorImpl()
