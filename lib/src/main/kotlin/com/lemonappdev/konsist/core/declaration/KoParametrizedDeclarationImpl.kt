613e37c465b957d42a0a652ac941706afad7b75a
Igor Wojda <igor.wojda@gmail.com>
2023-05-19 14:24:45 +0200

    KON-163 Make Parameters List Non Nullable (#164)
    * Make parameters list non-nullable
    
    * Fix integration tests

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoParametrizedDeclarationImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoParametrizedDeclarationImpl.kt
@@ -14,13 +14,12 @@ internal abstract class KoParametrizedDeclarationImpl(
         ktFunction
             .valueParameters
             .map { KoParameterDeclarationImpl.getInstance(it, this) }
-            .ifEmpty { null }
     }
 
     override fun hasValidParamTag(enabled: Boolean) = TagHelper.hasValidParamTag(enabled, parameters, kDoc)
 
     override fun hasParameterNamed(name: String?) = when (name) {
-        null -> parameters != null
-        else -> parameters?.firstOrNull()?.name == name
+        null -> true
+        else -> parameters.firstOrNull()?.name == name
     }
 }
