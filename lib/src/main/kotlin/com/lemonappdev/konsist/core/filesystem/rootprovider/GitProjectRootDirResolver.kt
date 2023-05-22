ef56e2a026a1279f40c390216a7b32ec04a7df76
Igor Wojda <igor.wojda@gmail.com>
2023-05-22 17:10:22 +0200

    Upgrade comment

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/filesystem/rootprovider/GitProjectRootDirResolver.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/filesystem/rootprovider/GitProjectRootDirResolver.kt
@@ -6,7 +6,9 @@ class GitProjectRootDirResolver(
     pathVerifier: PathVerifier,
 ) : ProjectRootDirResolver(pathVerifier) {
     /*
-    Random files and directory selected from fresh '.git' repository (creates using the 'git init' command)
+    A few files and directories selected from fresh '.git' directory. This directory stores config files for the git repository
+    (created using the 'git init' command).
+
     Description of the '.git' directory https://githowto.com/git_internals_git_directory
      */
     override val paths = setOf(
