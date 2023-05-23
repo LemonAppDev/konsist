00551c1941cbcb4f1454486b2fb91c76f4af18cd
Igor Wojda <igor.wojda@gmail.com>
2023-05-23 18:17:52 +0200

    Add starter projects

---
+++ samples/starter-projects/konsist-starter-android-gradle-kotlin-junit-5/konsistTest/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("de.mannodermaus.android-junit5") version "1.9.3.0"
+}
+
+android {
+    namespace = "com.sample"
+
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+//kotlin {
+//    jvmToolchain {
+//        languageVersion.set(JavaLanguageVersion.of(19))
+//    }
+//}
+
+dependencies {
+    implementation(project(":app"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+
+    testImplementation("com.lemonappdev:konsist:0.7.12")
+}
---
+++ samples/starter-projects/konsist-starter-spring-gradle-kotlin-junit-5/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "konsist-starter-spring-gradle-kotlin-junit-5"
