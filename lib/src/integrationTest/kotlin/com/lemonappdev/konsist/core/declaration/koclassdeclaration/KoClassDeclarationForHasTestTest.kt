16fb4f9ff985537141c786a9dcd544e68c6353d8
natalia.peterwas <n.peterwas@gmail.com>
2023-05-23 09:26:21 +0200

    fix spotless and detekt

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koclassdeclaration/KoClassDeclarationForHasTestTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koclassdeclaration/KoClassDeclarationForHasTestTest.kt
@@ -1,12 +1,5 @@
 package com.lemonappdev.konsist.core.declaration.koclassdeclaration
 
-import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
-import org.amshove.kluent.shouldBeEqualTo
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
-
 class KoClassDeclarationForHasTestTest {
     // Konsist koClassDeclaration.hasTest() tests are implemented in the test\konsist-path-tester project.
 }
