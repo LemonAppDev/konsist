a883286e59407825e978d9db4e1012d5b637e158
Igor Wojda <igor.wojda@gmail.com>
2023-05-24 11:03:01 +0200

    Configure checks for maven project

---
+++ scripts/replace-konsist-version/add-maven-local-repository-to-maven-config-file.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+
+def insert_maven_local(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    # Namespace map to handle namespaces in pom.xml
+    namespaces = {'default': 'http://maven.apache.org/POM/4.0.0'}
+    ET.register_namespace('', namespaces['default'])
+
+    # Find the <repositories> element, create it if not exist
+    repositories = root.find('default:repositories', namespaces)
+    if repositories is None:
+        repositories = ET.SubElement(root, 'repositories')
+
+    # Create new <repository> element
+    repository = ET.SubElement(repositories, 'repository')
+
+    id_ = ET.SubElement(repository, 'id')
+    id_.text = 'maven-local'
+    url = ET.SubElement(repository, 'url')
+    url.text = 'file://${user.home}/.m2/repository'
+
+    tree.write(filename)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", help="The path to the file to be modified.")
+    args = parser.parse_args()
+
+    konsist_version = insert_maven_local(args.file_path)
