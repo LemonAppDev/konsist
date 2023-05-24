bdefcd9c55adbfa2847daea8412199d72e8fe2e7
Igor Wojda <igor.wojda@gmail.com>
2023-05-24 14:29:45 +0200

    Upd tests

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,6 +7,11 @@ dependencies {
     implementation(libs.kotlin.stdlib.jdk8)
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.compiler)
+
+    implementation(libs.junitJupiterEngine)
+    implementation(libs.junitJupiterParams)
+    implementation(libs.mockk)
+    implementation(libs.kluent)
 }
 
 @Suppress("UnstableApiUsage")
@@ -21,13 +26,13 @@ testing {
             dependencies {
                 implementation(project(":lib"))
                 implementation(libs.mockk)
+                implementation(libs.kluent)
             }
         }
 
         register("konsistTest", JvmTestSuite::class) {
             dependencies {
                 implementation(project(":lib"))
-                implementation(libs.kluent)
             }
         }
     }
