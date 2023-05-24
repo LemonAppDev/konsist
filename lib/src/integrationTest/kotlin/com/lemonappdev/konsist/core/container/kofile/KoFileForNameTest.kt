66c5c601b2096dd14bec5d78dab438e82ddd424b
natalia.peterwas <n.peterwas@gmail.com>
2023-05-24 11:57:07 +0200

    add tests for KoFile.name

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/container/kofile/KoFileForNameTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/container/kofile/KoFileForNameTest.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.core.container.kofile
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
@@ -14,6 +15,62 @@ class KoFileForNameTest {
 
         // then
         sut.name shouldBeEqualTo "file-name.kt"
+    }
+
+    @Test
+    fun `file-has-name-with-prefix`() {
+        // given
+        val sut = getSnippetFile("file-has-name-with-prefix")
+            .files()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasNameStartingWith("file-has-name") shouldBeEqualTo true
+            hasNameStartingWith("wrong-prefix") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `file-has-name-with-suffix`() {
+        // given
+        val sut = getSnippetFile("file-has-name-with-suffix")
+            .files()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasNameEndingWith("with-suffix.kt") shouldBeEqualTo true
+            hasNameEndingWith("with-suffix") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `file-has-name-containing-text`() {
+        // given
+        val sut = getSnippetFile("file-has-name-containing-text")
+            .files()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasNameContaining("name-containing") shouldBeEqualTo true
+            hasNameContaining("not-containing") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `file-has-name-matching-regex`() {
+        // given
+        val sut = getSnippetFile("file-has-name-matching-regex")
+            .files()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasNameMatching(Regex("^[a-zA-Z[^0-9]]+")) shouldBeEqualTo true
+            hasNameMatching(Regex("[0-9]+")) shouldBeEqualTo false
+        }
     }
 
     private fun getSnippetFile(fileName: String) = getSnippetKoScope("core/container/kofile/snippet/forname/", fileName)
