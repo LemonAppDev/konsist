af3e1a3490b8e06b2122e970f8f32163b818251c
natalia.peterwas <n.peterwas@gmail.com>
2023-05-25 12:40:27 +0200

    fix tests

--- test-projects/konsist-path-tester/app/src/integrationTest/kotlin/com/lemonappdev/konsist/scope/from/KonsistScopeFromSourceSetTest.kt
+++ test-projects/konsist-path-tester/app/src/integrationTest/kotlin/com/lemonappdev/konsist/scope/from/KonsistScopeFromSourceSetTest.kt
@@ -45,6 +45,7 @@ class KonsistScopeFromSourceSetTest {
                 "$appIntegrationTestSourceSetDirectory/konsist/helper/ext/KoScopeExt.kt",
                 "$appIntegrationTestSourceSetDirectory/konsist/helper/util/PathProvider.kt",
                 "$appIntegrationTestSourceSetDirectory/konsist/koclass/KoClassDeclarationForHasTestTest.kt",
+                "$appIntegrationTestSourceSetDirectory/konsist/kofile/KoFileForContainingSourceSetName.kt",
                 "$appIntegrationTestSourceSetDirectory/konsist/scope/KonsistOperatorTest.kt",
                 "$appIntegrationTestSourceSetDirectory/konsist/scope/KonsistSliceTest.kt",
                 "$appIntegrationTestSourceSetDirectory/konsist/scope/KonsistToStringTest.kt",
