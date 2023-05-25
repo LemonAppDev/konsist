60f04467321cf11a199df9198392fc349f27df08
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-05-25 09:17:16 +0200

    Merge pull request #192
    * remove unnecessary question mark
    
    * add comment for all tests
    
    * add new test classes
    
    * divide KoPsiDeclarationForKDocTest into smaller parts
    
    * rename test names

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kopsideclaration/forhasvalidkdoc/KoPsiDeclarationForHasValidKDocVerifySampleTagTest.kt
@@ -0,0 +1,42 @@
+package com.lemonappdev.konsist.core.declaration.kopsideclaration.forhasvalidkdoc
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class KoPsiDeclarationForHasValidKDocVerifySampleTagTest {
+
+    // We used interface in snippet, because it doesn't override any method used in hasValidKDoc().
+    // It may changed in the future.
+    @ParameterizedTest
+    @MethodSource("provideValues")
+    fun `hasValidKDoc-with-sample-tag`(
+        fileName: String,
+        verifySampleTag: Boolean,
+        value: Boolean,
+    ) {
+        // given
+        val sut = getSnippetFile(fileName)
+            .declarations()
+            .first()
+
+        // then
+        sut.hasValidKDoc(verifyDescription = false, verifySampleTag = verifySampleTag) shouldBeEqualTo value
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/kopsideclaration/snippet/forhasvalidkdoc/forsampletag/", fileName)
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun provideValues() = listOf(
+            arguments("declaration-with-kdoc-with-sample-tag", true, true),
+            arguments("declaration-with-kdoc-with-sample-tag", false, true),
+            arguments("declaration-with-kdoc-without-sample-tag", true, false),
+            arguments("declaration-with-kdoc-without-sample-tag", false, true),
+        )
+    }
+}
