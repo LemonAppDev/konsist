9ae814aa485dc4ba36312c6a36870525b65fd2fd
natalia.peterwas <n.peterwas@gmail.com>
2023-05-25 12:08:36 +0200

    add tests in konsist-path-tester

--- test-projects/konsist-path-tester/app/src/integrationTest/kotlin/com/lemonappdev/konsist/kofile/KoFileForContainingSourceSetName.kt
+++ test-projects/konsist-path-tester/app/src/integrationTest/kotlin/com/lemonappdev/konsist/kofile/KoFileForContainingSourceSetName.kt
@@ -1,6 +1,12 @@
 package com.lemonappdev.konsist.kofile
 
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.helper.util.PathProvider.appIntegrationTestSourceSetDirectory
+import com.lemonappdev.konsist.helper.util.PathProvider.appMainSourceSetDirectory
+import com.lemonappdev.konsist.helper.util.PathProvider.dataMainSourceSetDirectory
+import com.lemonappdev.konsist.helper.util.PathProvider.dataTestSourceSetDirectory
+import com.lemonappdev.konsist.helper.util.PathProvider.rootMainSourceSetDirectory
+import com.lemonappdev.konsist.helper.util.PathProvider.projectRootDirectory
 import com.lemonappdev.konsist.helper.ext.mapToFilePaths
 import com.lemonappdev.konsist.helper.util.PathProvider
 import org.amshove.kluent.shouldBeEqualTo
@@ -8,10 +14,10 @@ import org.junit.jupiter.api.Test
 
 class KoFileForContainingSourceSetName {
     @Test
-    fun `containing source set name is 'main'`() {
+    fun `containing source set name is 'main' in app module`() {
         // given
         val sut = Konsist
-            .scopeFromFile("/sample/AppClass.kt")
+            .scopeFromFile("$appMainSourceSetDirectory/sample/AppClass.kt", absolutePath = true)
             .files()
             .first()
 
@@ -19,4 +25,51 @@ class KoFileForContainingSourceSetName {
         sut.containingSourceSetName shouldBeEqualTo "main"
     }
 
+    @Test
+    fun `containing source set name is 'integrationTest' in app module`() {
+        // given
+        val sut = Konsist
+            .scopeFromFile("$appIntegrationTestSourceSetDirectory/sample/AppClassTest.kt", absolutePath = true)
+            .files()
+            .first()
+
+        // then
+        sut.containingSourceSetName shouldBeEqualTo "integrationTest"
+    }
+
+    @Test
+    fun `containing source set name is 'main' in data module`() {
+        // given
+        val sut = Konsist
+            .scopeFromFile("$dataMainSourceSetDirectory/sample/LibClass.kt", absolutePath = true)
+            .files()
+            .first()
+
+        // then
+        sut.containingSourceSetName shouldBeEqualTo "main"
+    }
+
+    @Test
+    fun `containing source set name is 'test' in data module`() {
+        // given
+        val sut = Konsist
+            .scopeFromFile("$dataTestSourceSetDirectory/sample/LibClassTest.kt", absolutePath = true)
+            .files()
+            .first()
+
+        // then
+        sut.containingSourceSetName shouldBeEqualTo "test"
+    }
+
+    @Test
+    fun `containing source set name is 'main' in root module`() {
+        // given
+        val sut = Konsist
+            .scopeFromFile("$rootMainSourceSetDirectory/sample/RootClass.kt", absolutePath = true)
+            .files()
+            .first()
+
+        // then
+        sut.containingSourceSetName shouldBeEqualTo "main"
+    }
 }
