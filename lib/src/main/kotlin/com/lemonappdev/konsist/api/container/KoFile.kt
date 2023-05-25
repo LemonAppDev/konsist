cfdbda208fbca7099169a178639a934b157f8e63
natalia.peterwas <n.peterwas@gmail.com>
2023-05-25 14:05:35 +0200

    upd kDoc

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/container/KoFile.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/container/KoFile.kt
@@ -116,7 +116,7 @@ interface KoFile :
     /**
      * Whether file reside in module.
      *
-     * @param module The name of the module to check.
+     * @param module The name of the module to check. If this is the top-module, use "root".
      * @return `true` if a file resides in the specified module, `false` otherwise.
      */
     fun resideInModule(module: String): Boolean
