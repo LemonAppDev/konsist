02a9cbe574d22dcd3b4d0f0ac3978084cf932278
natalia.peterwas <n.peterwas@gmail.com>
2023-05-29 08:38:26 +0200

    add new test in SnippetTest

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/snippet/SnippetTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/snippet/SnippetTest.kt
@@ -4,6 +4,7 @@ import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.io.IOException
 
 class SnippetTest {
     @Test
@@ -42,12 +43,26 @@ class SnippetTest {
     @Test
     fun `every snippet has valid code`() {
         // given
-        val snippets = File("../")
-            .walk()
-            .filter { it.isKotlinSnippetFile }
-            .map { it.path.replace("kttxt", "kt") }
+        val map = mutableMapOf<String, String>()
 
-        val builder = ProcessBuilder()
+        val filePaths = File("/Users/natalia/IdeaProjects/konsist/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/container/kofile/snippet")
+            .walk()
+            .filter { it.isFile && it.name.endsWith(".kttxt") }
+            .map { it.path.replace("kttxt", "kt") }
+            .toList()
+
+
+        val texts = File("/Users/natalia/IdeaProjects/konsist/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/container/kofile/snippet")
+            .walk()
+            .filter { it.isFile && it.name.endsWith(".kttxt") }
+            .map { it.readText() }
+            .toList()
+
+        filePaths.forEachIndexed { index, s -> map[s] = texts[index] }
+        val sut = validateKotlinCode(map)
+
+        // then
+        sut shouldBeEqualTo 0
 
     }
 
@@ -64,5 +79,33 @@ class SnippetTest {
     companion object {
         private val File.isKotlinSnippetFile: Boolean get() = isFile && name.endsWith(".kttxt")
         private val File.isKotlinNotSnippetFile: Boolean get() = isFile && !name.endsWith(".kttxt")
+    }
+
+    private fun validateKotlinCode(map: Map<String, String>): Int {
+        var counter = 0
+        val file = File("snippet test.kt")
+
+        map.forEach {
+            file.writeText(it.value)
+
+            try {
+                val processBuilder = ProcessBuilder("kotlinc", file.path)
+                val process = processBuilder.start()
+
+                val errorOutput = process.errorStream.bufferedReader().use { reader -> reader.readText() }
+                if (errorOutput.isNotEmpty()) {
+                    counter++
+                    println(it.key)
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                counter++
+                println(it.key)
+            }
+            file.writeText("")
+        }
+
+        file.delete()
+        return counter
     }
 }
