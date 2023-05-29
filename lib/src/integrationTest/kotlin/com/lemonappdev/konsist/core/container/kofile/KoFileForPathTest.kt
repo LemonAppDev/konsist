f27c34cd16045917c421413233de1a048eb2166f
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-05-29 11:26:22 +0200

    remove `run` from tests (#201)

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/container/kofile/KoFileForPathTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/container/kofile/KoFileForPathTest.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.core.container.kofile
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
@@ -13,12 +14,10 @@ class KoFileForPathTest {
             .first()
 
         // then
-        sut
-            .path
-            .run {
-                startsWith("//") shouldBeEqualTo false
-                endsWith("kofile/snippet/forpath/file-path.kt") shouldBeEqualTo true
-            }
+        assertSoftly(sut.path) {
+            startsWith("//") shouldBeEqualTo false
+            endsWith("kofile/snippet/forpath/file-path.kt") shouldBeEqualTo true
+        }
     }
 
     @Test
