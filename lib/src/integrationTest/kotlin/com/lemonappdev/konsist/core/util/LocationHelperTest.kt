ceae4d33bc4d9e655f84acbb7426b3dbfa00c9ec
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-05-30 12:39:14 +0200

    KON-191 Add New Test To LocationHelperTest (#205)
    * add missing test
    
    * add requirements to LoCationHelper
    
    * Update LocationHelper.kt
    
    ---------
    
    Co-authored-by: Igor Wojda <igor.wojda@gmail.com>

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/util/LocationHelperTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/util/LocationHelperTest.kt
@@ -1,10 +1,51 @@
 package com.lemonappdev.konsist.core.util
 
+import com.lemonappdev.konsist.core.util.LocationHelper.resideInLocation
 import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldThrow
+import org.amshove.kluent.withMessage
 import org.junit.jupiter.api.Test
 
 class LocationHelperTest {
+    @Test
+    fun `reside in any package`() {
+        // given
+        val desiredPackage1 = "com.domain.update.usecase"
+        val desiredPackage2 = "com.domain"
+        val desiredPackage3 = "org.example.project"
+        val sut = LocationHelper
+
+        // then
+        assertSoftly(sut) {
+            resideInLocation("..", desiredPackage1) shouldBeEqualTo true
+            resideInLocation("..", desiredPackage2) shouldBeEqualTo true
+            resideInLocation("..", desiredPackage3) shouldBeEqualTo true
+        }
+    }
+
+    @Test
+    fun `throws exception when desired location is empty`() {
+        // given
+        val desiredPackage = "com.domain.update.usecase"
+        val sut = LocationHelper
+
+        // then
+        val actual = { sut.resideInLocation("", desiredPackage) }
+        actual shouldThrow IllegalArgumentException::class withMessage "Location name is empty"
+    }
+
+    @Test
+    fun `throws exception when desired location is single dot`() {
+        // given
+        val desiredPackage = "com.domain.update.usecase"
+        val sut = LocationHelper
+
+        // then
+        val actual = { sut.resideInLocation(".", desiredPackage) }
+        actual shouldThrow IllegalArgumentException::class withMessage "Incorrect location selector format: ."
+    }
+
     @Test
     fun `has given fragment with one word`() {
         // given
