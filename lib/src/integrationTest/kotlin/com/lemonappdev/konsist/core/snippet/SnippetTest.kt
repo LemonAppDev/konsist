94568d868a1bc67c2a46985444e8e3e4edea24ff
natalia.peterwas <n.peterwas@gmail.com>
2023-06-01 12:42:54 +0200

    upd test (it works but is too slow)

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/snippet/SnippetTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/snippet/SnippetTest.kt
@@ -6,8 +6,9 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 import java.io.BufferedReader
 import java.io.File
-import java.io.IOException
 import java.io.InputStreamReader
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class SnippetTest {
     @Test
@@ -44,30 +45,6 @@ class SnippetTest {
         }
     }
 
-    @Test
-    fun `every snippet has valid code`() {
-        // given
-        val map = mutableMapOf<File, String>()
-
-        val filePaths = File("src/integrationTest/..")
-            .walk()
-            .filter { it.isKotlinSnippetFile }
-
-        val texts = File("src/integrationTest/..")
-            .walk()
-            .filter { it.isKotlinSnippetFile }
-            .map { it.readText() }
-            .toList()
-
-        filePaths.forEachIndexed { index, s -> map[s] = texts[index] }
-
-        // when
-        val sut = validateKotlinCode(map)
-
-        // then
-        sut shouldBeEqualTo 0
-    }
-
     private fun snippetNamesFromFiles(regex: Regex, prefix: String, suffix: String) =
         File("..$sep")
             .walk()
@@ -80,56 +57,58 @@ class SnippetTest {
             .map { it.removePrefix("\"") }
             .map { it.removeSuffix(suffix) }
 
-    private fun validateKotlinCode(map: Map<File, String>): Int {
-        var counter = 0
-        val commands1 = listOf(
+    @Test
+    fun `every snippet has valid code`() {
+        // given
+        val files = File("src${sep}integrationTest$sep..")
+            .walk()
+            .filter { it.isKotlinSnippetFile }
+
+        // then
+        val sut = validateKotlinCode(files)
+        sut shouldBeEqualTo ""
+    }
+
+    private fun validateKotlinCode(files: Sequence<File>): String {
+        val commandConvertingTestDataToJar = listOf(
             "kotlinc",
-            File("src/integrationTest/kotlin/com/lemonappdev/konsist/testdata/TestData.kt").absolutePath,
+            File("src${sep}integrationTest${sep}kotlin${sep}com${sep}lemonappdev${sep}konsist${sep}testdata${sep}TestData.kt").absolutePath,
             "-include-runtime",
             "-d",
             "test.jar",
         )
 
-        val builder = ProcessBuilder(commands1)
-        builder.redirectErrorStream(true)
+        val builderTestDataJar = ProcessBuilder(commandConvertingTestDataToJar)
+        builderTestDataJar.redirectErrorStream(true)
 
-        val process1 = builder.start().waitFor()
-        require(process1 == 0) { "TestData is invalid file." }
+        val processConvertingTestDataToJar = builderTestDataJar
+            .start()
+            .waitFor()
 
-        val file = File("build/snippet-test.test-snippet.kt")
+        require(processConvertingTestDataToJar == 0) { "TestData is invalid file." }
 
-        map.forEach {
-            file.writeText(it.value)
+        Files.createDirectory(Paths.get("build").resolve("snippet-test$sep"))
 
-            val command2 = listOf(
-                "kotlinc",
-                "-cp",
-                "test.jar",
-                "-nowarn",
-                file.absolutePath,
-            )
+        var commands = ""
 
-            try {
-                val processBuilder = ProcessBuilder(command2)
-                processBuilder.redirectErrorStream(false)
-                val process = processBuilder.start()
+        files.forEach {
+            val snippetFileWithKt = it.copyTo(File("build${sep}snippet-test$sep${it.path.replace("kttxt", "kt")}"))
 
-                val errorReader = BufferedReader(InputStreamReader(process.errorStream))
-                val errorOutput = errorReader.readText()
-                if (errorOutput.isNotEmpty()) {
-                    counter++
-                    println("${it.key} \n $errorOutput")
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-                println(it.key)
-            }
-
-            file.delete()
-
-            File("com/").deleteRecursively()
-            File("META-INF/").deleteRecursively()
+            commands += "kotlinc -cp test.jar -nowarn ${snippetFileWithKt.absolutePath};"
         }
+
+        val processBuilder = ProcessBuilder("bash", "-c", commands)
+        processBuilder.redirectErrorStream(false)
+
+        val process = processBuilder.start()
+
+        val errorReader = BufferedReader(InputStreamReader(process.errorStream))
+
+        val errorOutput = errorReader.readText()
+
+        File("com$sep").deleteRecursively()
+        File("META-INF$sep").deleteRecursively()
+        File("build${sep}snippet-test").deleteRecursively()
 
         ProcessBuilder(
             "git",
@@ -137,7 +116,7 @@ class SnippetTest {
             "-f",
         ).start()
 
-        return counter
+        return errorOutput
     }
 
     companion object {
