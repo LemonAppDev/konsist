c0e1717a1bc743e86d602be8723ef5be67d1210a
natalia.peterwas <n.peterwas@gmail.com>
2023-06-05 08:52:35 +0200

    add new python test

---
+++ scripts/verify_snippet_valid_code.py
@@ -0,0 +1,89 @@
+import subprocess
+import shutil
+import os
+import glob
+import time
+
+script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(script_path))
+
+# convert TestData to jar. extension
+commandConvertingTestDataToJar = [
+    "kotlinc",
+    project_root + "/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/testdata/TestData.kt",    
+    "-include-runtime",
+    "-d",
+    "test.jar"
+]
+
+try:
+    process = subprocess.run(commandConvertingTestDataToJar, check=True, text=True, capture_output=True)
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred while running the command:\n{e.stderr}")
+else:
+    credits
+
+# add counter and snippet-test package
+counter = 0
+os.makedirs(os.path.join(project_root + "/build", "snippet-test"))
+
+def copy_and_rename_files(source_dir, destination_dir):
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith(".kttxt"):
+                source_file_path = os.path.join(root, file)
+                relative_dir = os.path.relpath(root, source_dir)
+                destination_subdir = os.path.join(destination_dir, relative_dir)
+                os.makedirs(destination_subdir, exist_ok=True)
+                destination_file_path = os.path.join(destination_subdir, file[:-6] + ".kt")
+                shutil.copy2(source_file_path, destination_file_path)
+
+source_dir = project_root + "/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core"
+destination_dir = project_root + "/build/snippet-test"
+
+copy_and_rename_files(source_dir, destination_dir)
+
+for root, dirs, files in os.walk(destination_dir):
+    for file in files:
+        
+        # create and run kotlinc command which verify valid kotlin code 
+        snippetCommand = [
+            "kotlinc", 
+            "-cp", 
+            "test.jar", 
+            "-nowarn", 
+            os.path.join(root, file)
+        ]
+        try:
+            process = subprocess.run(snippetCommand, check=True, text=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred while running the command:\n{e.stderr}")
+            counter += 1
+        else:
+            credits
+
+# Delete the "com" directory and its contents
+try:
+    shutil.rmtree(project_root + "/scripts/com")
+    print(f"Directory '{project_root}/scripts/com' deleted.")
+except FileNotFoundError:
+    print(f"Directory '{project_root}/scripts/com' does not exist.")
+except OSError as e:
+    print(f"An error occurred while deleting the directory '{project_root}/scripts/com': {e}")
+
+# Delete the "META-INF" directory and its contents
+try:
+    shutil.rmtree(project_root + "/scripts/META-INF")
+    print(f"Directory '{project_root}/scripts/META-INF' deleted.")
+except FileNotFoundError:
+    print(f"Directory '{project_root}/scripts/META-INF' does not exist.")
+except OSError as e:
+    print(f"An error occurred while deleting the directory '{project_root}/scripts/META-INF': {e}")
+
+# Delete the "snippet-test" package and its
+shutil.rmtree(project_root + "/build/snippet-test")
+
+# Execute the Git command "git clean -f"
+subprocess.run(["git", "clean", "-f"])
+
+print(counter)
