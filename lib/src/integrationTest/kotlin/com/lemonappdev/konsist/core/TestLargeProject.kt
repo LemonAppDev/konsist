a8adbb72d9142c9987c68155905757a0e9e8864b
natalia.peterwas <n.peterwas@gmail.com>
2023-06-05 13:06:54 +0200

    fix spotless

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
@@ -3,9 +3,9 @@ package com.lemonappdev.konsist.core
 import com.lemonappdev.konsist.api.Konsist
 import org.junit.jupiter.api.Test
 
-class TestLargeProject{
+class TestLargeProject {
     @Test
-    fun `parse-large-projects`(){
+    fun `parse-large-projects`() {
         Konsist.scopeFromDirectory("/Users/natalia/IdeaProjects/LargeKotlinProjects/kotlinx.coroutines", true)
             .declarations()
     }
