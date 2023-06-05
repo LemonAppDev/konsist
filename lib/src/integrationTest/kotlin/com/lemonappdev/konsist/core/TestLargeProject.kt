5e6b967c8b40ca6cc64cee1d779411eeb9fa5102
natalia.peterwas <n.peterwas@gmail.com>
2023-06-05 13:21:42 +0200

    fix bug and add example test

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.core
 
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.core.verify.assert
 import org.junit.jupiter.api.Test
 
 class TestLargeProject {
@@ -8,5 +9,6 @@ class TestLargeProject {
     fun `parse-large-projects`() {
         Konsist.scopeFromDirectory("/Users/natalia/IdeaProjects/LargeKotlinProjects/kotlinx.coroutines", true)
             .declarations()
+            .assert { it.name != "" }
     }
 }
