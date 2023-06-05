410263109faa45aa99235d0bd22a9cbe99b2523b
natalia.peterwas <n.peterwas@gmail.com>
2023-06-05 15:29:37 +0200

    remove sample test

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
@@ -1,15 +1,9 @@
 package com.lemonappdev.konsist.core
 
-import com.lemonappdev.konsist.api.Konsist
-import org.amshove.kluent.shouldNotBeEqualTo
 import org.junit.jupiter.api.Test
 
 class TestLargeProject {
     @Test
     fun `parse-large-projects`() {
-        Konsist.scopeFromDirectory("/Users/natalia/IdeaProjects/LargeKotlinProjects/kotlinx.coroutines", true)
-            .objects()
-            .first { it.fullyQualifiedName.contains("AgentPremain") }
-            .name shouldNotBeEqualTo ""
     }
 }
