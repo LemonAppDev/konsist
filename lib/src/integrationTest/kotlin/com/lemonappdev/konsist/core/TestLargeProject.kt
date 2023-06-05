7a3d46fdf974a51b1445f8a826a3e4944d8e5df6
natalia.peterwas <n.peterwas@gmail.com>
2023-06-05 13:02:55 +0200

    add test class

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/TestLargeProject.kt
@@ -1,3 +1,12 @@
 package com.lemonappdev.konsist.core
 
-class TestLargeProject
+import com.lemonappdev.konsist.api.Konsist
+import org.junit.jupiter.api.Test
+
+class TestLargeProject{
+    @Test
+    fun `parse-large-projects`(){
+        Konsist.scopeFromDirectory("/Users/natalia/IdeaProjects/LargeKotlinProjects/kotlinx.coroutines", true)
+            .declarations()
+    }
+}
