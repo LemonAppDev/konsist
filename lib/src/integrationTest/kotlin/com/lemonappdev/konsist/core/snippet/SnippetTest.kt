351e4b0aed6a6e7abaa1242533562168d34d7924
natalia.peterwas <n.peterwas@gmail.com>
2023-06-07 11:04:39 +0200

    remove duplicated test

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/snippet/SnippetTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/snippet/SnippetTest.kt
@@ -3,18 +3,8 @@ package com.lemonappdev.konsist.core.snippet
 import com.lemonappdev.konsist.core.ext.sep
 import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
-import java.nio.file.Files
-import java.nio.file.Paths
 
 class SnippetTest {
     @Test
@@ -63,74 +53,8 @@ class SnippetTest {
             .map { it.removePrefix("\"") }
             .map { it.removeSuffix(suffix) }
 
-    @ParameterizedTest
-    @MethodSource("provideValues")
-    fun `every snippet has valid code`(path: String, text: String) {
-        // given
-        val snippetFileWithKt = File("build${sep}snippet-test$sep${path.substringAfterLast("/").replace("kttxt", "kt")}")
-        snippetFileWithKt.writeText(text)
-
-        val processBuilder = ProcessBuilder("kotlinc", "-cp", "test.jar", "-nowarn", snippetFileWithKt.absolutePath)
-        processBuilder.redirectErrorStream(false)
-
-        val process = processBuilder.start()
-
-        val errorReader = BufferedReader(InputStreamReader(process.errorStream))
-
-        val sut = errorReader.readText()
-
-        // then
-        sut shouldBeEqualTo ""
-    }
-
     companion object {
         private val File.isKotlinSnippetFile: Boolean get() = isFile && name.endsWith(".kttxt")
         private val File.isKotlinNotSnippetFile: Boolean get() = isFile && !name.endsWith(".kttxt")
-
-        @JvmStatic
-        @BeforeAll
-        fun prepareFiles() {
-            val commandConvertingTestDataToJar = listOf(
-                "kotlinc",
-                File("src${sep}integrationTest${sep}kotlin${sep}com${sep}lemonappdev${sep}konsist${sep}testdata${sep}TestData.kt")
-                    .absolutePath,
-                "-include-runtime",
-                "-d",
-                "test.jar",
-            )
-
-            val builderTestDataJar = ProcessBuilder(commandConvertingTestDataToJar)
-            builderTestDataJar.redirectErrorStream(true)
-
-            val processConvertingTestDataToJar = builderTestDataJar
-                .start()
-                .waitFor()
-
-            require(processConvertingTestDataToJar == 0) { "TestData is invalid file." }
-
-            Files.createDirectory(Paths.get("build").resolve("snippet-test$sep"))
-        }
-
-        @AfterEach
-        fun deleteFiles() {
-            File("com$sep").deleteRecursively()
-            File("META-INF$sep").deleteRecursively()
-            File("build${sep}snippet-test").deleteRecursively()
-
-            ProcessBuilder(
-                "git",
-                "clean",
-                "-f",
-            ).start()
-        }
-
-        @Suppress("unused")
-        @JvmStatic
-        fun provideValues(): List<Arguments> = mutableListOf<Arguments>().also {
-            File("src${sep}integrationTest$sep..")
-                .walk()
-                .filter { it.isKotlinSnippetFile }
-                .forEach { file -> it.add(arguments(file.absolutePath, file.readText())) }
-        }
     }
 }
