7afcdc2a0de345eb5363e8592bbed4438e59d0e3
natalia.peterwas <n.peterwas@gmail.com>
2023-06-07 10:59:45 +0200

    Improve kdoc references

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/Konsist.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/Konsist.kt
@@ -4,8 +4,8 @@ import com.lemonappdev.konsist.api.container.koscope.KoScopeCreator
 import com.lemonappdev.konsist.core.container.koscope.KoScopeCreatorImpl
 
 /**
- * Represents the Konsist API. This is the main entry point to the Konist library.
+ * Represents the Konsist API. This is the main entry point to the Konsist library.
  *
- * It allows to create a [KoScope] instance from the given set of files such as all project files, single module, path etc.
+ * It allows to create a [com.lemonappdev.konsist.api.container.koscope.KoScope] instance from the given set of files such as all project files, single module, path etc.
  */
 object Konsist : KoScopeCreator by KoScopeCreatorImpl()
