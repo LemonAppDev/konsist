82b724698cfcdec4f2d66555fe7b3b097adfae42
natalia.peterwas <n.peterwas@gmail.com>
2023-06-12 13:43:17 +0200

    change test

--- scripts/verify_snippet_valid_code.py
+++ scripts/verify_snippet_valid_code.py
@@ -1,32 +1,30 @@
 import subprocess
 import shutil
 import os
-import glob
-import sys
-import time
 
 script_path = os.path.abspath(__file__)
 project_root = os.path.dirname(os.path.dirname(script_path))
 
 # convert TestData to jar. extension
-commandConvertingTestDataToJar = [
+command_converting_testdata_to_jar = [
     "kotlinc",
-    project_root + "/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/testdata/TestData.kt",    
+    project_root + "/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/testdata/TestData.kt",
     "-include-runtime",
     "-d",
     "test.jar"
 ]
 
 try:
-    subprocess.run(commandConvertingTestDataToJar, check=True, text=True, capture_output=True)
+    subprocess.run(command_converting_testdata_to_jar, check=True, text=True, capture_output=True)
 except subprocess.CalledProcessError as e:
     print(f"An error occurred while running the command:\n{e.stderr}")
+    error_occurred = True
 else:
-    credits
+    error_occurred = False
 
 # add snippet-test package
-counter = 0
-os.makedirs(os.path.join(project_root + "/build", "snippet-test"))
+source_dir = project_root + "/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core"
+destination_dir = project_root + "/build/snippet-test"
 
 def copy_and_rename_files(source_dir, destination_dir):
     for root, dirs, files in os.walk(source_dir):
@@ -39,29 +37,23 @@ def copy_and_rename_files(source_dir, destination_dir):
                 destination_file_path = os.path.join(destination_subdir, file[:-6] + ".kt")
                 shutil.copy2(source_file_path, destination_file_path)
 
-source_dir = project_root + "/lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core"
-destination_dir = project_root + "/build/snippet-test"
-
 copy_and_rename_files(source_dir, destination_dir)
 
 for root, dirs, files in os.walk(destination_dir):
     for file in files:
-        
-        # create and run kotlinc command which verify valid kotlin code 
-        snippetCommand = [
-            "kotlinc", 
-            "-cp", 
-            "test.jar", 
-            "-nowarn", 
+        # create and run kotlinc command which verifies valid kotlin code
+        snippet_command = [
+            "kotlinc",
+            "-cp",
+            "test.jar",
+            "-nowarn",
             os.path.join(root, file)
         ]
         try:
-            process = subprocess.run(snippetCommand, check=True, text=True, capture_output=True)
+            subprocess.run(snippet_command, check=True, text=True, capture_output=True)
         except subprocess.CalledProcessError as e:
             print(f"An error occurred while running the command:\n{e.stderr}")
-            counter += 1
-        else:
-            credits
+            error_occurred = True
 
 # Delete the "com" directory and its contents
 try:
@@ -79,13 +71,13 @@ except FileNotFoundError:
 except OSError as e:
     print(f"An error occurred while deleting the directory '{project_root}/scripts/META-INF': {e}")
 
-# Delete the "snippet-test" package and its
+# Delete the "snippet-test" package and its contents
 shutil.rmtree(project_root + "/build/snippet-test")
 
 # Execute the Git command "git clean -f"
 subprocess.run(["git", "clean", "-f"])
 
-if len(process.stderr) != 0:
+if error_occurred:
     sys.exit(1)
 else:
     sys.exit(0)
