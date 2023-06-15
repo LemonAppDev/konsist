bbf50fe5fb2e05662c5f0f7ad84365df345c56cf
natalia.peterwas <n.peterwas@gmail.com>
2023-06-15 13:33:44 +0200

    back to previous version

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/container/koscope/KoScopeCreatorImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/container/koscope/KoScopeCreatorImpl.kt
@@ -55,9 +55,6 @@ internal class KoScopeCreatorImpl : KoScopeCreator {
                 }
             }
 
-        println("Test1: $projectKotlinFiles")
-        println("Test2: $localProjectKotlinFiles")
-
         if (moduleName == null && sourceSetName == null) {
             return localProjectKotlinFiles
         }
@@ -77,7 +74,7 @@ internal class KoScopeCreatorImpl : KoScopeCreator {
         }
 
         return localProjectKotlinFiles
-//            .filter { it.path.matches(Regex(pathPrefix.toCanonicalPaths())) }
+            .filter { it.path.matches(Regex(pathPrefix.toCanonicalPaths())) }
     }
 
     override fun scopeFromProduction(moduleName: String?, sourceSetName: String?): KoScope {
