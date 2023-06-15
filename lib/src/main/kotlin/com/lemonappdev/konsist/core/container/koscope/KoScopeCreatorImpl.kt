edf9dec22de25c8f23c83ec214138c594cf29c53
natalia.peterwas <n.peterwas@gmail.com>
2023-06-15 10:14:31 +0200

    remove some code 3 - to testing

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/container/koscope/KoScopeCreatorImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/container/koscope/KoScopeCreatorImpl.kt
@@ -54,27 +54,27 @@ internal class KoScopeCreatorImpl : KoScopeCreator {
 //                    it
 //                }
 //            }
-
-        if (moduleName == null && sourceSetName == null) {
-            return localProjectKotlinFiles
-        }
-
-        var pathPrefix = if (moduleName == ROOT_MODULE_NAME) {
-            projectRootPath
-        } else if (moduleName != null) {
-            "$projectRootPath/$moduleName"
-        } else {
-            "$projectRootPath.*"
-        }
-
-        pathPrefix = if (sourceSetName != null) {
-            "$pathPrefix/src/$sourceSetName/.*"
-        } else {
-            "$pathPrefix/src/.*"
-        }
+//
+//        if (moduleName == null && sourceSetName == null) {
+//            return localProjectKotlinFiles
+//        }
+//
+//        var pathPrefix = if (moduleName == ROOT_MODULE_NAME) {
+//            projectRootPath
+//        } else if (moduleName != null) {
+//            "$projectRootPath/$moduleName"
+//        } else {
+//            "$projectRootPath.*"
+//        }
+//
+//        pathPrefix = if (sourceSetName != null) {
+//            "$pathPrefix/src/$sourceSetName/.*"
+//        } else {
+//            "$pathPrefix/src/.*"
+//        }
 
         return localProjectKotlinFiles
-            .filter { it.path.matches(Regex(pathPrefix.toCanonicalPaths())) }
+//            .filter { it.path.matches(Regex(pathPrefix.toCanonicalPaths())) }
     }
 
     override fun scopeFromProduction(moduleName: String?, sourceSetName: String?): KoScope {
