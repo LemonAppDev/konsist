7a81bd1fd31d49a4b6d239adadd03943d113cbca
natalia.peterwas <n.peterwas@gmail.com>
2023-06-16 15:14:45 +0200

    Enforce 1 argument form  in KoPackageDeclarationSequenceExt

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/sequence/KoPackageDeclarationSequenceExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/sequence/KoPackageDeclarationSequenceExt.kt
@@ -5,19 +5,25 @@ import com.lemonappdev.konsist.api.declaration.KoPackageDeclaration
 /**
  * Sequence containing all packages that have the qualified name.
  *
+ * @param qualifiedName The name to include.
  * @param qualifiedNames The names to include.
  * @return A sequence containing packages with the specified qualified names.
  */
-fun Sequence<KoPackageDeclaration>.withQualifiedName(vararg qualifiedNames: String): Sequence<KoPackageDeclaration> = filter {
-    qualifiedNames.any { name -> it.qualifiedName == name }
-}
+fun Sequence<KoPackageDeclaration>.withQualifiedName(qualifiedName: String, vararg qualifiedNames: String): Sequence<KoPackageDeclaration> =
+    filter {
+        it.qualifiedName == qualifiedName || qualifiedNames.any { name -> it.qualifiedName == name }
+    }
 
 /**
  * Sequence containing all packages that don't have the qualified name.
  *
+ * @param qualifiedName The name to exclude.
  * @param qualifiedNames The names to exclude.
  * @return A sequence containing packages without the specified qualified names.
  */
-fun Sequence<KoPackageDeclaration>.withoutQualifiedName(vararg qualifiedNames: String): Sequence<KoPackageDeclaration> = filter {
-    qualifiedNames.none { name -> it.qualifiedName == name }
+fun Sequence<KoPackageDeclaration>.withoutQualifiedName(
+    qualifiedName: String,
+    vararg qualifiedNames: String
+): Sequence<KoPackageDeclaration> = filter {
+    it.qualifiedName != qualifiedName && qualifiedNames.none { name -> it.qualifiedName == name }
 }
