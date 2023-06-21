557ea116db1c30492fc4279d0059254cb05afdd6
natalia.peterwas <n.peterwas@gmail.com>
2023-06-21 13:01:13 +0200

    add new Assert class

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/verify/KoArchitectureAssert.kt
@@ -0,0 +1,71 @@
+package com.lemonappdev.konsist.core.verify
+
+import com.lemonappdev.konsist.api.architecture.KoArchitecture
+import com.lemonappdev.konsist.api.container.koscope.KoScope
+import com.lemonappdev.konsist.api.ext.sequence.withPackage
+import com.lemonappdev.konsist.core.architecture.KoArchitectureImpl
+import com.lemonappdev.konsist.core.architecture.Layer
+import com.lemonappdev.konsist.core.exception.KoCheckFailedException
+import com.lemonappdev.konsist.core.exception.KoException
+import com.lemonappdev.konsist.core.exception.KoInternalException
+
+@Suppress("detekt.ThrowsCount")
+fun KoScope.assert(architecture: KoArchitecture) {
+    try {
+        val files = files()
+
+        val layerHasValidArchitecture = (architecture as KoArchitectureImpl)
+            .dependencies
+            .map { (t, u) ->
+                u?.let {
+                    val other = (architecture.allLayers - u.toList()).map { (it as Layer).isDefinedBy }
+
+                    files
+                        .withPackage(t.isDefinedBy)
+                        .all { other.all { other -> !it.hasImports(other) } }
+                } ?: true
+            }
+
+        val result = mutableMapOf<Layer, Boolean>()
+
+        architecture
+            .dependencies
+            .keys
+            .forEachIndexed { index, layer -> result[layer] = layerHasValidArchitecture[index] }
+
+        val passedLayers = mutableListOf<Layer>()
+        val failedLayers = mutableListOf<Layer>()
+
+        result.forEach { (t, u) ->
+            if (u) {
+                passedLayers += t
+            } else {
+                failedLayers += t
+            }
+        }
+
+        val allChecksPassed = failedLayers.isEmpty()
+
+        if (!allChecksPassed) {
+            throw KoCheckFailedException(getCheckFailedMessages(failedLayers))
+        }
+    } catch (e: KoException) {
+        throw e
+    } catch (@Suppress("detekt.TooGenericExceptionCaught") e: Exception) {
+        throw KoInternalException(e.message.orEmpty(), e)
+    }
+}
+
+private fun getCheckFailedMessages(failedDeclarations: List<Layer>): String {
+    val failedDeclarationsMessage = failedDeclarations.joinToString("\n")
+
+    return "Assert '${getTestMethodName()}' has failed. Invalid dependencies at (${failedDeclarations.size}):\n$failedDeclarationsMessage"
+}
+
+/**
+ * In this call stack hierarchy test name is at index 5.
+ */
+private fun getTestMethodName(): String? {
+    val stackTraceIndexOfTestMethod = 5
+    return Thread.currentThread().stackTrace[stackTraceIndexOfTestMethod].methodName
+}
