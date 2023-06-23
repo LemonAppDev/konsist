8ea6c01aa62012c9e0e0949af1ca3634f982d183
natalia.peterwas <n.peterwas@gmail.com>
2023-06-23 16:08:46 +0200

    add example tests

--- samples/snippets/every-class-has-test.kt
+++ samples/snippets/every-class-has-test.kt
@@ -1,5 +1,22 @@
 Konsist.scopeFromProject()
-    .classes()
-    .assert {
-        it.hasTest()
-    }
+.classes()
+.assert {
+    it.hasTest()
+}
+
+// without value and data classes: two ways
+
+Konsist.scopeFromProject()
+.classes()
+.withoutDataModifier()
+.withoutValueModifier()
+.assert {
+    it.hasTest()
+}
+
+Konsist.scopeFromProject()
+.classes()
+.withoutModifiers(KoModifier.DATA, KoModifier.VALUE)
+.assert {
+    it.hasTest()
+}
