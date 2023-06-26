bb285e4550ba406ed43843646af119a30b348c6b
natalia.peterwas <n.peterwas@gmail.com>
2023-06-26 15:59:36 +0200

    add first version of aasert to KoFiles

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/verify/kofileassert/KoFileAssert.kt
@@ -0,0 +1,87 @@
+package com.lemonappdev.konsist.core.verify.kofileassert
+
+import com.lemonappdev.konsist.api.container.KoFile
+import com.lemonappdev.konsist.api.declaration.KoAnnotationDeclaration
+import com.lemonappdev.konsist.api.declaration.KoBaseDeclaration
+import com.lemonappdev.konsist.api.declaration.KoNamedDeclaration
+import com.lemonappdev.konsist.core.container.KoFileImpl
+import com.lemonappdev.konsist.core.declaration.KoDeclarationImpl
+import com.lemonappdev.konsist.core.exception.KoCheckFailedException
+import com.lemonappdev.konsist.core.exception.KoException
+import com.lemonappdev.konsist.core.exception.KoInternalException
+import com.lemonappdev.konsist.core.exception.KoPreconditionFailedException
+
+fun <E : KoFile> Sequence<E>.assert(function: (E) -> Boolean?) {
+    assert(function, true)
+}
+
+fun <E : KoFile> Sequence<E>.assertNot(function: (E) -> Boolean?) {
+    assert(function, false)
+}
+
+@Suppress("detekt.ThrowsCount")
+private fun <E : KoFile> Sequence<E>.assert(function: (E) -> Boolean?, positiveCheck: Boolean) {
+    var lastFile: KoFile? = null
+
+    try {
+        val localList = this.toList()
+
+        if (localList.isEmpty()) {
+            val checkMethodName = Thread.currentThread().stackTrace[2].methodName
+            throw KoPreconditionFailedException(
+                "Files list is empty. Please make sure that list of files contain items " +
+                        "before calling the '$checkMethodName' method.",
+            )
+        }
+
+        val notSuppressedDeclarations = localList
+            .filterNot { checkIfSuppressed(it, Thread.currentThread().stackTrace[4].methodName) }
+
+        val result = notSuppressedDeclarations.groupBy {
+            lastFile = it
+            function(it)
+        }
+
+        val allChecksPassed = (result[positiveCheck]?.size ?: 0) == notSuppressedDeclarations.size
+
+        if (!allChecksPassed) {
+            val failedDeclarations = result[!positiveCheck] ?: emptyList()
+            throw KoCheckFailedException(getCheckFailedMessage(failedDeclarations))
+        }
+    } catch (e: KoException) {
+        throw e
+    } catch (@Suppress("detekt.TooGenericExceptionCaught") e: Exception) {
+        throw KoInternalException(e.message.orEmpty(), e, lastFile)
+    }
+}
+
+private fun <E : KoFile> getCheckFailedMessage(failedDeclarations: List<E>): String {
+    val failedDeclarationsMessage = failedDeclarations.joinToString("\n") {
+        val name = if (it is KoFile) it.name else ""
+        val konsistDeclarationClassNamePrefix = "Ko"
+        val declarationType = it::class.simpleName?.substringAfter(konsistDeclarationClassNamePrefix)
+
+        "${it.path} ($name $declarationType)"
+    }
+
+    return "Assert '${getTestMethodName()}' has failed. Invalid declarations (${failedDeclarations.size}):\n$failedDeclarationsMessage"
+}
+
+/**
+ * In this call stack hierarchy test name is at index 5.
+ */
+private fun getTestMethodName(): String? {
+    val stackTraceIndexOfTestMethod = 5
+    return Thread.currentThread().stackTrace[stackTraceIndexOfTestMethod].methodName
+}
+
+private fun checkIfSuppressed(file: KoFile, testMethodName: String): Boolean {
+    val annotationParameter = (file as KoFileImpl)
+        .annotations
+        .firstOrNull { it.name == "Suppress" }
+        ?.text
+        ?.removePrefix("@file:Suppress(\"")
+        ?.removeSuffix("\")")
+
+    return annotationParameter == testMethodName || annotationParameter == "konsist.$testMethodName"
+}
