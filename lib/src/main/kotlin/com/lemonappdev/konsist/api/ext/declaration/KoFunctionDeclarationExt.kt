a227a0271f1eb98cff9efdfcc310e33cfbcb7999
natalia.peterwas <n.peterwas@gmail.com>
2023-06-26 12:29:06 +0200

    fix konsist test

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/declaration/KoFunctionDeclarationExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/declaration/KoFunctionDeclarationExt.kt
@@ -2,4 +2,9 @@ package com.lemonappdev.konsist.api.ext.declaration
 
 import com.lemonappdev.konsist.api.declaration.KoFunctionDeclaration
 
+/**
+ * Whether this function has receiver.
+ *
+ * @return `true` if the function has receiver with the specified KClass name, `false` otherwise.
+ */
 inline fun <reified T> KoFunctionDeclaration.hasReceiverOf(): Boolean = T::class.simpleName == receiver?.name
