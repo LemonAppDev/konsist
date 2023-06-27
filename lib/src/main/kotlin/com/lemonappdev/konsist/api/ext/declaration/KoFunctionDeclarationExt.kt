c5cd746315f203c8001fa3770bbf56ce96be97c8
natalia.peterwas <n.peterwas@gmail.com>
2023-06-27 10:10:00 +0200

    rename `receiver` to `receiverType`

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/declaration/KoFunctionDeclarationExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/declaration/KoFunctionDeclarationExt.kt
@@ -3,8 +3,8 @@ package com.lemonappdev.konsist.api.ext.declaration
 import com.lemonappdev.konsist.api.declaration.KoFunctionDeclaration
 
 /**
- * Whether this function has receiver.
+ * Whether this function has receiver type.
  *
- * @return `true` if the function has receiver with the specified KClass name, `false` otherwise.
+ * @return `true` if the function has receiver type with the specified KClass name, `false` otherwise.
  */
 inline fun <reified T> KoFunctionDeclaration.hasReceiverTypeOf(): Boolean = T::class.simpleName == receiverType?.name
