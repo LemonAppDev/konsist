80a185db535f6aa84aa9658b001008cb7d30af81
natalia.peterwas <n.peterwas@gmail.com>
2023-06-27 13:41:30 +0200

    rename architectureIntegrationTest to konsistArchitectureApiTest

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -36,7 +36,7 @@ testing {
             }
         }
 
-        register("architectureIntegrationTest", JvmTestSuite::class) {
+        register("konsistArchitectureApiTest", JvmTestSuite::class) {
             dependencies {
                 implementation(project(":lib"))
                 implementation(libs.kluent)
