42076fba19e58762cf45add2630ab4e12a64d981
natalia.peterwas <n.peterwas@gmail.com>
2023-06-27 16:42:59 +0200

    add new exceptions

--- lib/src/konsistArchitectureApiTest/kotlin/com/lemonappdev/konsist/architecture1/Architecture1Test.kt
+++ lib/src/konsistArchitectureApiTest/kotlin/com/lemonappdev/konsist/architecture1/Architecture1Test.kt
@@ -28,23 +28,6 @@ class Architecture1Test {
     }
 
     @Test
-    fun `throws an exception when self dependency is set`() {
-        // given
-        val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
-
-        val sut = {
-            Konsist
-                .architecture(domain)
-                .addDependencies { domain.dependsOn(domain) }
-        }
-
-        // then
-        sut shouldThrow KoPreconditionFailedException::class withMessage """
-            Layer: Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..) cannot be dependent on itself.
-        """.trimIndent()
-    }
-
-    @Test
     fun `throws an exception when the layer that the domain layer should depend on is not added to the architecture`() {
         // given
         val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
@@ -79,6 +62,106 @@ class Architecture1Test {
         sut shouldThrow KoPreconditionFailedException::class withMessage """
             Layer not added to the architecture:
             Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..).
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when self dependency is set`() {
+        // given
+        val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
+
+        val sut = {
+            Konsist
+                .architecture(domain)
+                .addDependencies { domain.dependsOn(domain) }
+        }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Layer: Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..) cannot be dependent on itself.
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when layer is set as independent and then set as depend on other layer`() {
+        // given
+        val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
+        val presentation = Layer("Presentation", "com.lemonappdev.konsist.architecture1.project.presentation..")
+
+        val sut = {
+            Konsist
+                .architecture(domain, presentation)
+                .addDependencies {
+                    domain.notDependOnAnyLayer()
+                    domain.dependsOn(presentation)
+                }
+        }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Layer: Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..) was set as independent before.
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when layer is set as independent twice`() {
+        // given
+        val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
+
+        val sut = {
+            Konsist
+                .architecture(domain)
+                .addDependencies {
+                    domain.notDependOnAnyLayer()
+                    domain.notDependOnAnyLayer()
+                }
+        }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Dependency that Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..) should be independent was duplicated.
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when layer is set as dependent on other layer and then as independent`() {
+        // given
+        val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
+        val presentation = Layer("Presentation", "com.lemonappdev.konsist.architecture1.project.presentation..")
+
+        val sut = {
+            Konsist
+                .architecture(domain, presentation)
+                .addDependencies {
+                    domain.dependsOn(presentation)
+                    domain.notDependOnAnyLayer()
+                }
+        }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Layer: Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..) had a previously set dependency with Layer(name=Presentation, isDefinedBy=com.lemonappdev.konsist.architecture1.project.presentation..).
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when layer is set as dependent on the same layer twice`() {
+        // given
+        val domain = Layer("Domain", "com.lemonappdev.konsist.architecture1.project.domain..")
+        val presentation = Layer("Presentation", "com.lemonappdev.konsist.architecture1.project.presentation..")
+
+        val sut = {
+            Konsist
+                .architecture(domain, presentation)
+                .addDependencies {
+                    domain.dependsOn(presentation)
+                    domain.dependsOn(presentation)
+                }
+        }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Dependency between Layer(name=Domain, isDefinedBy=com.lemonappdev.konsist.architecture1.project.domain..) and Layer(name=Presentation, isDefinedBy=com.lemonappdev.konsist.architecture1.project.presentation..) is set more than once.
         """.trimIndent()
     }
 }
--- lib/src/main/kotlin/com/lemonappdev/konsist/core/architecture/KoArchitectureImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/architecture/KoArchitectureImpl.kt
@@ -2,12 +2,17 @@ package com.lemonappdev.konsist.core.architecture
 
 import com.lemonappdev.konsist.api.architecture.KoArchitecture
 import com.lemonappdev.konsist.core.exception.KoPreconditionFailedException
+import com.sun.tools.javac.comp.Todo
 
 class KoArchitectureImpl(vararg layers: Layer) : KoArchitecture {
     val dependencies = mutableMapOf<Layer, Set<Layer>>()
+    private val statuses = mutableMapOf<Layer, Status>()
 
     val allLayers = layers.toMutableList()
-        .onEach { layer -> dependencies[layer] = setOf(layer) }
+        .onEach { layer ->
+            dependencies[layer] = setOf(layer)
+            statuses[layer] = Status.NONE
+        }
 
     override fun addDependencies(dependencies: KoArchitecture.() -> Unit): KoArchitecture {
         dependencies()
@@ -15,17 +20,72 @@ class KoArchitectureImpl(vararg layers: Layer) : KoArchitecture {
     }
 
     override fun Layer.dependsOn(layer: Layer, vararg layers: Layer) {
+        checkIfLayerIsAddToArchitecture(this, listOf(layer) + layers)
         checkIfLayerIsDependentOnItself(this, layer, *layers)
-        checkIfLayerIsAddToArchitecture(this, listOf(layer)+layers)
+        checkStatusOfLayer(false, this, layer, *layers)
         checkCircularDependencies(this, layer, *layers)
 
         dependencies[this] = (dependencies.getOrDefault(this, setOf(this))) + layer + layers
+        statuses[this] = Status.DEPEND_ON_LAYER
     }
 
     override fun Layer.notDependOnAnyLayer() {
         checkIfLayerIsAddToArchitecture(this)
+        checkStatusOfLayer(true, this)
 
         dependencies[this] = setOf(this)
+        statuses[this] = Status.INDEPENDENT
+    }
+
+    private fun checkIfLayerIsAddToArchitecture(layer: Layer, layers: List<Layer>? = null) {
+        layers?.let {
+            if (layers.any { layer -> !allLayers.contains(layer) }) {
+                val notAddedLayers = layers.filterNot { layer -> allLayers.contains(layer) }
+                throw KoPreconditionFailedException("Layers not added to the architecture:\n${notAddedLayers.joinToString(separator = "\n")}.")
+            }
+        }
+        if (!allLayers.contains(layer)) {
+            throw KoPreconditionFailedException("Layer not added to the architecture:\n$layer.")
+        }
+    }
+
+    private fun checkIfLayerIsDependentOnItself(layer: Layer, vararg layers: Layer) {
+        if (layers.any { it == layer }) {
+            throw KoPreconditionFailedException("Layer: $layer cannot be dependent on itself.")
+        }
+    }
+
+    private fun checkStatusOfLayer(toBeIndependent: Boolean, layer: Layer, vararg layers: Layer): Boolean {
+        return when (statuses[layer]) {
+            Status.INDEPENDENT -> {
+                if (toBeIndependent) {
+                    throw KoPreconditionFailedException("Dependency that $layer should be independent was duplicated.")
+                } else {
+                    throw KoPreconditionFailedException("Layer: $layer was set as independent before.")
+                }
+            }
+
+            Status.DEPEND_ON_LAYER -> {
+                val dependency = dependencies.getOrDefault(layer, emptySet())
+
+                if (toBeIndependent) {
+                    // TODO("change error message")
+                    val alreadySetLayer = dependency.first { it != layer }
+                    throw KoPreconditionFailedException("Layer: $layer had a previously set dependency with $alreadySetLayer.")
+                } else if (layers.any { dependency.contains(it) }) {
+                    val alreadySetLayer = layers.first { dependency.contains(it) }
+                    throw KoPreconditionFailedException("Dependency between $layer and $alreadySetLayer is set more than once.")
+                } else {
+                    true
+                }
+            }
+
+            Status.NONE -> {
+                true
+            }
+
+            else -> throw KoPreconditionFailedException("Unknown status of layer: $layer.")
+        }
     }
 
     private fun checkCircularDependencies(layer: Layer, vararg layers: Layer) {
@@ -38,8 +98,8 @@ class KoArchitectureImpl(vararg layers: Layer) : KoArchitecture {
         if (notEmpty != null) {
             throw KoPreconditionFailedException(
                 "Illegal circular dependencies:\n" +
-                    notEmpty.filterNot { it == null }
-                        .joinToString(prefix = "$layer -->\n", postfix = "$layer.", separator = "") { "$it -->\n" },
+                        notEmpty.filterNot { it == null }
+                            .joinToString(prefix = "$layer -->\n", postfix = "$layer.", separator = "") { "$it -->\n" },
             )
         }
     }
@@ -70,22 +130,10 @@ class KoArchitectureImpl(vararg layers: Layer) : KoArchitecture {
             lists.firstOrNull { it.last() == null } ?: emptyList()
         }
     }
+}
 
-    private fun checkIfLayerIsDependentOnItself(layer: Layer, vararg layers: Layer) {
-        if (layers.any { it == layer }) {
-            throw KoPreconditionFailedException("Layer: $layer cannot be dependent on itself.")
-        }
-    }
-
-    private fun checkIfLayerIsAddToArchitecture(layer: Layer, layers: List<Layer>? = null) {
-        layers?.let {
-            if (layers.any { layer -> !allLayers.contains(layer) }) {
-                val notAddedLayers = layers.filterNot { layer -> allLayers.contains(layer)  }
-                throw KoPreconditionFailedException("Layers not added to the architecture:\n${notAddedLayers.joinToString(separator = "\n")}.")
-            }
-        }
-        if (!allLayers.contains(layer)) {
-            throw KoPreconditionFailedException("Layer not added to the architecture:\n$layer.")
-        }
-    }
+private enum class Status {
+    INDEPENDENT,
+    DEPEND_ON_LAYER,
+    NONE,
 }
