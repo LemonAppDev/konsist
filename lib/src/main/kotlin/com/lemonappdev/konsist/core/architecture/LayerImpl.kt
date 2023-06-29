ba153a9774f48fd5c4ce1b7fa5d316a5423ac35f
natalia.peterwas <n.peterwas@gmail.com>
2023-06-29 13:56:20 +0200

    fix spotless

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/architecture/LayerImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/architecture/LayerImpl.kt
@@ -3,7 +3,7 @@ package com.lemonappdev.konsist.core.architecture
 import com.lemonappdev.konsist.api.architecture.Layer
 import com.lemonappdev.konsist.core.exception.KoPreconditionFailedException
 
-data class LayerImpl(internal val name: String, internal val definedBy: String): Layer {
+data class LayerImpl(internal val name: String, internal val definedBy: String) : Layer {
     init {
         if (!definedBy.endsWith("..")) {
             throw KoPreconditionFailedException("Layer $name must be defined by package ending with '..'. Now: $definedBy .")
