a0bf3995f7eb30a504c0741143b15adfe244f684
natalia.peterwas <n.peterwas@gmail.com>
2023-06-30 15:58:49 +0200

    fix detekt

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/verify/CommonAssert.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/verify/CommonAssert.kt
@@ -9,6 +9,7 @@ import com.lemonappdev.konsist.core.exception.KoPreconditionFailedException
 private const val INDEX_FOUR = 4
 private const val INDEX_FIVE = 5
 private const val INDEX_SIX = 6
+private const val INDEX_SEVEN = 7
 
 /**
  * In this call stack hierarchy test name is at index 4.
@@ -28,7 +29,7 @@ internal fun getTestMethodNameFromSixthIndex() = getTestMethodName(INDEX_SIX)
 /**
  * In this call stack hierarchy test name is at index 7.
  */
-internal fun getTestMethodNameFromSeventhIndex() = getTestMethodName(7)
+internal fun getTestMethodNameFromSeventhIndex() = getTestMethodName(INDEX_SEVEN)
 
 private fun getTestMethodName(index: Int): String = Thread.currentThread().stackTrace[index].methodName
 
