d79e4fc9781cd0ed971e8eb6285d771208bb92bf
natalia.peterwas <n.peterwas@gmail.com>
2023-07-05 09:33:17 +0200

    add extensions for KoPackageDeclaration and test for them

---
+++ lib/src/test/kotlin/com/lemonappdev/konsist/api/ext/sequence/kopackagedeclaration/KoPackageDeclarationForMatchingFilePathExtTest.kt
@@ -0,0 +1,49 @@
+package com.lemonappdev.konsist.api.ext.sequence.kopackagedeclaration
+
+import com.lemonappdev.konsist.api.ext.sequence.withMatchingFilePath
+import com.lemonappdev.konsist.api.ext.sequence.withQualifiedName
+import com.lemonappdev.konsist.api.ext.sequence.withoutMatchingFilePath
+import com.lemonappdev.konsist.api.ext.sequence.withoutQualifiedName
+import com.lemonappdev.konsist.core.declaration.KoPackageDeclarationImpl
+import io.mockk.every
+import io.mockk.mockk
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoPackageDeclarationForMatchingFilePathExtTest {
+    @Test
+    fun `withMatchingFilePath() returns package which has matching file path`() {
+        // given
+        val package1: KoPackageDeclarationImpl = mockk {
+            every { hasMatchingFilePath } returns true
+        }
+        val package2: KoPackageDeclarationImpl = mockk {
+            every { hasMatchingFilePath } returns false
+        }
+        val packages = sequenceOf(package1, package2)
+
+        // when
+        val sut = packages.withMatchingFilePath()
+
+        // then
+        sut.toList() shouldBeEqualTo listOf(package1)
+    }
+
+    @Test
+    fun `withoutMatchingFilePath() returns package which has not matching file path`() {
+        // given
+        val package1: KoPackageDeclarationImpl = mockk {
+            every { hasMatchingFilePath } returns true
+        }
+        val package2: KoPackageDeclarationImpl = mockk {
+            every { hasMatchingFilePath } returns false
+        }
+        val packages = sequenceOf(package1, package2)
+
+        // when
+        val sut = packages.withoutMatchingFilePath()
+
+        // then
+        sut.toList() shouldBeEqualTo listOf(package2)
+    }
+}
