e032c7095136b9c0db0e610e64cf1eab70b3ee2a
natalia.peterwas <n.peterwas@gmail.com>
2023-07-06 13:26:13 +0200

    add implementation

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoPackageDeclarationImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoPackageDeclarationImpl.kt
@@ -3,11 +3,15 @@ package com.lemonappdev.konsist.core.declaration
 import com.lemonappdev.konsist.api.declaration.KoPackageDeclaration
 import com.lemonappdev.konsist.api.provider.KoParentProvider
 import com.lemonappdev.konsist.core.cache.KoDeclarationCache
+import com.lemonappdev.konsist.core.provider.KoFullyQualifiedNameProviderCore
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtPackageDirective
 
 internal class KoPackageDeclarationImpl private constructor(private val ktPackageDirective: KtPackageDirective) :
-    KoBaseDeclarationImpl(ktPackageDirective), KoPackageDeclaration {
+    KoBaseDeclarationImpl(ktPackageDirective),
+    KoPackageDeclaration,
+    KoFullyQualifiedNameProviderCore
+{
 
     override val fullyQualifiedName: String by lazy {
         if (ktPackageDirective.fqName != FqName.ROOT) {
