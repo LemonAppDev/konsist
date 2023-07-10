3861658dcc88aed42c24b08d67874e78848d2075
natalia.peterwas <n.peterwas@gmail.com>
2023-07-10 15:47:45 +0200

    use KoRepresentsTypeProvider in KoAnnotationDeclaration

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoAnnotationDeclaration.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoAnnotationDeclaration.kt
@@ -2,6 +2,7 @@ package com.lemonappdev.konsist.api.declaration
 
 import com.lemonappdev.konsist.api.provider.KoFullyQualifiedNameProvider
 import com.lemonappdev.konsist.api.provider.KoParentProvider
+import com.lemonappdev.konsist.api.provider.KoRepresentsTypeProvider
 
 /**
  * Represents a Kotlin annotation.
@@ -9,13 +10,5 @@ import com.lemonappdev.konsist.api.provider.KoParentProvider
 interface KoAnnotationDeclaration :
     KoBaseDeclaration,
     KoFullyQualifiedNameProvider,
-    KoParentProvider {
-
-    /**
-     * Whether this annotation represents the specified type.
-     *
-     * @param name the name of type to compare. It can be either a simple name or a fully qualified name.
-     * @return `true` if this annotation represents the specified type, `false` otherwise.
-     */
-    fun representsType(name: String): Boolean
-}
+    KoParentProvider,
+    KoRepresentsTypeProvider
--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoAnnotationDeclarationImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoAnnotationDeclarationImpl.kt
@@ -2,12 +2,16 @@ package com.lemonappdev.konsist.core.declaration
 
 import com.lemonappdev.konsist.api.declaration.KoAnnotationDeclaration
 import com.lemonappdev.konsist.api.provider.KoParentProvider
+import com.lemonappdev.konsist.api.provider.KoRepresentsTypeProvider
 import com.lemonappdev.konsist.core.cache.KoDeclarationCache
+import com.lemonappdev.konsist.core.provider.KoRepresentsTypeProviderCore
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 
 internal class KoAnnotationDeclarationImpl private constructor(
     private val ktAnnotationEntry: KtAnnotationEntry,
-) : KoBaseDeclarationImpl(ktAnnotationEntry), KoAnnotationDeclaration {
+) : KoBaseDeclarationImpl(ktAnnotationEntry),
+    KoAnnotationDeclaration,
+    KoRepresentsTypeProviderCore {
     override val name: String by lazy { ktAnnotationEntry.shortName.toString() }
 
     override val fullyQualifiedName: String by lazy {
@@ -16,8 +20,6 @@ internal class KoAnnotationDeclarationImpl private constructor(
             .firstOrNull { it.text.endsWith(".$name") }
             ?.name ?: ""
     }
-
-    override fun representsType(name: String): Boolean = name == this.name || name == fullyQualifiedName
 
     internal companion object {
         private val cache: KoDeclarationCache<KoAnnotationDeclaration> = KoDeclarationCache()
