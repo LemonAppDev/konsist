655cd5ce67849905533ffd806dae398bc19adfd1
natalia.peterwas <n.peterwas@gmail.com>
2023-07-14 12:52:36 +0200

    fix spotless

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoInitBlockDeclarationImpl.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/KoInitBlockDeclarationImpl.kt
@@ -3,7 +3,6 @@ package com.lemonappdev.konsist.core.declaration
 import com.lemonappdev.konsist.api.declaration.KoBaseDeclaration
 import com.lemonappdev.konsist.api.declaration.KoDeclaration
 import com.lemonappdev.konsist.api.declaration.KoInitBlockDeclaration
-import com.lemonappdev.konsist.api.declaration.KoNamedDeclaration
 import com.lemonappdev.konsist.core.cache.KoDeclarationCache
 import com.lemonappdev.konsist.core.declaration.provider.KoDeclarationCoreProviderUtil
 import org.jetbrains.kotlin.psi.KtAnonymousInitializer
