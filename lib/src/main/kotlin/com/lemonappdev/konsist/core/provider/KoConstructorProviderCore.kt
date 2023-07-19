a759d1e7f7a985be60231c2d364778a23f9f89fc
natalia.peterwas <n.peterwas@gmail.com>
2023-07-19 12:47:24 +0200

    upd KoConstructorProviderCore

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoConstructorProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoConstructorProviderCore.kt
@@ -1,5 +1,6 @@
 package com.lemonappdev.konsist.core.provider
 
+import com.lemonappdev.konsist.api.provider.KoConstructorProvider
 import com.lemonappdev.konsist.api.provider.KoConstructorsProvider
 
-internal interface KoConstructorProviderCore: KoConstructorsProvider
+internal interface KoConstructorProviderCore: KoConstructorProvider
