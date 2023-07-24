211d1b679d3dcc38d5f7082e8444fb5729b1c9d6
natalia.peterwas <n.peterwas@gmail.com>
2023-07-24 10:12:50 +0200

    upd tests at KoFunctionDeclaration

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofunctiondeclaration/KoFunctionDeclarationForKoContainingFileProviderTest.kt
@@ -0,0 +1,26 @@
+package com.lemonappdev.konsist.core.declaration.kofunctiondeclaration
+
+import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import org.amshove.kluent.assertSoftly
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoFunctionDeclarationForKoContainingFileProviderTest {
+    @Test
+    fun `function-containing-file`() {
+        // given
+        val sut = getSnippetFile("function-containing-file")
+            .functions()
+            .first()
+
+        // then
+        sut
+            .containingFile
+            .nameWithExtension
+            .endsWith("file.kt")
+            .shouldBeEqualTo(true)
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        getSnippetKoScope("core/declaration/kofunctiondeclaration/snippet/forkocontainingfileprovider/", fileName)
+}
---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofunctiondeclaration/KoFunctionDeclarationForKoResideInOrOutsidePackageProviderTest.kt
@@ -0,0 +1,56 @@
+package com.lemonappdev.konsist.core.declaration.kofunctiondeclaration
+
+import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import org.amshove.kluent.assertSoftly
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldNotBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoFunctionDeclarationForKoResideInOrOutsidePackageProviderTest {
+    @Test
+    fun `function-not-reside-in-file-package`() {
+        // given
+        val sut = getSnippetFile("function-not-reside-in-file-package")
+            .functions()
+            .first()
+
+        // then
+        sut.resideInPackage("com") shouldBeEqualTo false
+    }
+
+    @Test
+    fun `function-reside-in-file-package`() {
+        // given
+        val sut = getSnippetFile("function-reside-in-file-package")
+            .functions()
+            .first()
+
+        // then
+        sut.resideInPackage("com..") shouldBeEqualTo true
+    }
+
+    @Test
+    fun `function-not-reside-outside-file-package`() {
+        // given
+        val sut = getSnippetFile("function-not-reside-outside-file-package")
+            .functions()
+            .first()
+
+        // then
+        sut.resideOutsidePackage("com..") shouldBeEqualTo false
+    }
+
+    @Test
+    fun `function-reside-outside-file-package`() {
+        // given
+        val sut = getSnippetFile("function-reside-outside-file-package")
+            .functions()
+            .first()
+
+        // then
+        sut.resideOutsidePackage("com") shouldBeEqualTo true
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        getSnippetKoScope("core/declaration/kofunctiondeclaration/snippet/forkoresideinoroutsidepackageprovider/", fileName)
+}
