087884d08883cfc12ea12df794574bcc3ded885f
natalia.peterwas <n.peterwas@gmail.com>
2023-07-26 09:43:38 +0200

    add `toString` tests

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koparameterdeclaration/KoParameterDeclarationTest.kt
@@ -0,0 +1,24 @@
+package com.lemonappdev.konsist.core.declaration.koparameterdeclaration
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoParameterDeclarationTest {
+    @Test
+    fun `parameter-to-string`() {
+        // given
+        val sut = getSnippetFile("parameter-to-string")
+            .classes()
+            .first()
+            .primaryConstructor
+            ?.parameters
+            ?.first()
+
+        // then
+        sut.toString() shouldBeEqualTo sut?.locationWithText
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/koparameterdeclaration/snippet/forgeneral/", fileName)
+}
