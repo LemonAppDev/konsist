1d5219fa11f266a1e098f1e9947802ba868ca661
natalia.peterwas <n.peterwas@gmail.com>
2023-08-01 11:51:03 +0200

    fix after merge

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/verify/kodeclarationassert/fordeclarationsequence/KoDeclarationAssertForDeclarationSequenceTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/verify/kodeclarationassert/fordeclarationsequence/KoDeclarationAssertForDeclarationSequenceTest.kt
@@ -1,8 +1,6 @@
 package com.lemonappdev.konsist.core.verify.kodeclarationassert.fordeclarationsequence
 
 import com.lemonappdev.konsist.TestSnippetProvider
-import com.lemonappdev.konsist.api.ext.sequence.withPrimaryConstructor
-import com.lemonappdev.konsist.api.provider.KoNameProvider
 import com.lemonappdev.konsist.core.exception.KoCheckFailedException
 import com.lemonappdev.konsist.core.verify.assert
 import com.lemonappdev.konsist.core.verify.assertNot
