f3b2b8e02ec504207469250fab8d3fb30eeaf7fc
natalia.peterwas <n.peterwas@gmail.com>
2023-08-02 17:34:43 +0200

    upd package providers

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/packagee/KoPackageProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/packagee/KoPackageProviderCore.kt
@@ -2,7 +2,5 @@ package com.lemonappdev.konsist.core.provider.packagee
 
 import com.lemonappdev.konsist.api.provider.KoPackageProvider
 import com.lemonappdev.konsist.core.provider.KoBaseProviderCore
-import com.lemonappdev.konsist.core.provider.KoContainingFileProviderCore
-import com.lemonappdev.konsist.core.provider.KoParentProviderCore
 
-internal interface KoPackageProviderCore : KoPackageProvider, KoContainingFileProviderCore, KoBaseProviderCore, KoParentProviderCore
+internal interface KoPackageProviderCore : KoPackageProvider, KoBaseProviderCore
