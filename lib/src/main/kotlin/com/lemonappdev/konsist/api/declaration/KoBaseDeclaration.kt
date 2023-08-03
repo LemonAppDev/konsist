3861600b7147c377e3f5ad417b43aaa3f399229a
natalia.peterwas <n.peterwas@gmail.com>
2023-08-03 12:01:22 +0200

    add tests and kdoc

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoBaseDeclaration.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoBaseDeclaration.kt
@@ -1,6 +1,8 @@
 package com.lemonappdev.konsist.api.declaration
 
+import com.lemonappdev.konsist.api.provider.KoBaseProvider
+
 /**
  * Represents a Kotlin base declaration.
  */
-interface KoBaseDeclaration
+interface KoBaseDeclaration: KoBaseProvider
