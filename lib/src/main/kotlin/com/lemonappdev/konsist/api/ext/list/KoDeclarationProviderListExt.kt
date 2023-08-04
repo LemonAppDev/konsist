5093a60df26cb8984277b46c66bf2d20eb2b63dc
natalia.peterwas <n.peterwas@gmail.com>
2023-08-04 10:55:35 +0200

    upd kdoc

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoDeclarationProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoDeclarationProviderListExt.kt
@@ -8,7 +8,7 @@ import com.lemonappdev.konsist.api.provider.KoDeclarationProvider
  *
  * @param includeNested Whether to include nested declarations.
  * @param includeLocal Whether to include local declarations.
- * @return A list containing elements.
+ * @return A list containing all declarations.
  */
 fun <T : KoDeclarationProvider> List<T>.declarations(
     includeNested: Boolean = false,
