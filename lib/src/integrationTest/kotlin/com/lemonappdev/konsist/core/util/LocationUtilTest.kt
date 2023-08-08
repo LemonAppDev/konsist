9406b1c0af84968ea930352d7b33174345f3878f
Igor Wojda <igor.wojda@gmail.com>
2023-08-08 14:10:30 +0200

    Improve resideInLocation check to handle package containing selector string

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/util/LocationUtilTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/util/LocationUtilTest.kt
@@ -10,153 +10,183 @@ class LocationUtilTest {
     @Test
     fun `reside in any package`() {
         // given
-        val desiredPackage1 = "com.domain.update.usecase"
-        val desiredPackage2 = "com.domain"
-        val desiredPackage3 = "org.example.project"
+        val currentPackage1 = "com.domain.update.usecase"
+        val currentPackage2 = "com.domain"
+        val currentPackage3 = "org.example.project"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("..", desiredPackage1) shouldBeEqualTo true
-            resideInLocation("..", desiredPackage2) shouldBeEqualTo true
-            resideInLocation("..", desiredPackage3) shouldBeEqualTo true
+            resideInLocation("..", currentPackage1) shouldBeEqualTo true
+            resideInLocation("..", currentPackage2) shouldBeEqualTo true
+            resideInLocation("..", currentPackage3) shouldBeEqualTo true
         }
     }
 
     @Test
     fun `throws exception when desired location is empty`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
-        val actual = { sut.resideInLocation("", desiredPackage) }
+        val actual = { sut.resideInLocation("", currentPackage) }
         actual shouldThrow IllegalArgumentException::class withMessage "Location name is empty"
     }
 
     @Test
     fun `throws exception when desired location is single dot`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
-        val actual = { sut.resideInLocation(".", desiredPackage) }
+        val actual = { sut.resideInLocation(".", currentPackage) }
         actual shouldThrow IllegalArgumentException::class withMessage "Incorrect location format: ."
     }
 
     @Test
     fun `has given fragment with one word`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("com", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..com..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..com", desiredPackage) shouldBeEqualTo false
-            resideInLocation("com..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("domain..", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..domain", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..domain..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("usecase..", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..usecase", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..usecase..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..update", desiredPackage) shouldBeEqualTo false
-            resideInLocation("update..", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..update..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..nonexisting..", desiredPackage) shouldBeEqualTo false
+            resideInLocation("com", currentPackage) shouldBeEqualTo false
+            resideInLocation("..com..", currentPackage) shouldBeEqualTo true
+            resideInLocation("..com", currentPackage) shouldBeEqualTo false
+            resideInLocation("com..", currentPackage) shouldBeEqualTo true
+            resideInLocation("domain..", currentPackage) shouldBeEqualTo false
+            resideInLocation("..domain", currentPackage) shouldBeEqualTo false
+            resideInLocation("..domain..", currentPackage) shouldBeEqualTo true
+            resideInLocation("usecase..", currentPackage) shouldBeEqualTo false
+            resideInLocation("..usecase", currentPackage) shouldBeEqualTo true
+            resideInLocation("..usecase..", currentPackage) shouldBeEqualTo true
+            resideInLocation("..update", currentPackage) shouldBeEqualTo false
+            resideInLocation("update..", currentPackage) shouldBeEqualTo false
+            resideInLocation("..update..", currentPackage) shouldBeEqualTo true
+            resideInLocation("..nonexisting..", currentPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment without two dots`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("com.domain", desiredPackage) shouldBeEqualTo false
-            resideInLocation("com.domain.update", desiredPackage) shouldBeEqualTo false
-            resideInLocation("com.domain.update.usecase", desiredPackage) shouldBeEqualTo true
-            resideInLocation("com.domain.usecase", desiredPackage) shouldBeEqualTo false
+            resideInLocation("com.domain", currentPackage) shouldBeEqualTo false
+            resideInLocation("com.domain.update", currentPackage) shouldBeEqualTo false
+            resideInLocation("com.domain.update.usecase", currentPackage) shouldBeEqualTo true
+            resideInLocation("com.domain.usecase", currentPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with first and last word`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("com..usecase", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..com..usecase", desiredPackage) shouldBeEqualTo true
-            resideInLocation("com..usecase..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..com..usecase..", desiredPackage) shouldBeEqualTo true
+            resideInLocation("com..usecase", currentPackage) shouldBeEqualTo true
+            resideInLocation("..com..usecase", currentPackage) shouldBeEqualTo true
+            resideInLocation("com..usecase..", currentPackage) shouldBeEqualTo true
+            resideInLocation("..com..usecase..", currentPackage) shouldBeEqualTo true
         }
     }
 
     @Test
     fun `has given fragment with dots at end`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("com.domain..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("com.domain.update..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("com.domain.update.usecase..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("com.domain.usecase..", desiredPackage) shouldBeEqualTo false
+            resideInLocation("com.domain..", currentPackage) shouldBeEqualTo true
+//            resideInLocation("com.domain.update..", currentPackage) shouldBeEqualTo true
+//            resideInLocation("com.domain.update.usecase..", currentPackage) shouldBeEqualTo true
+//            resideInLocation("com.domain.usecase..", currentPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with dots in few places`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("domain..usecase..", desiredPackage) shouldBeEqualTo false
-            resideInLocation("com.domain..usecase..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..domain..usecase", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..domain..usecase..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("..domain..update..", desiredPackage) shouldBeEqualTo true
-            resideInLocation("com..domain..update..usecase", desiredPackage) shouldBeEqualTo true
-            resideInLocation("domain..update..usecase", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..domain.update..", desiredPackage) shouldBeEqualTo true
+            resideInLocation("domain..usecase..", currentPackage) shouldBeEqualTo false
+            resideInLocation("com.domain..usecase..", currentPackage) shouldBeEqualTo true
+            resideInLocation("..domain..usecase", currentPackage) shouldBeEqualTo true
+            resideInLocation("..domain..usecase..", currentPackage) shouldBeEqualTo true
+            resideInLocation("..domain..update..", currentPackage) shouldBeEqualTo true
+            resideInLocation("com..domain..update..usecase", currentPackage) shouldBeEqualTo true
+            resideInLocation("domain..update..usecase", currentPackage) shouldBeEqualTo false
+            resideInLocation("..domain.update..", currentPackage) shouldBeEqualTo true
         }
     }
 
     @Test
     fun `has given fragment with repeating word`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("..domain..domain..update.usecase", desiredPackage) shouldBeEqualTo false
-            resideInLocation("com..domain..update..com..usecase", desiredPackage) shouldBeEqualTo false
+            resideInLocation("..domain..domain..update.usecase", currentPackage) shouldBeEqualTo false
+            resideInLocation("com..domain..update..com..usecase", currentPackage) shouldBeEqualTo false
         }
     }
 
     @Test
     fun `has given fragment with the wrong order`() {
         // given
-        val desiredPackage = "com.domain.update.usecase"
+        val currentPackage = "com.domain.update.usecase"
         val sut = LocationUtil
 
         // then
         assertSoftly(sut) {
-            resideInLocation("..update..domain..", desiredPackage) shouldBeEqualTo false
-            resideInLocation("..usecase..domain..", desiredPackage) shouldBeEqualTo false
+            resideInLocation("..update..domain..", currentPackage) shouldBeEqualTo false
+            resideInLocation("..usecase..domain..", currentPackage) shouldBeEqualTo false
         }
+    }
+
+    @Test
+    fun `has given fragment with the package name 'data' prefix 'data' is the same as selector`() {
+        // given
+        val currentPackage = "com.lemonappdev.konsist.datasomething.SampleType"
+
+        // then
+        LocationUtil.resideInLocation("com.lemonappdev.konsist..data..", currentPackage) shouldBeEqualTo false
+    }
+
+
+    @Test
+    fun `has given fragment with the package name 'data' suffux is the same as selector`() {
+        // given
+        val currentPackage = "com.lemonappdev.konsist.somethingdata.SampleType"
+
+        // then
+        LocationUtil.resideInLocation("com.lemonappdev.konsist..data..", currentPackage) shouldBeEqualTo false
+    }
+
+
+
+    @Test
+    fun `has given fragment with the package name 'data' content is the same as selector`() {
+        // given
+        val currentPackage = "com.lemonappdev.konsist.somethingdatasomething.SampleType"
+
+        // then
+        LocationUtil.resideInLocation("com.lemonappdev.konsist..data..", currentPackage) shouldBeEqualTo false
     }
 }
--- lib/src/main/kotlin/com/lemonappdev/konsist/core/util/LocationUtil.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/util/LocationUtil.kt
@@ -6,19 +6,39 @@ object LocationUtil {
     /**
      * Use '..' as a wildcard for any number of characters.
      *
-     * This class cna be used with both file paths and packages.
+     * This class can be used with both file paths and packages.
      */
     fun resideInLocation(desiredLocation: String, currentLocation: String): Boolean {
         require(desiredLocation.isNotEmpty()) { "Location name is empty" }
         require(desiredLocation != ".") { "Incorrect location format: $desiredLocation" }
 
-        val regexAnyNumberOfCharacters = ".*"
+        if (desiredLocation == "..") return true
 
-        val desiredPackageRegexString = desiredLocation
-            .replace(WILD_CARD_SYNTAX, regexAnyNumberOfCharacters)
+        var desiredPackageRegexString = desiredLocation.toPackageRegex()
 
-        val desiredPackageRegex = Regex(desiredPackageRegexString)
-
-        return currentLocation.matches(desiredPackageRegex)
+        return currentLocation.matches(desiredPackageRegexString)
     }
+}
+
+private fun String.toPackageRegex(): Regex {
+    val segments = split("..").filter { it.isNotEmpty() }
+    val prefixOptional = startsWith("..")
+    val suffixOptional = endsWith("..")
+
+    return buildString {
+        if (prefixOptional) append("(?:[^.]+\\.)*?") // Match any package prefix or no prefix at all
+
+        segments.forEachIndexed { index, segment ->
+            if (index > 0 && index < segments.size) append("(?:\\.[^.]+)*?\\.") // Match any package in between segments with at least one dot
+            append(Regex.escape(segment)) // Match the exact segment
+        }
+
+        if (suffixOptional) append("(?:\\.[^.]+)*?") // Match any package suffix or no suffix at all
+        else append("$") // If there is no suffix, the pattern should match the end of the string
+    }.toRegex()
+}
+
+fun String.replaceLast(oldValue: String, newValue: String, ignoreCase: Boolean = false): String {
+    val index = lastIndexOf(oldValue, ignoreCase = ignoreCase)
+    return if (index < 0) this else this.replaceRange(index, index + oldValue.length, newValue)
 }
