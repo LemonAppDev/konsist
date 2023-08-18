f3316661b8b38d9a328ef0df629a099a0525b13f
natalia.peterwas <n.peterwas@gmail.com>
2023-08-18 18:24:02 +0200

    remove `explicit` word

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoPropertyTypeProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoPropertyTypeProvider.kt
@@ -3,16 +3,16 @@ package com.lemonappdev.konsist.api.provider
 import com.lemonappdev.konsist.api.declaration.KoTypeDeclaration
 
 /**
- * An interface representing a Kotlin declaration that provides access to the explicit type information.
+ * An interface representing a Kotlin declaration that provides access to the type information.
  */
 interface KoPropertyTypeProvider : KoBaseProvider {
     /**
-     * Declaration explicit type.
+     * Declaration type.
      */
     val type: KoTypeDeclaration?
 
     /**
-     * Whatever declaration has an explicit type.
+     * Whatever declaration has a type.
      *
      * @param name the type name to check for (optional).
      * @return `true` if the declaration has the specified type (or any type if [name] is `null`), `false` otherwise.
