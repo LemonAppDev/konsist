c214cd17b23cec77cff01104144c07fe40039f3e
Igor Wojda <igor.wojda@gmail.com>
2023-08-19 13:54:43 +0200

    Release v0.10.0 (#359)
    * Upd version to 0.10.0
    
    * Update starter project version

--- samples/starter-projects/konsist-starter-android-gradle-kotlin-junit-5/konsistTest/build.gradle.kts
+++ samples/starter-projects/konsist-starter-android-gradle-kotlin-junit-5/konsistTest/build.gradle.kts
@@ -37,5 +37,5 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 
-    testImplementation("com.lemonappdev:konsist:0.9.0")
+    testImplementation("com.lemonappdev:konsist:0.10.0-SNAPSHOT")
 }
