4728a8ace8fc4e7cdc385c71d88bc272c55751f7
natalia.peterwas <n.peterwas@gmail.com>
2023-08-22 10:38:23 +0200

    add every extension has a receiver tag tests

--- lib/src/snippet/kotlin/com/lemonappdev/konsist/LibrarySnippets.kt
+++ lib/src/snippet/kotlin/com/lemonappdev/konsist/LibrarySnippets.kt
@@ -2,7 +2,11 @@ package com.lemonappdev.konsist
 
 import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.ext.koscope.declarationsOf
+import com.lemonappdev.konsist.api.ext.provider.hasValidParameterKDoc
+import com.lemonappdev.konsist.api.ext.provider.hasValidReceiverTypeKDoc
+import com.lemonappdev.konsist.api.ext.provider.hasValidReturnTypeKDoc
 import com.lemonappdev.konsist.api.provider.KoKDocProvider
+import com.lemonappdev.konsist.api.provider.KoReceiverTypeProvider
 import com.lemonappdev.konsist.api.verify.assert
 
 class LibrarySnippets {
@@ -10,6 +14,35 @@ class LibrarySnippets {
         Konsist.scopeFromPackage("..api..")
             .declarationsOf<KoKDocProvider>(includeNested = true)
             .assert { it.hasKDoc }
+    }
+
+    fun `every function with parameters has a param tags`() {
+        Konsist.scopeFromPackage("..api..")
+            .functions(includeNested = true)
+            .assert { it.hasValidParameterKDoc() }
+    }
+
+    fun `every function with return value has a return tag`() {
+        Konsist.scopeFromPackage("..api..")
+            .functions(includeNested = true)
+            .assert { it.hasValidReturnTypeKDoc() }
+    }
+
+    fun `option 1 - every extension has a receiver tag`() {
+        val scope1 = Konsist.scopeFromPackage("..api..")
+            .functions(includeNested = true, includeLocal = true)
+
+        val scope2 = Konsist.scopeFromPackage("..api..")
+            .properties(includeNested = true, includeLocal = true)
+
+        (scope1 + scope2)
+            .assert { it.hasValidReceiverTypeKDoc() }
+    }
+
+    fun `option 2 - every extension has a receiver tag`() {
+        Konsist.scopeFromPackage("..api..")
+            .declarationsOf<KoReceiverTypeProvider>()
+            .assert { it is KoKDocProvider && it.hasValidReceiverTypeKDoc() }
     }
 
     fun `every public function in api package must have explicit return type`() {
