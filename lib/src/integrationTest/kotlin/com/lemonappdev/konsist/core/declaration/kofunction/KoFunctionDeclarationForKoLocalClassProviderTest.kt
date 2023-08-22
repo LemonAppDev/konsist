b43b5b0d48042adc2b08a79d3e6749bf4f19384b
natalia.peterwas <n.peterwas@gmail.com>
2023-08-22 11:19:45 +0200

    add tests for KoLocalClassProvider

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofunction/KoFunctionDeclarationForKoLocalClassProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofunction/KoFunctionDeclarationForKoLocalClassProviderTest.kt
@@ -6,38 +6,40 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 class KoFunctionDeclarationForKoLocalClassProviderTest {
-//    @Test
-//    fun `function-contains-no-local-classes`() {
-//        // given
-//        val sut = getSnippetFile("function-contains-no-local-classes")
-//            .functions()
-//            .first()
-//
-//        // then
-//        assertSoftly(sut) {
-//            numLocalClasses shouldBeEqualTo 0
-//            containsLocalClass("SampleClass") shouldBeEqualTo false
-//            localClasses
-//                .shouldBeEqualTo(emptyList())
-//        }
-//    }
-//
-//    @Test
-//    fun `function-contains-local-class`() {
-//        // given
-//        val sut = getSnippetFile("function-contains-local-class")
-//            .functions()
-//            .first()
-//
-//        // then
-//        assertSoftly(sut) {
-//            numLocalClasses shouldBeEqualTo 1
-//            containsLocalClass("SampleClass") shouldBeEqualTo true
-//            localClasses
-//                .map { it.name }
-//                .shouldBeEqualTo(listOf("SampleClass"))
-//        }
-//    }
+    @Test
+    fun `function-contains-no-local-classes`() {
+        // given
+        val sut = getSnippetFile("function-contains-no-local-classes")
+            .functions()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            numLocalClasses shouldBeEqualTo 0
+            countLocalClasses { it.name == "SampleClass" } shouldBeEqualTo 0
+            containsLocalClass { it.name == "SampleClass" } shouldBeEqualTo false
+            localClasses shouldBeEqualTo emptyList()
+        }
+    }
+
+    @Test
+    fun `function-contains-local-class`() {
+        // given
+        val sut = getSnippetFile("function-contains-local-class")
+            .functions()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            numLocalClasses shouldBeEqualTo 2
+            countLocalClasses { it.name == "SampleClass1" } shouldBeEqualTo 1
+            containsLocalClass { it.name == "SampleClass1"} shouldBeEqualTo true
+            containsLocalClass { it.name == "OtherClass"} shouldBeEqualTo false
+            localClasses
+                .map { it.name }
+                .shouldBeEqualTo(listOf("SampleClass1", "SampleClass2" ))
+        }
+    }
 
     private fun getSnippetFile(fileName: String) =
         getSnippetKoScope("core/declaration/kofunction/snippet/forkolocalclassprovider/", fileName)
--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koinitblock/KoInitBlockDeclarationForKoLocalClassProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koinitblock/KoInitBlockDeclarationForKoLocalClassProviderTest.kt
@@ -6,42 +6,44 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 class KoInitBlockDeclarationForKoLocalClassProviderTest {
-//    @Test
-//    fun `init-block-contains-no-local-classes`() {
-//        // given
-//        val sut = getSnippetFile("init-block-contains-no-local-classes")
-//            .classes()
-//            .first()
-//            .initBlocks
-//            .first()
-//
-//        // then
-//        assertSoftly(sut) {
-//            numLocalClasses shouldBeEqualTo 0
-//            containsLocalClass("SampleLocalClass") shouldBeEqualTo false
-//            localClasses
-//                .shouldBeEqualTo(emptyList())
-//        }
-//    }
-//
-//    @Test
-//    fun `init-block-contains-local-class`() {
-//        // given
-//        val sut = getSnippetFile("init-block-contains-local-class")
-//            .classes()
-//            .first()
-//            .initBlocks
-//            .first()
-//
-//        // then
-//        assertSoftly(sut) {
-//            numLocalClasses shouldBeEqualTo 1
-//            containsLocalClass("SampleLocalClass") shouldBeEqualTo true
-//            localClasses
-//                .map { it.name }
-//                .shouldBeEqualTo(listOf("SampleLocalClass"))
-//        }
-//    }
+    @Test
+    fun `init-block-contains-no-local-classes`() {
+        // given
+        val sut = getSnippetFile("init-block-contains-no-local-classes")
+            .classes()
+            .first()
+            .initBlocks
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            numLocalClasses shouldBeEqualTo 0
+            countLocalClasses { it.name == "SampleLocalClass" } shouldBeEqualTo 0
+            containsLocalClass { it.name == "SampleLocalClass" } shouldBeEqualTo false
+            localClasses shouldBeEqualTo emptyList()
+        }
+    }
+
+    @Test
+    fun `init-block-contains-local-class`() {
+        // given
+        val sut = getSnippetFile("init-block-contains-local-class")
+            .classes()
+            .first()
+            .initBlocks
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            numLocalClasses shouldBeEqualTo 2
+            countLocalClasses { it.name == "SampleLocalClass1" } shouldBeEqualTo 1
+            containsLocalClass { it.name == "SampleLocalClass1"} shouldBeEqualTo true
+            containsLocalClass { it.name == "OtherClass"} shouldBeEqualTo false
+            localClasses
+                .map { it.name }
+                .shouldBeEqualTo(listOf("SampleLocalClass1", "SampleLocalClass2"))
+        }
+    }
 
     private fun getSnippetFile(fileName: String) =
         getSnippetKoScope("core/declaration/koinitblock/snippet/forkolocalclassprovider/", fileName)
