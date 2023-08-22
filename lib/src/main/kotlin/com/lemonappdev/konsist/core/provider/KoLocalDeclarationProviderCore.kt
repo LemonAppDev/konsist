69e714fd477a392b3dfe5d07cd1146e7e57be2c1
natalia.peterwas <n.peterwas@gmail.com>
2023-08-22 11:33:06 +0200

    add lambda param to KoLocalDeclarationProvider and tests for them

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoLocalDeclarationProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoLocalDeclarationProviderCore.kt
@@ -1,5 +1,7 @@
 package com.lemonappdev.konsist.core.provider
 
+import com.lemonappdev.konsist.api.declaration.KoBaseDeclaration
+import com.lemonappdev.konsist.api.declaration.KoClassDeclaration
 import com.lemonappdev.konsist.api.provider.KoLocalDeclarationProvider
 import com.lemonappdev.konsist.api.provider.KoNameProvider
 
@@ -7,5 +9,9 @@ internal interface KoLocalDeclarationProviderCore : KoLocalDeclarationProvider, 
     override val numLocalDeclarations: Int
         get() = localDeclarations.size
 
-    override fun containsLocalDeclaration(name: String): Boolean = localDeclarations.any { (it as? KoNameProvider)?.name == name }
+    override fun countLocalDeclarations(predicate: (KoBaseDeclaration) -> Boolean): Int =
+        localDeclarations.count { predicate(it) }
+
+    override fun containsLocalDeclaration(predicate: (KoBaseDeclaration) -> Boolean): Boolean =
+        localDeclarations.any { predicate(it) }
 }
