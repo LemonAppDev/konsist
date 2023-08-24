aab07ffc82798033cf28477c80e168f879043326
natalia.peterwas <n.peterwas@gmail.com>
2023-08-24 11:06:25 +0200

    upd script

--- scripts/update-snippets.py
+++ scripts/update-snippets.py
@@ -2,6 +2,7 @@ import os
 
 # Get the path of the current script
 script_path = os.path.abspath(__file__)
+print(script_path)
 
 # Get the directory containing the script
 script_directory = os.path.dirname(script_path)
