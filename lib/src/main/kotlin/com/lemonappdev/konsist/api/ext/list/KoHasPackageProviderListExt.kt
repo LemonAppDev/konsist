fee7d675bda02c9ec1fcaf4a723980de4a0b8e50
natalia.peterwas <n.peterwas@gmail.com>
2023-08-25 10:52:54 +0200

    add extensions

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoHasPackageProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoHasPackageProviderListExt.kt
@@ -1,7 +1,22 @@
 package com.lemonappdev.konsist.api.ext.list
 
 import com.lemonappdev.konsist.api.provider.KoHasPackageProvider
+import com.lemonappdev.konsist.api.provider.KoPackageMatchingPathProvider
 import com.lemonappdev.konsist.api.provider.KoPackageProvider
+
+/**
+ * List containing elements with a matching package.
+ *
+ * @return A list containing elements with a matching package.
+ */
+fun <T : KoHasPackageProvider> List<T>.withMatchingPackage(): List<T> = filter { it.hasMatchingPackage }
+
+/**
+ * List containing elements without a matching package.
+ *
+ * @return A list containing elements without a matching package.
+ */
+fun <T : KoHasPackageProvider> List<T>.withoutMatchingPackage(): List<T> = filterNot { it.hasMatchingPackage }
 
 /**
  * List containing elements with package.
