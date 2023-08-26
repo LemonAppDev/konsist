bcea0a677c175f4ddf240b9c30e9c8efad3c7645
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-08-26 19:34:59 +0200

    unify formatting code (#380)

--- lib/src/snippet/kotlin/com/lemonappdev/konsist/LibrarySnippets.kt
+++ lib/src/snippet/kotlin/com/lemonappdev/konsist/LibrarySnippets.kt
@@ -7,19 +7,22 @@ import com.lemonappdev.konsist.api.verify.assert
 
 class LibrarySnippets {
     fun `every api declaration has KDoc`() {
-        Konsist.scopeFromPackage("..api..")
+        Konsist
+            .scopeFromPackage("..api..")
             .declarationsOf<KoKDocProvider>(includeNested = true)
             .assert { it.hasKDoc }
     }
 
     fun `every public function in api package must have explicit return type`() {
-        Konsist.scopeFromPackage("..api..")
+        Konsist
+            .scopeFromPackage("..api..")
             .functions(includeNested = true)
             .assert { it.hasReturnType }
     }
 
     fun `every public property in api package must have specify type explicitly`() {
-        Konsist.scopeFromPackage("..api..")
+        Konsist
+            .scopeFromPackage("..api..")
             .properties(includeNested = true)
             .assert { it.hasType() }
     }
