49e1af6fb97e015fc5f86165836c319a529b8a13
natalia.peterwas <n.peterwas@gmail.com>
2023-08-28 12:48:48 +0200

    upd android and spring comment

--- scripts/update-snippets.py
+++ scripts/update-snippets.py
@@ -24,6 +24,18 @@ def upd_file_text(txt):
         # Replace capital letters with a blank space
         modified_string = re.sub(r'[A-Z]', replace, input_string)
 
+        if modified_string == " Android Snippets":
+            android_text = """
+        Konsist can be used to guard the consistency of the [Android](https://www.android.com/) project.&#x20;
+
+        {% hint style="info" %}
+        The [android-showcase](https://github.com/igorwojda/android-showcase) project contains set of kotnsist tests.
+        {% endhint %}"""
+            text = modified_string + android_text
+        if modified_string == " Spring Snippets":
+            spring_text = "\nKonsist can be used to guard the consistency of the [Spring](https://spring.io/) project."
+            text = modified_string + spring_text
+
         return "#" + modified_string + "\n\n"
 
     # formatting: # class name
