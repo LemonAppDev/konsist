b621054997093f9fc759ac3c19d2d8eb1c4c6148
natalia.peterwas <n.peterwas@gmail.com>
2023-08-30 11:26:28 +0200

    fix spotless

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoReturnStatementsProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoReturnStatementsProviderCore.kt
@@ -2,7 +2,6 @@ package com.lemonappdev.konsist.core.provider
 
 import com.lemonappdev.konsist.api.provider.KoReturnStatementsProvider
 import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtFunction
 
 internal interface KoReturnStatementsProviderCore : KoReturnStatementsProvider {
     val ktElement: KtElement
