7ec4bb87ad0a69244ed87a12540f3a7788e3e840
natalia.peterwas <n.peterwas@gmail.com>
2023-08-30 11:04:12 +0200

    Upd message in annotation

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoHasTestProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoHasTestProvider.kt
@@ -3,7 +3,7 @@ package com.lemonappdev.konsist.api.provider
 /**
  * An interface representing a Kotlin declaration that provides information about whether it has a test.
  */
-@Deprecated("Will be removed from v1.0.0", ReplaceWith("KoHasTestClassProvider"))
+@Deprecated("Will be removed in v1.0.0", ReplaceWith("KoHasTestClassProvider"))
 interface KoHasTestProvider : KoBaseProvider {
     /**
      * Whatever declaration has a Test.
@@ -13,7 +13,7 @@ interface KoHasTestProvider : KoBaseProvider {
      * @param sourceSetName      the name of the source set to check (optional).
      * @return `true` if the declaration has a test, `false` otherwise.
      */
-    @Deprecated("Will be removed from v1.0.0", ReplaceWith("hasTestClass()"))
+    @Deprecated("Will be removed in v1.0.0", ReplaceWith("hasTestClass()"))
     fun hasTest(
         testFileNameSuffix: String = "Test",
         moduleName: String? = null,
--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoHasTestProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoHasTestProviderCore.kt
@@ -1,9 +1,9 @@
 package com.lemonappdev.konsist.core.provider
 
 import com.lemonappdev.konsist.api.provider.KoHasTestProvider
-@Deprecated("Will be removed from v1.0.0", ReplaceWith("KoHasTestClassProviderCore"))
+@Deprecated("Will be removed in v1.0.0", ReplaceWith("KoHasTestClassProviderCore"))
 internal interface KoHasTestProviderCore : KoHasTestProvider, KoHasTestClassProviderCore {
-    @Deprecated("Will be removed from v1.0.0", replaceWith = ReplaceWith("hasTestClass()"))
+    @Deprecated("Will be removed in v1.0.0", replaceWith = ReplaceWith("hasTestClass()"))
     override fun hasTest(testFileNameSuffix: String, moduleName: String?, sourceSetName: String?): Boolean =
         hasTestClass(testFileNameSuffix, moduleName, sourceSetName)
 }
