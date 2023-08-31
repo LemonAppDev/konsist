9f0560cc7bad7fa23bb505e1e6a6627c92fbe6ed
natalia.peterwas <n.peterwas@gmail.com>
2023-08-31 11:05:43 +0200

    Add tests

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/provider/KoPropertyProviderExt.kt
@@ -0,0 +1,16 @@
+package com.lemonappdev.konsist.api.ext.provider
+
+import com.lemonappdev.konsist.api.provider.KoKDocProvider
+import com.lemonappdev.konsist.api.provider.KoPropertyProvider
+
+/**
+ * Whether declaration has a valid KDoc with a PROPERTY tag.
+ *
+ * @return `true` if the declaration has a valid KDoc with the PROPERTY tag, `false` otherwise.
+ */
+fun <T : KoPropertyProvider> T.hasValidKDocPropertyTags(): Boolean = if (properties().isNotEmpty()) {
+    (properties().count() == (this as? KoKDocProvider)?.kDoc?.propertyTags?.count()) &&
+            (properties().map { it.name } == kDoc?.propertyTags?.map { it.value })
+} else {
+    true
+}
