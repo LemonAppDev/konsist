3d5b3218c0abf79170b7924cbe1e732cfa36f060
Igor Wojda <igor.wojda@gmail.com>
2023-09-01 09:17:03 +0200

    Update KDoc (#411)

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/provider/KoRepresentsTypeProviderExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/provider/KoRepresentsTypeProviderExt.kt
@@ -5,6 +5,11 @@ import com.lemonappdev.konsist.api.provider.KoRepresentsTypeProvider
 /**
  * Returns `true` if declaration represents the type of [T].
  *
+ * Due to type erasure, avoid passing generics as type arguments because they won't be accurately represented at runtime.
+ * For generics use representsType method instead and pass the string representing the type:
+ * No: representsTypeOf<List<String>>()
+ * Yes: representsType("List<String>")
+ *
  * @return `true` if declaration represents the type of [T], `false` otherwise.
  */
 inline fun <reified T> KoRepresentsTypeProvider.representsTypeOf(): Boolean =
