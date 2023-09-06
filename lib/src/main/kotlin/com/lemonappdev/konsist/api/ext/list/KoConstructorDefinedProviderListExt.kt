c0e5a1dd4df3aa2321aafd1f0f496cb437ac2da7
natalia.peterwas <n.peterwas@gmail.com>
2023-09-06 17:01:46 +0200

    add extensions for KoConstructorDefinedProvider

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoConstructorDefinedProviderListExt.kt
@@ -0,0 +1,18 @@
+package com.lemonappdev.konsist.api.ext.list
+
+import com.lemonappdev.konsist.api.provider.KoConstructorDefinedProvider
+import com.lemonappdev.konsist.api.provider.KoGenericTypeProvider
+
+/**
+ * List containing elements defined within a constructor.
+ *
+ * @return A list containing elements defined within a constructor.
+ */
+fun <T : KoConstructorDefinedProvider> List<T>.withConstructorDefined(): List<T> = filter { it.isConstructorDefined }
+
+/**
+ * List containing elements defined outside a constructor.
+ *
+ * @return A list containing elements defined outside a constructor.
+ */
+fun <T : KoConstructorDefinedProvider> List<T>.withoutConstructorDefined(): List<T> = filterNot { it.isConstructorDefined }
