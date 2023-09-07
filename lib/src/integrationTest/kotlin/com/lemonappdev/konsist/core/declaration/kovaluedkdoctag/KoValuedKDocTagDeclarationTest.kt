84f0defb4d248ddf53b3e9c002c9e91a31be5c46
natalia.peterwas <n.peterwas@gmail.com>
2023-09-07 11:26:33 +0200

    add konsist test and add missing `toString` implementation

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kovaluedkdoctag/KoValuedKDocTagDeclarationTest.kt
@@ -0,0 +1,25 @@
+package com.lemonappdev.konsist.core.declaration.kovaluedkdoctag
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import com.lemonappdev.konsist.api.ext.list.kDocs
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoValuedKDocTagDeclarationTest {
+    @Test
+    fun `kdoc-valued-tag-to-string`() {
+        // given
+        val sut = getSnippetFile("kdoc-valued-tag-to-string")
+            .functions()
+            .kDocs
+            .first()
+            .paramTags
+            .first()
+
+        // then
+        sut.toString() shouldBeEqualTo "@param sampleParameter sample text"
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/kovaluedkdoctag/snippet/forgeneral/", fileName)
+}
--- lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoKDocTagDeclaration.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoKDocTagDeclaration.kt
@@ -1,8 +1,12 @@
 package com.lemonappdev.konsist.api.declaration
 
+import com.lemonappdev.konsist.api.provider.KoBaseProvider
 import com.lemonappdev.konsist.api.provider.KoKDocTagNameAndDescriptionProvider
 
 /**
  * Represents a Kotlin documentation tag declaration.
  */
-interface KoKDocTagDeclaration : KoKDocTagNameAndDescriptionProvider
+interface KoKDocTagDeclaration :
+    KoKDocTagNameAndDescriptionProvider,
+    KoBaseDeclaration,
+    KoBaseProvider
