bf30100f9695039ffe46b2c16fd7f38bab98ffb7
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-09-07 12:29:40 +0200

    add konsist test and add missing `toString` implementation (#459)

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kokdoctag/KoKDocTagDeclarationTest.kt
@@ -0,0 +1,25 @@
+package com.lemonappdev.konsist.core.declaration.kokdoctag
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import com.lemonappdev.konsist.api.ext.list.kDocs
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoKDocTagDeclarationTest {
+    @Test
+    fun `kdoc-tag-to-string`() {
+        // given
+        val sut = getSnippetFile("kdoc-tag-to-string")
+            .functions()
+            .kDocs
+            .first()
+            .tags
+            .first()
+
+        // then
+        sut.toString() shouldBeEqualTo "@return sample text"
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/kokdoctag/snippet/forgeneral/", fileName)
+}
