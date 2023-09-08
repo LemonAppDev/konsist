c85299f48a8c185c28950991bacf43f49da7de55
natalia.peterwas <n.peterwas@gmail.com>
2023-09-08 10:32:50 +0200

    upd KoReturnTypeProvider

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofunction/KoFunctionDeclarationForKoReturnTypeProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofunction/KoFunctionDeclarationForKoReturnTypeProviderTest.kt
@@ -1,64 +1,13 @@
 package com.lemonappdev.konsist.core.declaration.kofunction
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import com.lemonappdev.konsist.testdata.SampleType
+import net.bytebuddy.matcher.ElementMatchers.hasType
 import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 class KoFunctionDeclarationForKoReturnTypeProviderTest {
-    @Test
-    fun `function-return-type`() {
-        // given
-        val sut = getSnippetFile("function-return-type")
-            .functions()
-            .first()
-
-        // then
-        assertSoftly(sut) {
-            hasReturnType shouldBeEqualTo true
-            returnType?.sourceType shouldBeEqualTo "SampleType"
-            returnType?.aliasType shouldBeEqualTo null
-            returnType?.name shouldBeEqualTo "SampleType"
-            returnType?.isAlias shouldBeEqualTo false
-            returnType?.fullyQualifiedName shouldBeEqualTo "com.lemonappdev.konsist.testdata.SampleType"
-        }
-    }
-
-    @Test
-    fun `function-return-import-alias`() {
-        // given
-        val sut = getSnippetFile("function-return-import-alias")
-            .functions()
-            .first()
-
-        // then
-        assertSoftly(sut) {
-            hasReturnType shouldBeEqualTo true
-            returnType?.sourceType shouldBeEqualTo "SampleType"
-            returnType?.aliasType shouldBeEqualTo "ImportAlias"
-            returnType?.name shouldBeEqualTo "ImportAlias"
-            returnType?.isAlias shouldBeEqualTo true
-        }
-    }
-
-    @Test
-    fun `extension-function-return-type`() {
-        // given
-        val sut = getSnippetFile("extension-function-return-type")
-            .functions()
-            .first()
-
-        // then
-        assertSoftly(sut) {
-            hasReturnType shouldBeEqualTo true
-            returnType?.sourceType shouldBeEqualTo "SampleType"
-            returnType?.aliasType shouldBeEqualTo null
-            returnType?.name shouldBeEqualTo "SampleType"
-            returnType?.isAlias shouldBeEqualTo false
-            returnType?.fullyQualifiedName shouldBeEqualTo "com.lemonappdev.konsist.testdata.SampleType"
-        }
-    }
-
     @Test
     fun `function-not-return-type`() {
         // given
@@ -69,10 +18,10 @@ class KoFunctionDeclarationForKoReturnTypeProviderTest {
         // then
         assertSoftly(sut) {
             hasReturnType shouldBeEqualTo false
-            returnType?.sourceType shouldBeEqualTo null
-            returnType?.aliasType shouldBeEqualTo null
-            returnType?.name shouldBeEqualTo null
-            returnType?.isAlias shouldBeEqualTo null
+            returnType shouldBeEqualTo null
+            hasReturnType() shouldBeEqualTo false
+            hasReturnType { it.name == "String" } shouldBeEqualTo false
+            hasReturnTypeOf(String::class) shouldBeEqualTo false
         }
     }
 
@@ -86,10 +35,67 @@ class KoFunctionDeclarationForKoReturnTypeProviderTest {
         // then
         assertSoftly(sut) {
             hasReturnType shouldBeEqualTo false
-            returnType?.sourceType shouldBeEqualTo null
-            returnType?.aliasType shouldBeEqualTo null
-            returnType?.name shouldBeEqualTo null
-            returnType?.isAlias shouldBeEqualTo null
+            returnType shouldBeEqualTo null
+            hasReturnType() shouldBeEqualTo false
+            hasReturnType { it.name == "String" } shouldBeEqualTo false
+            hasReturnTypeOf(String::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `function-return-type`() {
+        // given
+        val sut = getSnippetFile("function-return-type")
+            .functions()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasReturnType shouldBeEqualTo true
+            returnType?.name shouldBeEqualTo "SampleType"
+            hasReturnType() shouldBeEqualTo true
+            hasReturnType { it.name == "SampleType" } shouldBeEqualTo true
+            hasReturnType { it.name == "Int" } shouldBeEqualTo false
+            hasReturnTypeOf(SampleType::class) shouldBeEqualTo true
+            hasReturnTypeOf(Int::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `function-return-import-alias`() {
+        // given
+        val sut = getSnippetFile("function-return-import-alias")
+            .functions()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasReturnType shouldBeEqualTo true
+            returnType?.name shouldBeEqualTo "ImportAlias"
+            hasReturnType() shouldBeEqualTo true
+            hasReturnType { it.name == "ImportAlias" } shouldBeEqualTo true
+            hasReturnType { it.name == "Int" } shouldBeEqualTo false
+            hasReturnTypeOf(SampleType::class) shouldBeEqualTo false
+            hasReturnTypeOf(Int::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `extension-function-return-type`() {
+        // given
+        val sut = getSnippetFile("extension-function-return-type")
+            .functions()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasReturnType shouldBeEqualTo true
+            returnType?.name shouldBeEqualTo "SampleType"
+            hasReturnType() shouldBeEqualTo true
+            hasReturnType { it.name == "SampleType" } shouldBeEqualTo true
+            hasReturnType { it.name == "Int" } shouldBeEqualTo false
+            hasReturnTypeOf(SampleType::class) shouldBeEqualTo true
+            hasReturnTypeOf(Int::class) shouldBeEqualTo false
         }
     }
 
