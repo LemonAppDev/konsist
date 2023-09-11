2a514d1aaa020484548eb63e5f0760dca18e672e
natalia.peterwas <n.peterwas@gmail.com>
2023-09-11 19:01:10 +0200

    add KDoc

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoMatchesProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoMatchesProvider.kt
@@ -1,5 +1,14 @@
 package com.lemonappdev.konsist.api.provider
 
+/**
+ * An interface representing a Kotlin declaration that provides information about whether it matches a specified element.
+ */
 interface KoMatchesProvider: KoBaseProvider {
+    /**
+     *  Whether the given [element] matches to declaration.
+     *
+     * @param element The element to be matched, typically represented as a `String`.
+     * @return `true` if the [element] matches the declaration, `false` otherwise.
+     */
     fun matches(element: String): Boolean
 }
