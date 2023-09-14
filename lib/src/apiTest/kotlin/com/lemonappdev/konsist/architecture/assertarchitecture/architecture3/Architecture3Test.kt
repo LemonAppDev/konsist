adaf63c60885a519c59b3ca835eafa323f1a0cd1
natalia.peterwas <n.peterwas@gmail.com>
2023-09-14 13:16:38 +0200

    fix spotless and detekt

--- lib/src/apiTest/kotlin/com/lemonappdev/konsist/architecture/assertarchitecture/architecture3/Architecture3Test.kt
+++ lib/src/apiTest/kotlin/com/lemonappdev/konsist/architecture/assertarchitecture/architecture3/Architecture3Test.kt
@@ -4,14 +4,9 @@ import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.architecture.KoArchitectureCreator.architecture
 import com.lemonappdev.konsist.api.architecture.KoArchitectureCreator.assertArchitecture
 import com.lemonappdev.konsist.api.architecture.Layer
-import com.lemonappdev.konsist.core.exception.KoPreconditionFailedException
-import com.lemonappdev.konsist.core.filesystem.PathProvider
-import org.amshove.kluent.shouldThrow
-import org.amshove.kluent.withMessage
 import org.junit.jupiter.api.Test
 
 class Architecture3Test {
-    private val rootPath = PathProvider.getInstance().rootProjectPath
     private val domain =
         Layer("Domain", "com.lemonappdev.konsist.architecture.assertarchitecture.architecture3.project.domain..")
     private val presentation =
@@ -34,7 +29,7 @@ class Architecture3Test {
     }
 
     @Test
-    fun `passes when dependency is set that presentation and domain layers are dependent on each other and architecture is passed as parameter`() {
+    fun `passes when dependency is set that two layers are dependent on each other and architecture is passed as parameter`() {
         // given
         val architecture = architecture {
             domain.dependsOn(presentation)
