aab5dec288514dab209eca6feaacad4a91c9915a
Jonathan <jfe.sarco@gmail.com>
2023-09-19 19:55:22 +0200

    draft using stack walker plus jvmChain

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/verify/CommonAssert.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/verify/CommonAssert.kt
@@ -8,21 +8,35 @@ private const val INDEX_SEVEN = 7
 /**
  * In this call stack hierarchy test name is at index 4.
  */
-internal fun getTestMethodNameFromFourthIndex() = getTestMethodName(INDEX_FOUR)
+internal fun getTestMethodNameFromFourthIndex() = getTestMethodNameWithStackWalker(INDEX_FOUR)
 
 /**
  * In this call stack hierarchy test name is at index 5.
  */
-internal fun getTestMethodNameFromFifthIndex() = getTestMethodName(INDEX_FIVE)
+internal fun getTestMethodNameFromFifthIndex() = getTestMethodNameWithStackWalker(INDEX_FIVE)
 
 /**
  * In this call stack hierarchy test name is at index 5.
  */
-internal fun getTestMethodNameFromSixthIndex() = getTestMethodName(INDEX_SIX)
+internal fun getTestMethodNameFromSixthIndex() = getTestMethodNameWithStackWalker(INDEX_SIX)
 
 /**
  * In this call stack hierarchy test name is at index 7.
  */
-internal fun getTestMethodNameFromSeventhIndex() = getTestMethodName(INDEX_SEVEN)
+internal fun getTestMethodNameFromSeventhIndex() = getTestMethodNameWithStackWalker(INDEX_SEVEN)
 
+/**
+ * Use StalkWalker to get the testMethodName
+ */
+private fun getTestMethodNameWithStackWalker(index: Int): String =
+    StackWalker.getInstance()
+        .walk {
+            it.skip(index.toLong() - 1).findFirst().get()
+        }
+        .methodName
+
+@Deprecated(message = "Using StackWalker",
+    replaceWith = ReplaceWith("getTestMethodNameWithStackWalker")
+)
+@Suppress("UnusedPrivateMember")
 private fun getTestMethodName(index: Int): String = Thread.currentThread().stackTrace[index].methodName
