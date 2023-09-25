0fded52cc29a4341d9632f4713c73d3c39e30b49
Igor Wojda <igor.wojda@gmail.com>
2023-09-26 01:48:56 +0200

    Update comment

--- buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
+++ buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
@@ -1,11 +1,14 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.kotlin.dsl.`java-library`
+
 plugins {
     `java-library`
 }
 
 java {
-    // Make consist compatible with  Java 1.8
+    // Make consist compatible with  Java 1.9
     toolchain {
-        // Java 8 == bytecode version 52.0
+        // Java 9 == bytecode version 53.0
         languageVersion.set(JavaLanguageVersion.of(9))
     }
 
