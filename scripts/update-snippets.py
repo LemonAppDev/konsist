ded697b96d07b4e56031c16dcbf3e98394811ef7
natalia.peterwas <n.peterwas@gmail.com>
2023-09-26 13:14:23 +0200

    extract some methods

--- scripts/update-snippets.py
+++ scripts/update-snippets.py
@@ -3,46 +3,75 @@ import subprocess
 import datetime
 import math
 
-# Function to format the snippet text
-def format_snippet_text(file_text):
-    # Split the text of the file into a list of functions
+# Variables ============================================================================================================
+
+# Methods ==============================================================================================================
+def split_text_to_function_list(file_text):
     text_without_suffix = file_text.removesuffix("}\n") + "\n\t"
     function_list = text_without_suffix.split("fun ")
     function_list.pop(0)
+    return function_list
 
+def format_heading_text(element):
+    # Format function name as heading - capitalize letters and replace ''' into '`'
+    function_name_as_heading = element.split("`")[1]
+    function_name_words = function_name_as_heading.replace("'", "`").split(" ")
+    function_name_words = map(lambda capitalize: capitalize.replace(capitalize, capitalize[0].upper() + capitalize[1:]), function_name_words)
+    return " ".join(function_name_words)
+
+def split_snippet_text(element):
+    snippet_parts = element.split("{", maxsplit=1)
+    snippet_name = snippet_parts[0]
+    function_body = snippet_parts[1].removeprefix("\n")
+    return snippet_name, function_body
+
+def remove_tabs(line, formatted_lines):
+    if line.strip():  # Ignore empty lines
+        indentation = len(line) - len(line.lstrip())
+        number = indentation - 4*(math.floor(indentation / 4) - 1)
+        formatted_lines.append(line[number:])
+    else:
+         # Preserve empty lines
+         formatted_lines.append(line)
+    return formatted_lines
+
+# Function to format the snippet text
+def format_snippet_text(file_text):
+    function_list = split_text_to_function_list(file_text)
     text = ""
 
     for element in function_list:
-        # Format function name as heading - capitalize letters and replace ''' into '`'
-        function_name_as_heading = element.split("`")[1]
-        function_name_words = function_name_as_heading.replace("'", "`").split(" ")
-        function_name_words = map(lambda capitalize: capitalize.replace(capitalize, capitalize[0].upper() + capitalize[1:]), function_name_words)
-        function_name_as_heading = " ".join(function_name_words)
+        heading = format_heading_text(element)
+        snippet_name, function_body = split_snippet_text(element)
 
-        function_text = element.split("{", maxsplit = 1)
-        function_name_as_snippet_body = function_text[0]
-
-        function_body = function_text[1].removeprefix("\n")
         function_body_lines = function_body.split('\n')
         formatted_lines = []
 
         # Iterate through lines to remove unnecessary tabs
         for line in function_body_lines:
-            if line.strip():  # Ignore empty lines
-                indentation = len(line) - len(line.lstrip())
-
-                number = indentation - 4*(math.floor(indentation / 4) - 1)
-                formatted_lines.append(line[number:])
-            else:
-                # Preserve empty lines
-                formatted_lines.append(line)
+            formatted_lines = remove_tabs(line, formatted_lines)
 
         # Reconstruct the function body text
         formatted_text = '\n'.join(formatted_lines[:-1])
 
-        text += "## " + str(function_list.index(element) + 1) +  ". " + function_name_as_heading + "\n\n```kotlin\n@Test\nfun " + function_name_as_snippet_body + "{\n" + formatted_text + "```\n\n"
+        text += "## " + str(function_list.index(element) + 1) +  ". " + heading + "\n\n```kotlin\n@Test\nfun " + snippet_name + "{\n" + formatted_text + "```\n\n"
 
     return text
+
+def construct_destination_path(destination_folder, filename_md):
+    return os.path.join(destination_folder, filename_md)
+
+
+def create_git_branch(branch_name):
+    try:
+        subprocess.run(["git", "checkout", branch_name], check=True)
+    except subprocess.CalledProcessError:
+        subprocess.run(["git", "checkout", "-b", branch_name], check=True)
+
+def push_changes(branch_name):
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", "Upd snippet code at docs"], check=True)
+    subprocess.run(["git", "push", "origin", branch_name], check=True)
 
 # Function to copy content from source file to destination file
 def copy_content(source_kt_path, source_md_path, destination_folder):
@@ -50,7 +79,7 @@ def copy_content(source_kt_path, source_md_path, destination_folder):
         filename_md = os.path.basename(source_md_path)
 
         # Construct the paths for destination
-        destination_path = os.path.join(destination_folder, filename_md)
+        destination_path = construct_destination_path(destination_folder, filename_md)
 
         # Read text of .md file
         with open(source_md_path, "r") as source_file:
@@ -69,7 +98,7 @@ def copy_content(source_kt_path, source_md_path, destination_folder):
             destination_file.write(md_content+kt_text)
 
     except Exception as e:
-        print(f"Error copying content: {e}")
+        print(f"Error copying content: {e} ")
 
 # Directories
 source_kt_directory = "~/IdeaProjects/konsist/lib/src/snippet/kotlin/com/lemonappdev/konsist"
@@ -92,12 +121,7 @@ formatted_date = current_date.strftime("%Y-%m-%d")
 new_branch_name = formatted_date + "-update-snippet-code"
 
 # Check if the branch exists
-try:
-    # If the branch exists, switch to it
-    subprocess.run(["git", "checkout", new_branch_name], check=True)
-except subprocess.CalledProcessError:
-    # If the branch doesn't exist, create it
-    subprocess.run(["git", "checkout", "-b", new_branch_name], check=True)
+create_git_branch(new_branch_name)
 
 # Iterate through all .md and .kt files in the source folder and copy them content
 for filename_md in os.listdir(expanded_source_md_directory):
@@ -110,16 +134,8 @@ for filename_md in os.listdir(expanded_source_md_directory):
                 copy_content(kt_path, md_path, expanded_destination_directory)
 
 # Run git add command
-add_command = ["git", "add", "."]
-subprocess.run(add_command, check=True)
-
 # Commit and push changes
-commit_message = "Upd snippet code at docs"
-commit_command = ["git", "commit", "-m", commit_message]
-subprocess.run(commit_command, check=True)
-
-command = ["git", "push", "origin", new_branch_name]
-subprocess.run(command, check=True)
+push_changes(new_branch_name)
 
 # Create a pull request
 prTitle = "Update snippet code from " + formatted_date
