bbd559daf8d2ed1fa0fee42651716b84882c5549
Igor Wojda <igor.wojda@gmail.com>
2023-09-27 13:59:27 +0200

    Revert "Rename multiplatform projects"
    This reverts commit dee7e14c6b37045c06f24e42a0f92e4e8db91ccb.

--- samples/starter-projects/konsist-starter-multiplatform-gradle-kotlin-junit5/settings.gradle.kts
+++ samples/starter-projects/konsist-starter-multiplatform-gradle-kotlin-junit5/settings.gradle.kts
@@ -9,4 +9,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "konsist-starter-kmp-gradle-kotlin-junit5"
+rootProject.name = "konsist-starter-multiplatform-gradle-kotlin-junit5"
--- samples/starter-projects/konsist-starter-multiplatform-gradle-kotlin-kotest/settings.gradle.kts
+++ samples/starter-projects/konsist-starter-multiplatform-gradle-kotlin-kotest/settings.gradle.kts
@@ -9,4 +9,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "konsist-starter-kmp-gradle-kotlin-kotest"
+rootProject.name = "konsist-starter-multiplatform-gradle-kotlin-kotest"
