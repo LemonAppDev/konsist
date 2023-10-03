e55d28d698aff4848dfe6ff30a783b017f3ef337
natalia.peterwas <n.peterwas@gmail.com>
2023-10-03 18:43:53 +0200

    add code, but it dodn't work well

--- scripts/update_snippets.py
+++ scripts/update_snippets.py
@@ -8,10 +8,13 @@ import re
 # Directories
 source_directory = "~/IdeaProjects/konsist/lib/src/snippet/kotlin/com/lemonappdev/konsist"
 destination_directory = "~/IdeaProjects/konsist-documentation/inspiration/snippets"
+summary_path = "~/IdeaProjects/konsist-documentation/SUMMARY.md"
 
 # Expand user paths
 expanded_source_directory = os.path.expanduser(source_directory)
 expanded_destination_directory = os.path.expanduser(destination_directory)
+expanded_summary_path = os.path.expanduser(summary_path)
+
 
 # Methods ==============================================================================================================
 def split_text_to_function_list(file_text):
@@ -20,9 +23,11 @@ def split_text_to_function_list(file_text):
     function_list.pop(0)
     return function_list
 
+
 def split_text_to_classes_list(file_text):
     function_list = file_text.split("class ")[2:]
     return function_list
+
 
 def capitalize_first_letter(word):
     if len(word) == 0:
@@ -30,14 +35,17 @@ def capitalize_first_letter(word):
     else:
         return word[0].upper() + word[1:]
 
+
 def capitalize_all_first_letters(text):
     words = map(lambda capitalize: capitalize.replace(capitalize, capitalize[0].upper() + capitalize[1:]), text)
     return " ".join(words)
+
 
 def split_words_by_capital(word):
     # Use regular expression to split words by capital letters
     words = re.findall(r'[A-Z][a-z]*', word)
     return ' '.join(words)
+
 
 def format_heading_function_text(element):
     # Format function name as heading - capitalize letters and replace ''' into '`'
@@ -46,11 +54,13 @@ def format_heading_function_text(element):
     function_name_words = capitalize_all_first_letters(function_name_words)
     return "".join(function_name_words)
 
+
 def format_heading_class_text(element):
     # Format function name as heading - split class name to separate words and capitalize letters
     text = capitalize_first_letter(element)
     words = split_words_by_capital(text.split(" ")[0])
     return words
+
 
 def split_function_snippet_text(element):
     snippet_parts = element.split("{", maxsplit=1)
@@ -58,15 +68,17 @@ def split_function_snippet_text(element):
     function_body = snippet_parts[1].removeprefix("\n")
     return snippet_name, function_body
 
+
 def remove_tabs(line, formatted_lines):
     if line.strip():  # Ignore empty lines
         indentation = len(line) - len(line.lstrip())
-        number = indentation - 4*(math.floor(indentation / 4) - 1)
+        number = indentation - 4 * (math.floor(indentation / 4) - 1)
         formatted_lines.append(line[number:])
     else:
         # Preserve empty lines
         formatted_lines.append(line)
     return formatted_lines
+
 
 # Function to format the snippet text
 def format_function_snippet_text(file_text):
@@ -87,9 +99,11 @@ def format_function_snippet_text(file_text):
         # Reconstruct the function body text
         formatted_text = '\n'.join(formatted_lines[:-1])
 
-        text += "## " + str(function_list.index(element) + 1) +  ". " + heading + "\n\n```kotlin\n@Test\nfun " + snippet_name + "{\n" + formatted_text + "```\n\n"
+        text += "## " + str(function_list.index(
+            element) + 1) + ". " + heading + "\n\n```kotlin\n@Test\nfun " + snippet_name + "{\n" + formatted_text + "```\n\n"
 
     return text
+
 
 def format_class_snippet_text(file_text):
     class_list = split_text_to_classes_list(file_text)
@@ -110,15 +124,19 @@ def format_class_snippet_text(file_text):
         # Reconstruct the class body text
         formatted_text = '\n'.join(formatted_lines[:-1])
 
-        text += "## " + str(class_list.index(element) + 1) +  ". " + heading + "\n\n```kotlin\n" + formatted_text + "```\n\n"
+        text += "## " + str(
+            class_list.index(element) + 1) + ". " + heading + "\n\n```kotlin\n" + formatted_text + "```\n\n"
 
     return text
+
 
 def get_current_date():
     current_date = datetime.datetime.now().date()
     return current_date.strftime("%Y-%m-%d")
 
+
 branch_name = get_current_date() + "-update-snippet-code"
+
 
 def create_git_branch():
     try:
@@ -126,19 +144,23 @@ def create_git_branch():
     except subprocess.CalledProcessError:
         subprocess.run(["git", "checkout", "-b", branch_name, "main"], check=True)
 
+
 def push_changes():
     subprocess.run(["git", "add", "."], check=True)
     subprocess.run(["git", "commit", "-m", "Upd snippet code at docs"], check=True)
     subprocess.run(["git", "push", "origin", branch_name], check=True)
 
+
 # Construct the paths for destination
 def construct_destination_path(destination_folder, filename_md):
     return os.path.join(destination_folder, filename_md)
+
 
 # Read content from a file
 def read_file(file_path):
     with open(file_path, "r") as source_file:
         return source_file.read()
+
 
 # Write content to a file
 def write_file(directory, file_path, content):
@@ -153,6 +175,7 @@ def write_file(directory, file_path, content):
     with open(file_path, "w") as new_file:
         new_file.write(content)
 
+
 # Add a missing line at the end of a Markdown file
 def add_empty_line_to_md_file(md_content):
     if md_content.splitlines()[-1] != "":
@@ -160,6 +183,7 @@ def add_empty_line_to_md_file(md_content):
         return new_md_content
     else:
         return md_content
+
 
 def remove_files_recursively_except_readme(directory_path):
     try:
@@ -174,7 +198,94 @@ def remove_files_recursively_except_readme(directory_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
 # Copy content from source .kt and .md files to a destination file
+def get_helper_root(directory_path):
+    try:
+        # List all files and directories in the current directory
+        for root, dirs, files in os.walk(directory_path):
+            for file_name in files:
+                if file_name.lower() != "readme.md":  # Skip "README.md"
+                    text = root.split("inspiration/snippets")[1]
+                    return "inspiration/snippets" + text
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def number_of_packages(root):
+    num = len(root.split("/"))
+    return num - 2
+
+
+def snippet_name_to_summary(path):
+    try:
+        # List all files and directories in the current directory
+        for root, dirs, files in os.walk(path):
+            for file_name in files:
+                if file_name.lower() != "readme.md":  # Skip "README.md"
+                    # get snippet file
+                    file_path = os.path.join(root, file_name)
+                    file_text = read_file(file_path)
+                    first_line = file_text.split("\n")[0]
+                    first_line = "* [" + first_line.removeprefix("#").strip() + "]"
+
+                    # get short path to file
+                    helper_root = root.split("inspiration/snippets")[1] + "/"
+
+                    # add blank spaces
+                    num = number_of_packages(root)
+                    blank_space = " "
+
+                    return blank_space * num + first_line + "(inspiration/snippets" + helper_root + file_name + ")"
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+x = snippet_name_to_summary(expanded_destination_directory)
+
+def complete_summary_file():
+    # Read the existing content of the file
+    with open(expanded_summary_path, "r") as file:
+        file_content = file.read()
+
+    text = "* [Snippets](inspiration/snippets/README.md)"
+
+    # Find the position where you want to insert the new text
+    insertion_point = file_content.find(text)
+
+    if insertion_point != -1:
+        # Find the end of the line where insertion_point is found
+        end_of_line = file_content.find('\n', insertion_point)
+
+        # If the end_of_line is found, insert the new text after it
+        if end_of_line != -1:
+            modified_content = (
+                    file_content[:end_of_line+1]
+                    + x
+                    + "\n"
+                    + file_content[end_of_line+1:]
+            )
+
+            # Write the modified content back to the file
+            with open(expanded_summary_path, "w") as file:
+                file.write(modified_content)
+    #
+    # if insertion_point != -1:
+    #     # Insert the new text after the target line
+    #     modified_content = (
+    #             file_content[:insertion_point]
+    #             + text
+    #              + "\n"
+    #             + snippet_name_to_summary(expanded_destination_directory)
+    #             + file_content[insertion_point:]
+    #     )
+    #
+    #     # Write the modified content back to the file
+    #     with open(expanded_summary_path, "w") as file:
+    #         file.write(modified_content)
+
+
 def copy_content(expanded_source_directory, expanded_destination_directory):
     # Iterate through all .md and .kt files in the source folder and copy them content
     for root, dirs, files in os.walk(expanded_source_directory):
@@ -197,6 +308,7 @@ def copy_content(expanded_source_directory, expanded_destination_directory):
                             destination_path = construct_destination_path(path, filename_md)
 
                             md_content = read_file(md_path)
+
                             new_md_content = add_empty_line_to_md_file(md_content)
 
                             kt_content = read_file(kt_path)
@@ -213,24 +325,33 @@ def copy_content(expanded_source_directory, expanded_destination_directory):
                         except Exception as e:
                             print(f"Error copying content: {e}")
 
+
 def create_and_merge_pr():
     pr_title = "Update snippet code from " + get_current_date()
     os.system("gh pr create --title '" + pr_title + "' --body '""'")
     os.system("gh pr merge --merge --delete-branch")
 
+
 # Script ===============================================================================================================
 
-# Change the current working directory
-os.chdir(expanded_destination_directory)
+# # Change the current working directory
+# os.chdir(expanded_destination_directory)
+#
+# create_git_branch()
+#
+# remove_files_recursively_except_readme(expanded_destination_directory)
+#
+# copy_content(expanded_source_directory, expanded_destination_directory)
+#
 
-create_git_branch()
+complete_summary_file()
 
-remove_files_recursively_except_readme(expanded_destination_directory)
-
-copy_content(expanded_source_directory, expanded_destination_directory)
 
 # Commit and push changes
-push_changes()
+# push_changes()
 
 # Create and merge a pull request
-create_and_merge_pr()
+# create_and_merge_pr()
+
+
+
