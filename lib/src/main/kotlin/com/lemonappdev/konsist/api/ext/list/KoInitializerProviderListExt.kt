ad0825d86edfd1c2c59fa14190bfdf712831b661
natalia.peterwas <n.peterwas@gmail.com>
2023-10-05 15:21:23 +0200

    deprecate old extensions

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoInitializerProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoInitializerProviderListExt.kt
@@ -15,3 +15,19 @@ fun <T : KoInitializerProvider> List<T>.withInitialized(): List<T> = filter { it
  * @return A list containing declarations that have not been initialized.
  */
 fun <T : KoInitializerProvider> List<T>.withoutInitialized(): List<T> = filterNot { it.isInitialized }
+
+/**
+ * List containing declarations that have implementation.
+ *
+ * @return A list containing declarations with the implementation.
+ */
+@Deprecated("Will be removed in v1.0.0", ReplaceWith("withInitialized()"))
+fun <T : KoInitializerProvider> List<T>.withImplementation(): List<T> = filter { it.hasImplementation }
+
+/**
+ * List containing declarations without implementation.
+ *
+ * @return A list containing declarations without the implementation.
+ */
+@Deprecated("Will be removed in v1.0.0", ReplaceWith("withoutInitialized()"))
+fun <T : KoInitializerProvider> List<T>.withoutImplementation(): List<T> = filterNot { it.hasImplementation }
