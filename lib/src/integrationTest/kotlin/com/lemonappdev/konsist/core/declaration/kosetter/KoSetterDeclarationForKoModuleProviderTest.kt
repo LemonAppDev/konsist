9ad40b345959f0d5cbf5c3e1f7c2837859a09bf3
natalia.peterwas <n.peterwas@gmail.com>
2023-10-05 10:18:18 +0200

    rename in comment

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kosetter/KoSetterDeclarationForKoModuleProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kosetter/KoSetterDeclarationForKoModuleProviderTest.kt
@@ -1,6 +1,6 @@
 package com.lemonappdev.konsist.core.declaration.kosetter
 
 class KoSetterDeclarationForKoModuleProviderTest {
-    // Konsist koGetter.moduleName and koGetter.resideInModule() tests
+    // Konsist koSetter.moduleName and koSetter.resideInModule() tests
     // are implemented in the test\konsist-path-tester project.
 }
