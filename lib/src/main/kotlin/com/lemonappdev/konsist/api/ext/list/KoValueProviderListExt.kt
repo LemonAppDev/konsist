541d3354807abcd6d940ea4756587365505ca333
natalia.peterwas <n.peterwas@gmail.com>
2023-10-06 09:17:50 +0200

    add new extensions

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoValueProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoValueProviderListExt.kt
@@ -5,21 +5,42 @@ import com.lemonappdev.konsist.api.provider.KoValueProvider
 /**
  * List containing elements with value.
  *
- * @param value The value to include.
  * @param values The value(s) to include.
- * @return A list containing elements with the specified values.
+ * @return A list containing elements with the specified values (or any value if [values] is empty).
  */
-fun <T : KoValueProvider> List<T>.withValue(value: String, vararg values: String): List<T> = filter {
-    it.value == value || values.any { value -> it.value == value }
+fun <T : KoValueProvider> List<T>.withValue(vararg values: String): List<T> = filter {
+    when {
+        values.isEmpty() -> it.hasValue()
+        else -> values.any { value -> it.hasValue(value) }
+    }
 }
 
 /**
  * List containing elements without value.
  *
- * @param value The value to exclude.
  * @param values The value(s) to exclude.
- * @return A list containing elements without the specified values.
+ * @return A list containing elements without the specified values (or none value if [values] is empty).
  */
-fun <T : KoValueProvider> List<T>.withoutValue(value: String, vararg values: String): List<T> = filter {
-    it.value != value && values.none { value -> it.value == value }
+fun <T : KoValueProvider> List<T>.withoutValue(vararg values: String): List<T> = filter {
+    when {
+        values.isEmpty() ->  !it.hasValue()
+        else -> values.none { value ->  it.hasValue(value) }
+    }
 }
+
+/**
+ * List containing declarations that have a value matching the provided predicate.
+ *
+ * @param predicate A function that defines the condition to be met by a declaration value.
+ * @return A list containing declarations with the value matching the provided predicate.
+ */
+fun <T : KoValueProvider> List<T>.withValue(predicate: (String?) -> Boolean): List<T> = filter { predicate(it.value) }
+
+/**
+ * List containing declarations that don't have a value matching the provided predicate.
+ *
+ * @param predicate A function that defines the condition to be met by a declaration value.
+ * @return A list containing declarations without the value matching the provided predicate.
+ */
+fun <T : KoValueProvider> List<T>.withoutValue(predicate: (String?) -> Boolean): List<T> =
+    filterNot { predicate(it.value) }
--- lib/src/test/kotlin/com/lemonappdev/konsist/api/ext/list/KoValueProviderListExtTest.kt
+++ lib/src/test/kotlin/com/lemonappdev/konsist/api/ext/list/KoValueProviderListExtTest.kt
@@ -8,23 +8,39 @@ import org.junit.jupiter.api.Test
 
 class KoValueProviderListExtTest {
     @Test
-    fun `withValue() returns declaration with given value`() {
+    fun `withValue() returns declaration with any value`() {
         // given
-        val value1 = "value1"
-        val value2 = "value2"
         val declaration1: KoValueProvider = mockk {
-            every { value } returns value1
+            every { hasValue() } returns true
         }
         val declaration2: KoValueProvider = mockk {
-            every { value } returns value2
+            every { hasValue() } returns false
         }
         val declarations = listOf(declaration1, declaration2)
 
         // when
-        val sut = declarations.withValue(value1)
+        val sut = declarations.withValue()
 
         // then
         sut shouldBeEqualTo listOf(declaration1)
+    }
+
+    @Test
+    fun `withoutValue() returns declaration without value`() {
+        // given
+        val declaration1: KoValueProvider = mockk {
+            every { hasValue() } returns true
+        }
+        val declaration2: KoValueProvider = mockk {
+            every { hasValue() } returns false
+        }
+        val declarations = listOf(declaration1, declaration2)
+
+        // when
+        val sut = declarations.withoutValue()
+
+        // then
+        sut shouldBeEqualTo listOf(declaration2)
     }
 
     @Test
@@ -32,15 +48,17 @@ class KoValueProviderListExtTest {
         // given
         val value1 = "value1"
         val value2 = "value2"
-        val value3 = "value3"
         val declaration1: KoValueProvider = mockk {
-            every { value } returns value1
+            every { hasValue(value1) } returns true
+            every { hasValue(value2) } returns false
         }
         val declaration2: KoValueProvider = mockk {
-            every { value } returns value2
+            every { hasValue(value1) } returns false
+            every { hasValue(value2) } returns true
         }
         val declaration3: KoValueProvider = mockk {
-            every { value } returns value3
+            every { hasValue(value1) } returns false
+            every { hasValue(value2) } returns false
         }
         val declarations = listOf(declaration1, declaration2, declaration3)
 
@@ -52,10 +70,39 @@ class KoValueProviderListExtTest {
     }
 
     @Test
-    fun `withoutValue() returns declaration without given value`() {
+    fun `withoutValue() returns declaration without any of given value`() {
         // given
         val value1 = "value1"
         val value2 = "value2"
+        val declaration1: KoValueProvider = mockk {
+            every { hasValue(value1) } returns true
+            every { hasValue(value2) } returns false
+        }
+        val declaration2: KoValueProvider = mockk {
+            every { hasValue(value1) } returns false
+            every { hasValue(value2) } returns true
+        }
+        val declaration3: KoValueProvider = mockk {
+            every { hasValue(value1) } returns false
+            every { hasValue(value2) } returns false
+        }
+        val declarations = listOf(declaration1, declaration2, declaration3)
+
+        // when
+        val sut = declarations.withoutValue(value1, value2)
+
+        // then
+        sut shouldBeEqualTo listOf(declaration3)
+    }
+
+    @Test
+    fun `withValue{predicate} returns declaration with value matching to predicate`() {
+        // given
+        val value1 = "sampleValue1"
+        val value2 = "sampleValue2"
+        val prefix = "sample"
+        val suffix = "1"
+        val predicate: (String?) -> Boolean = { it?.startsWith(prefix) ?: false  && it?.endsWith(suffix) ?: false }
         val declaration1: KoValueProvider = mockk {
             every { value } returns value1
         }
@@ -65,33 +112,32 @@ class KoValueProviderListExtTest {
         val declarations = listOf(declaration1, declaration2)
 
         // when
-        val sut = declarations.withoutValue(value1)
+        val sut = declarations.withValue(predicate)
 
         // then
-        sut shouldBeEqualTo listOf(declaration2)
+        sut shouldBeEqualTo listOf(declaration1)
     }
 
     @Test
-    fun `withoutValue() returns declaration without any of given value`() {
+    fun `withoutValue{predicate} returns declaration without value matching to predicate`() {
         // given
-        val value1 = "value1"
-        val value2 = "value2"
-        val value3 = "value3"
+        val value1 = "sampleValue1"
+        val value2 = "sampleValue2"
+        val prefix = "sample"
+        val suffix = "1"
+        val predicate: (String?) -> Boolean = { it?.startsWith(prefix) ?: false  && it?.endsWith(suffix) ?: false }
         val declaration1: KoValueProvider = mockk {
             every { value } returns value1
         }
         val declaration2: KoValueProvider = mockk {
             every { value } returns value2
         }
-        val declaration3: KoValueProvider = mockk {
-            every { value } returns value3
-        }
-        val declarations = listOf(declaration1, declaration2, declaration3)
+        val declarations = listOf(declaration1, declaration2)
 
         // when
-        val sut = declarations.withoutValue(value1, value2)
+        val sut = declarations.withoutValue(predicate)
 
         // then
-        sut shouldBeEqualTo listOf(declaration3)
+        sut shouldBeEqualTo listOf(declaration2)
     }
 }
