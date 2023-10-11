86ddf3f418346e489bb86c1c9481acc5d67f8786
natalia.peterwas <n.peterwas@gmail.com>
2023-10-11 09:29:35 +0200

    add tests for KoParentClassProvider

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koclass/KoClassDeclarationForKoParentClassProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koclass/KoClassDeclarationForKoParentClassProviderTest.kt
@@ -1,6 +1,8 @@
 package com.lemonappdev.konsist.core.declaration.koclass
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import com.lemonappdev.konsist.testdata.SampleClass
+import com.lemonappdev.konsist.testdata.SampleParentClass
 import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
@@ -18,6 +20,10 @@ class KoClassDeclarationForKoParentClassProviderTest {
             parentClass shouldBeEqualTo null
             hasParentClass() shouldBeEqualTo false
             hasParentClass { it.name == "SampleParentClass" } shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass") shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass", "OtherClass") shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class) shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class, SampleClass::class) shouldBeEqualTo false
             hasParentClass("SampleParentClass") shouldBeEqualTo false
         }
     }
@@ -35,6 +41,12 @@ class KoClassDeclarationForKoParentClassProviderTest {
             hasParentClass() shouldBeEqualTo true
             hasParentClass { it.name == "SampleParentClass" } shouldBeEqualTo true
             hasParentClass { it.name == "OtherClass" } shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass") shouldBeEqualTo true
+            hasParentClassWithName("OtherClass") shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass", "OtherClass") shouldBeEqualTo true
+            hasParentClassOf(SampleParentClass::class) shouldBeEqualTo true
+            hasParentClassOf(SampleClass::class) shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class, SampleClass::class) shouldBeEqualTo true
             hasParentClass("SampleParentClass") shouldBeEqualTo true
             hasParentClass("OtherClass") shouldBeEqualTo false
         }
@@ -53,6 +65,12 @@ class KoClassDeclarationForKoParentClassProviderTest {
             hasParentClass() shouldBeEqualTo true
             hasParentClass { it.name == "SampleParentClass" } shouldBeEqualTo true
             hasParentClass { it.name == "OtherClass" } shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass") shouldBeEqualTo true
+            hasParentClassWithName("OtherClass") shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass", "OtherClass") shouldBeEqualTo true
+            hasParentClassOf(SampleParentClass::class) shouldBeEqualTo true
+            hasParentClassOf(SampleClass::class) shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class, SampleClass::class) shouldBeEqualTo true
             hasParentClass("SampleParentClass") shouldBeEqualTo true
             hasParentClass("OtherClass") shouldBeEqualTo false
         }
--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koobject/KoObjectDeclarationForKoParentClassProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koobject/KoObjectDeclarationForKoParentClassProviderTest.kt
@@ -1,6 +1,8 @@
 package com.lemonappdev.konsist.core.declaration.koobject
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import com.lemonappdev.konsist.testdata.SampleClass
+import com.lemonappdev.konsist.testdata.SampleParentClass
 import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
@@ -17,6 +19,11 @@ class KoObjectDeclarationForKoParentClassProviderTest {
         assertSoftly(sut) {
             parentClass shouldBeEqualTo null
             hasParentClass() shouldBeEqualTo false
+            hasParentClass { it.name == "SampleParentClass" } shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass") shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass", "OtherClass") shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class) shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class, SampleClass::class) shouldBeEqualTo false
             hasParentClass("SampleParentClass") shouldBeEqualTo false
         }
     }
@@ -32,6 +39,14 @@ class KoObjectDeclarationForKoParentClassProviderTest {
         assertSoftly(sut) {
             parentClass?.name shouldBeEqualTo "SampleParentClass"
             hasParentClass() shouldBeEqualTo true
+            hasParentClass { it.name == "SampleParentClass" } shouldBeEqualTo true
+            hasParentClass { it.name == "OtherClass" } shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass") shouldBeEqualTo true
+            hasParentClassWithName("OtherClass") shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass", "OtherClass") shouldBeEqualTo true
+            hasParentClassOf(SampleParentClass::class) shouldBeEqualTo true
+            hasParentClassOf(SampleClass::class) shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class, SampleClass::class) shouldBeEqualTo true
             hasParentClass("SampleParentClass") shouldBeEqualTo true
             hasParentClass("OtherClass") shouldBeEqualTo false
         }
@@ -48,6 +63,14 @@ class KoObjectDeclarationForKoParentClassProviderTest {
         assertSoftly(sut) {
             parentClass?.name shouldBeEqualTo "SampleParentClass"
             hasParentClass() shouldBeEqualTo true
+            hasParentClass { it.name == "SampleParentClass" } shouldBeEqualTo true
+            hasParentClass { it.name == "OtherClass" } shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass") shouldBeEqualTo true
+            hasParentClassWithName("OtherClass") shouldBeEqualTo false
+            hasParentClassWithName("SampleParentClass", "OtherClass") shouldBeEqualTo true
+            hasParentClassOf(SampleParentClass::class) shouldBeEqualTo true
+            hasParentClassOf(SampleClass::class) shouldBeEqualTo false
+            hasParentClassOf(SampleParentClass::class, SampleClass::class) shouldBeEqualTo true
             hasParentClass("SampleParentClass") shouldBeEqualTo true
             hasParentClass("OtherClass") shouldBeEqualTo false
         }
