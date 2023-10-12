895d791a4926d7b1acbe1d8dae708b50ddf469b3
natalia.peterwas <n.peterwas@gmail.com>
2023-10-12 14:59:33 +0200

    fix old tests

--- lib/src/apiTest/kotlin/com/lemonappdev/konsist/architecture/assertarchitecture/architecture2/project/presentation/sample/PresentationThirdClass.kt
+++ lib/src/apiTest/kotlin/com/lemonappdev/konsist/architecture/assertarchitecture/architecture2/project/presentation/sample/PresentationThirdClass.kt
@@ -1,5 +1,5 @@
 package com.lemonappdev.konsist.architecture.assertarchitecture.architecture2.project.presentation.sample
 
-import com.lemonappdev.konsist.architecture.optionalparameters.project.domain.DomainFirstClass
+import com.lemonappdev.konsist.architecture.assertarchitecture.architecture2.project.domain.DomainFirstClass
 
 class PresentationThirdClass(val sampleParameter: DomainFirstClass)
