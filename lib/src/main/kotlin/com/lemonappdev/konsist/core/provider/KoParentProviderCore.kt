a71b263d6999b8b97d45f49017f2721924cd3fb5
natalia.peterwas <n.peterwas@gmail.com>
2023-10-17 10:54:27 +0200

    upd searching parents (we check also num parameters)

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoParentProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoParentProviderCore.kt
@@ -1,12 +1,19 @@
 package com.lemonappdev.konsist.core.provider
 
+import com.lemonappdev.konsist.api.declaration.KoClassDeclaration
+import com.lemonappdev.konsist.api.declaration.KoInterfaceDeclaration
 import com.lemonappdev.konsist.api.declaration.KoParentDeclaration
 import com.lemonappdev.konsist.api.provider.KoParentProvider
 import com.lemonappdev.konsist.core.declaration.KoExternalParentDeclarationCore
 import com.lemonappdev.konsist.core.model.DataCore
 import com.lemonappdev.konsist.core.util.ParentUtil.checkIfParentOf
+import org.jetbrains.kotlin.psi.KtClassBody
 import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtConstructorCalleeExpression
+import org.jetbrains.kotlin.psi.KtSecondaryConstructor
 import org.jetbrains.kotlin.psi.KtSuperTypeListEntry
+import org.jetbrains.kotlin.psi.KtValueArgument
+import org.jetbrains.kotlin.psi.KtValueArgumentList
 import kotlin.reflect.KClass
 
 internal interface KoParentProviderCore :
@@ -28,45 +35,84 @@ internal interface KoParentProviderCore :
                     .substringBefore("(")
                     .substringBefore("<")
 
-                val classFromFile = containingFile
-                    .classes()
-                    .firstOrNull { decl -> decl.name == name }
-
-                if (classFromFile != null) {
-                    return@map classFromFile
-                }
-
-                val interfaceFromFile = containingFile
-                    .interfaces()
-                    .firstOrNull { decl -> decl.name == name }
-
-                if (interfaceFromFile != null) {
-                    return@map interfaceFromFile
-                }
-
-                val declarationFromImport =
+                val fqn =
                     containingFile
                         .imports
                         .firstOrNull { import -> import.name.substringAfterLast(".") == name }
+                        ?.name
 
-                val parentFromProject = if (declarationFromImport != null) {
-                    DataCore
-                        .classes
-                        .firstOrNull { parent ->
-                            (parent.packagee?.fullyQualifiedName + "." + parent.name) == declarationFromImport.name
-                        }
-                        ?: DataCore
-                            .interfaces
-                            .firstOrNull { parent ->
-                                (parent.packagee?.fullyQualifiedName + "." + parent.name) == declarationFromImport.name
-                            }
+                val isCallable = it
+                    .children
+                    .filterIsInstance<KtConstructorCalleeExpression>()
+                    .isNotEmpty()
+
+                val parent = if (isCallable) {
+                    getParentClass(name, fqn, it)
                 } else {
-                    null
+                    /*
+                    We check this because parent may be a class without primary constructor, e.g.
+                    class SampleClass : SampleSuperClass {
+                         constructor(param: Int) : super(param)
+                    }
+
+                    open class SampleSuperClass(param: Int)
+                     */
+                    val hasSecondaryConstructor = ktClassOrObject
+                        .children
+                        .filterIsInstance<KtClassBody>()
+                        .firstOrNull()
+                        ?.children
+                        ?.filterIsInstance<KtSecondaryConstructor>()
+                        ?.isNotEmpty()
+                        ?: false
+
+                    if (hasSecondaryConstructor) {
+                        getParentClass(name, fqn, it)
+                    } else {
+                        getParentInterface(name, fqn)
+                    }
                 }
 
-                parentFromProject ?: KoExternalParentDeclarationCore(name, it)
+                return@map parent ?: KoExternalParentDeclarationCore(name, it)
             }
             ?: emptyList()
+
+    private fun getParentClass(
+        name: String,
+        fqn: String?,
+        ktSuperTypeListEntry: KtSuperTypeListEntry
+    ): KoClassDeclaration? {
+        val numArguments = ktSuperTypeListEntry
+            .children
+            .filterIsInstance<KtValueArgumentList>()
+            .firstOrNull()
+            ?.children
+            ?.filterIsInstance<KtValueArgument>()
+            ?.firstOrNull()
+            ?.children
+            ?.size ?: 0
+
+        return containingFile
+            .classes()
+            .firstOrNull { decl -> checkClassCompatibility(decl, name, fqn, numArguments) }
+            ?: DataCore
+                .classes
+                .firstOrNull { parent -> checkClassCompatibility(parent, name, fqn, numArguments) }
+    }
+
+    private fun checkClassCompatibility(koClass: KoClassDeclaration, name: String, fqn: String?, numArguments: Int) =
+        (koClass.name == name || (koClass.packagee?.fullyQualifiedName + "." + koClass.name) == fqn)
+                && (koClass.constructors.any { it.numParameters == numArguments }
+                || (numArguments == 0 && koClass.numConstructors == 0))
+
+    private fun getParentInterface(name: String, fqn: String?): KoInterfaceDeclaration? = containingFile
+        .interfaces()
+        .firstOrNull { decl -> decl.name == name }
+        ?: DataCore
+            .interfaces
+            .firstOrNull { parent ->
+                (parent.packagee?.fullyQualifiedName + "." + parent.name) == fqn
+            }
 
     override val numParents: Int
         get() = parents.size
