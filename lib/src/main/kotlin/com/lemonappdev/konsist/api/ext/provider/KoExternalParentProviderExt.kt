db745e873740e7591abcbc8005443cfe7473433c
Igor Wojda <igor.wojda@gmail.com>
2023-10-22 23:39:53 +0200

    Upd docs (#733)

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/provider/KoExternalParentProviderExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/provider/KoExternalParentProviderExt.kt
@@ -4,7 +4,7 @@ import com.lemonappdev.konsist.api.provider.KoExternalParentProvider
 
 /**
  * Returns `true` if declaration represents the type of [T].
- * The external parent is a parent defined outside project codebase (defined inside external library).
+ * The external parent is a parent defined outside the project codebase (defined inside external library).
  *
  * @return `true` if declaration represents the type of [T], `false` otherwise.
  */
