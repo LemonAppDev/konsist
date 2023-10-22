fd9d6471d26e40abe92ac1d85480a800c752515d
Igor Wojda <igor.wojda@gmail.com>
2023-10-23 01:21:54 +0200

    Upd docs (#734)

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/architecture/Layer.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/architecture/Layer.kt
@@ -7,7 +7,12 @@ import com.lemonappdev.konsist.core.util.LocationUtil
  * Represents a layer within an architecture.
  *
  * @param name The name of the layer.
- * @param definedBy The package or module that defines the layer. It should end with '..'.
+ * @param definedBy The package that defines the layer.
+ * To include classes in a give package the package and all the subpackages pass two dots at the end e.g.
+ * `com.app.domain..`. To include classes only from a package pass the package name e.g. `com.app.domain`.
+ *
+ * The double dots at the end signify that the layer corresponds to the com.myapp.business package, encompassing
+ * all of its sub-packages.
  *
  * @throws KoPreconditionFailedException if the [definedBy] package does not end with '..'.
  */
