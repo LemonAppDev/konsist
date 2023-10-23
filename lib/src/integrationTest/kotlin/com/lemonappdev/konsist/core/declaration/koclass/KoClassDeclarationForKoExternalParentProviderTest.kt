03beb9c702cb0bdb59cb37319d0beb7a175aca50
natalia.peterwas <n.peterwas@gmail.com>
2023-10-23 11:50:33 +0200

    fix after merge

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koclass/KoClassDeclarationForKoExternalParentProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koclass/KoClassDeclarationForKoExternalParentProviderTest.kt
@@ -210,7 +210,6 @@ class KoClassDeclarationForKoExternalParentProviderTest {
             )
             numExternalParents(indirectParents = false) shouldBeEqualTo 1
             numExternalParents(indirectParents = true) shouldBeEqualTo 2
-            hasAllExternalParentsOf(SampleExternalInterface::class, SampleExternalGenericInterface::class) shouldBeEqualTo true
         }
     }
 
