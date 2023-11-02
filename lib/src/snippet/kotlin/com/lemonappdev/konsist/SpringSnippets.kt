64d28b510cfea2c716c8cd9af3b8ff270f53206e
Igor Wojda <igor.wojda@gmail.com>
2023-11-02 08:16:27 +0100

    Add Snippet (#740)

--- lib/src/snippet/kotlin/com/lemonappdev/konsist/SpringSnippets.kt
+++ lib/src/snippet/kotlin/com/lemonappdev/konsist/SpringSnippets.kt
@@ -1,7 +1,9 @@
 package com.lemonappdev.konsist
 
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.functions
 import com.lemonappdev.konsist.api.ext.list.withAnnotationOf
+import com.lemonappdev.konsist.api.verify.assertFalse
 import com.lemonappdev.konsist.api.verify.assertTrue
 import org.springframework.stereotype.Repository
 import org.springframework.web.bind.annotation.RestController
@@ -29,5 +31,16 @@ class SpringSnippets {
             .classes()
             .withAnnotationOf(RestController::class)
             .assertTrue { it.resideInPackage("..controller..") }
+    }
+
+    fun `classes with 'RestController' annotation should never return collection`() {
+        Konsist
+            .scopeFromPackage("story.controller..")
+            .classes()
+            .withAnnotationOf(RestController::class)
+            .functions()
+            .assertFalse(additionalMessage = "Don't use Kotlin Collection Types") { function ->
+                function.hasReturnType { it.hasNameStartingWith("List") }
+            }
     }
 }
