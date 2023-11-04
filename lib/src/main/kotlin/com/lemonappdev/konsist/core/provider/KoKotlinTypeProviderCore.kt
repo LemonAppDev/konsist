80b36ed10c36b66a1d3fe11066bba26fc626ef7a
Igor Wojda <igor.wojda@gmail.com>
2023-11-04 23:28:27 +0100

    Add isKotlinBasicType and isKotlinCollectionType

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoKotlinTypeProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoKotlinTypeProviderCore.kt
@@ -2,10 +2,13 @@ package com.lemonappdev.konsist.core.provider
 
 import com.lemonappdev.konsist.api.provider.KoKotlinTypeProvider
 
-internal interface KoKotlinTypeProviderCore : KoKotlinTypeProvider, KoSourceAndAliasTypeProviderCore, KoBaseProviderCore {
+internal interface KoKotlinTypeProviderCore :
+    KoKotlinTypeProvider,
+    KoSourceAndAliasTypeProviderCore,
+    KoBaseProviderCore {
     // Basic types in Kotlin are described here: https://kotlinlang.org/docs/basic-types.html
-    private val kotlinBasicTypes: List<String>
-        get() = listOf(
+    private val kotlinBasicTypes: Set<String>
+        get() = setOf(
             "Byte",
             "Short",
             "Int",
@@ -27,25 +30,55 @@ internal interface KoKotlinTypeProviderCore : KoKotlinTypeProvider, KoSourceAndA
         )
 
     // Collections in Kotlin are described here: https://kotlinlang.org/docs/collections-overview.html#collection
-    private val kotlinCollectionTypes: List<String>
-        get() = listOf(
-            "Iterable",
-            "MutableIterable",
+    // and here https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections
+    private val kotlinCollectionTypes: Set<String>
+        get() = setOf(
+            "AbstractCollection",
+            "AbstractIterator",
+            "AbstractList",
+            "AbstractMap",
+            "AbstractMutableCollection",
+            "AbstractMutableList",
+            "AbstractMutableMap",
+            "AbstractMutableSet",
+            "AbstractSet",
+            "ArrayDeque",
+            "ArrayList",
             "Collection",
-            "MutableCollection",
+            "HashMap",
+            "HashSet",
+            "LinkedHashMap",
+            "LinkedHashSet",
+            "LinkedList",
             "List",
-            "MutableList",
             "Map",
+            "MutableCollection",
+            "MutableList",
             "MutableMap",
-            "Set",
             "MutableSet",
+            "Set",
         )
 
     override val isKotlinType: Boolean
         get() = if (isAlias) {
             false
         } else {
-            val kotlinTypes = kotlinBasicTypes + kotlinCollectionTypes
-            kotlinTypes.any { it == baseSourceType }
+            isKotlinBasicType || isKotlinCollectionType
+        }
+
+    override val isKotlinBasicType: Boolean
+        get() = if (isAlias) {
+            false
+        } else {
+            val rawSourceType = baseSourceType.replace("?", "")
+            kotlinBasicTypes.any { it == rawSourceType }
+        }
+
+    override val isKotlinCollectionType: Boolean
+        get() = if (isAlias) {
+            false
+        } else {
+            val rawSourceType = baseSourceType.replace("?", "")
+            kotlinCollectionTypes.any { it == rawSourceType }
         }
 }
