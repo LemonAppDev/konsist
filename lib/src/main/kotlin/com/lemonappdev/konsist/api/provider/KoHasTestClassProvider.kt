2eac2f4f8f79ab73b32b3777ad7014e9d04d1994
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2023-11-16 12:10:54 +0100

    remove old logic and add new one to hasTestClass method

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoHasTestClassProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoHasTestClassProvider.kt
@@ -7,13 +7,13 @@ interface KoHasTestClassProvider : KoBaseProvider {
     /**
      * Determines whatever declaration has a Test class.
      *
-     * @param testFileNameSuffix the suffix of the test file name. By default, "Test".
+     * @param testPropertyName the test property name to check. By default, "sut".
      * @param moduleName         the name of the module to check (optional).
      * @param sourceSetName      the name of the source set to check (optional).
      * @return `true` if the declaration has a test class, `false` otherwise.
      */
     fun hasTestClass(
-        testFileNameSuffix: String = "Test",
+        testPropertyName: String = "sut",
         moduleName: String? = null,
         sourceSetName: String? = null,
     ): Boolean
--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoHasTestClassProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoHasTestClassProviderCore.kt
@@ -1,11 +1,26 @@
 package com.lemonappdev.konsist.core.provider
 
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.declaration.KoPropertyDeclaration
 import com.lemonappdev.konsist.api.provider.KoHasTestClassProvider
+import com.lemonappdev.konsist.api.provider.KoNameProvider
+import com.lemonappdev.konsist.api.provider.KoTacitTypeProvider
+import hasTacitTypeOf
 
 internal interface KoHasTestClassProviderCore : KoHasTestClassProvider, KoNameProviderCore, KoBaseProviderCore {
-    override fun hasTestClass(testFileNameSuffix: String, moduleName: String?, sourceSetName: String?): Boolean = Konsist
+    override fun hasTestClass(testPropertyName: String, moduleName: String?, sourceSetName: String?): Boolean = Konsist
         .scopeFromTest(moduleName, sourceSetName)
         .classes()
-        .any { it.name == name + testFileNameSuffix }
+        .any {
+            it.hasProperty { property -> property.hasTestProperty(testPropertyName, name) } ||
+                    it.hasFunction { function ->
+                        function.hasVariable { variable ->
+                            variable.hasTestProperty(testPropertyName, name)
+                        }
+                    }
+        }
 }
+
+private fun <T> T.hasTestProperty(name: String, type: String) where
+        T : KoNameProvider,
+        T : KoTacitTypeProvider = this.name == name && hasTacitType(type)
