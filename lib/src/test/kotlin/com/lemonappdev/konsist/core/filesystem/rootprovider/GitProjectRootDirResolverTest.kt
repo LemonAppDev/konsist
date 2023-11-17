102f8410414c1f99822c870abb4d7c1abedb2022
Emil Orvik Kollstrøm <emilok@hey.com>
2023-11-17 02:05:09 +0100

    Fix git dir resolver uses the wrong paths (#780)
    Co-authored-by: Emil Kollstrøm <e.kollstrom@sportradar.com>

---
+++ lib/src/test/kotlin/com/lemonappdev/konsist/core/filesystem/rootprovider/GitProjectRootDirResolverTest.kt
@@ -0,0 +1,27 @@
+package com.lemonappdev.konsist.core.filesystem.rootprovider
+
+import com.lemonappdev.konsist.core.filesystem.PathVerifier
+import io.mockk.every
+import io.mockk.mockk
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class GitProjectRootDirResolverTest {
+    @Test
+    fun `git dir resolver is using the correct git folder paths`() {
+        // given
+        val file = mockk<File>()
+        val pathVerifier = mockk<PathVerifier>()
+        every { pathVerifier.verifyPathIfExists(file, "./.git/config") } returns true
+        every { pathVerifier.verifyPathIfExists(file, "./.git/HEAD") } returns true
+        every { pathVerifier.verifyPathIfExists(file, "./.git/refs") } returns true
+        val sut = GitProjectRootDirResolver(pathVerifier)
+
+        // when
+        val actual = sut.getProjectRootDir(file)
+
+        // then
+        actual shouldBeEqualTo file
+    }
+}
