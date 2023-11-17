48767e3da30ad878c7166128556ede4f153b4471
Emil Orvik Kollstrøm <emilok@hey.com>
2023-11-17 02:05:09 +0100

    Fix git dir resolver uses the wrong paths (#780)
    Co-authored-by: Emil Kollstrøm <e.kollstrom@sportradar.com>

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/filesystem/rootprovider/GitProjectRootDirResolver.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/filesystem/rootprovider/GitProjectRootDirResolver.kt
@@ -12,10 +12,9 @@ class GitProjectRootDirResolver(
 
     Description of the '.git' directory https://githowto.com/git_internals_git_directory
      */
-    override val paths =
-        setOf(
-            ".git/config".toOsSeparator(),
-            ".git/HEAD".toOsSeparator(),
-            ".git/refs".toOsSeparator(),
-        )
+    override val paths = setOf(
+        "./.git/config".toOsSeparator(),
+        "./.git/HEAD".toOsSeparator(),
+        "./.git/refs".toOsSeparator(),
+    )
 }
