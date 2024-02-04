860693090a25378823a9a3a743251df9ca394259
Igor Wojda <igor.wojda@gmail.com>
2024-02-04 22:37:13 +0100

    Merge `main` to `develop` (#834)
    * Upgrade version to 0.13.0
    
    * Update check kttx snippets
    
    * Update check kttx snippets
    
    * Update check kttx snippets
    
    * Update version
    
    * Update scripts
    
    * Update scripts
    
    * Update dev readme
    
    * Release `v0.13.0` (#688)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Igor Wojda <igor.wojda@gmail.com>
    Co-authored-by: NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
    Co-authored-by: Jony Sarco <jfe.sarco@gmail.com>
    
    * Revert "Release `v0.13.0`" (#689)
    
    * Update Konsist in starter projects to Konsist `v0.13.0` (#697)
    
    * Update Kotest Snippets (#701)
    
    * Replace REGEX to allow using .. as wildcard again (#720)
    
    * Update README.md (#771)
    
    * Fixed broken link to the getting started guide. (#827)
    
    * Fix wrong detection of Git root project dir (#831)
    
    Detection never worked before because Git creates directory
    called '.git', while code was checking 'git'.
    
    * Fix test
    
    * Disable KMP starter projects
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
    Co-authored-by: Jony Sarco <jfe.sarco@gmail.com>
    Co-authored-by: Guillermo Guerrero González <3263397+guiguegon@users.noreply.github.com>
    Co-authored-by: Atiq Ur Rehman <6600985@gmail.com>
    Co-authored-by: Piotr Kubowicz <piotr.kubowicz@gmail.com>

--- lib/src/test/kotlin/com/lemonappdev/konsist/api/architecture/LayerTest.kt
+++ lib/src/test/kotlin/com/lemonappdev/konsist/api/architecture/LayerTest.kt
@@ -19,7 +19,18 @@ class LayerTest {
     }
 
     @Test
-    fun `throws an exception when layer is defined by package without three dots at the end`() {
+    fun `throws an exception when layer is defined by package without two dots at the end with dots as wildcard`() {
+        // given
+        val sut = { Layer("Domain", "package..feature") }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Layer Domain must be defined by package ending with '..'. Now: package..feature .
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when layer is defined by package with three dots at the end`() {
         // given
         val sut = { Layer("Domain", "package...") }
 
@@ -43,11 +54,11 @@ class LayerTest {
     @Test
     fun `throws an exception when layer is defined by package containing dots with more than two dots at the end`() {
         // given
-        val sut = { Layer("Domain", "first.second.third_p.package....") }
+        val sut = { Layer("Domain", "first.second..third_p.package....") }
 
         // then
         sut shouldThrow KoPreconditionFailedException::class withMessage """
-            Layer Domain must be defined by package ending with '..'. Now: first.second.third_p.package.... .
+            Layer Domain must be defined by package ending with '..'. Now: first.second..third_p.package.... .
         """.trimIndent()
     }
 
@@ -59,6 +70,28 @@ class LayerTest {
         // then
         sut shouldNotThrow KoPreconditionFailedException::class withMessage """
             Layer Domain must be defined by package ending with '..'. Now: first.second.third_p.package.... .
+        """.trimIndent()
+    }
+
+    @Test
+    fun `do not throw an exception when the package ends with two dots and there are another two dots used as wildcard`() {
+        // given
+        val sut = { Layer("Domain", "first.second..package..") }
+
+        // then
+        sut shouldNotThrow KoPreconditionFailedException::class withMessage """
+            Layer Domain must be defined by package ending with '..'. Now: first.second.third_p.package.... .
+        """.trimIndent()
+    }
+
+    @Test
+    fun `throws an exception when the package ends with two dots and there more than two dots used as wildcard`() {
+        // given
+        val sut = { Layer("Domain", "first.second..package..feature1..") }
+
+        // then
+        sut shouldThrow KoPreconditionFailedException::class withMessage """
+            Layer Domain must be defined by package ending with '..'. Now: first.second..package..feature1.. .
         """.trimIndent()
     }
 }
