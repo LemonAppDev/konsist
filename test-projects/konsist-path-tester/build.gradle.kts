70743194bbea43fa176648fc2e65f21c5290c137
Igor Wojda <igor.wojda@gmail.com>
2024-02-05 23:23:56 +0100

    Remove Scope Violation Suppress (#855)

--- test-projects/konsist-path-tester/build.gradle.kts
+++ test-projects/konsist-path-tester/build.gradle.kts
@@ -1,4 +1,3 @@
-@Suppress("DSL_SCOPE_VIOLATION") // Because of IDE bug https://youtrack.jetbrains.com/issue/KTIJ-19370
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.testLogger)
