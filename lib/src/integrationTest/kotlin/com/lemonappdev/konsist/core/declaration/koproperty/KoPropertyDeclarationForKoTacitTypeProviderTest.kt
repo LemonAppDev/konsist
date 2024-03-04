669a020a13cab8a39f59ec8e78f48a84c1e3f928
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-03-04 01:08:20 +0100

    KON-543 Add `hasTacitType` (#773)
    * add varaibles
    
    * rename localVariables to variables
    
    * add extensions and tests for them
    
    * add test for KoVariableDeclaration (on functions)
    
    * add tests for declarations which implements KoVariableProvider
    
    * KoVariableModifier - remove KoModifierProvider and fix fqn
    
    * add tests in konsist-path-tester and fix detekt
    
    * remove `includeLocal` from properties
    
    * renames providers
    
    * add some parametrized tests
    
    * fix spotless and detekt
    
    * add next tests
    
    * add rest (in Konsist) tests for KoVariableDeclaration
    
    * add tests for KoVariableDeclaration in `konsist-path-tester`
    
    * fix spotless and detekt
    
    * rename old anames to new one
    
    * fix snippets
    
    * fix spotless
    
    * fix after rebase
    
    * add KoTacitTypeProvider and tests
    
    * add provider ext
    
    * add missing tests
    
    * add kdocs
    
    * fix spotless and detekt
    
    * Clean up
    
    ---------
    
    Co-authored-by: Igor Wojda <igor.wojda@gmail.com>

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koproperty/KoPropertyDeclarationForKoTacitTypeProviderTest.kt
@@ -0,0 +1,76 @@
+package com.lemonappdev.konsist.core.declaration.koproperty
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import com.lemonappdev.konsist.api.ext.list.properties
+import com.lemonappdev.konsist.testdata.SampleClass
+import com.lemonappdev.konsist.testdata.SampleClassWithParameter
+import org.amshove.kluent.assertSoftly
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoPropertyDeclarationForKoTacitTypeProviderTest {
+    @Test
+    fun `property-has-no-tacit-type`() {
+        // given
+        val sut = getSnippetFile("property-has-no-tacit-type")
+            .properties()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTacitType("String") shouldBeEqualTo false
+            hasTacitTypeOf(String::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `property-has-explicit-simple-type`() {
+        // given
+        val sut = getSnippetFile("property-has-explicit-simple-type")
+            .properties()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTacitType("String") shouldBeEqualTo true
+            hasTacitType("Int") shouldBeEqualTo false
+            hasTacitTypeOf(String::class) shouldBeEqualTo true
+            hasTacitTypeOf(Int::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `property-has-explicit-complex-type`() {
+        // given
+        val sut = getSnippetFile("property-has-explicit-complex-type")
+            .properties()
+            .first { it.name == "sampleProperty" }
+
+        // then
+        assertSoftly(sut) {
+            hasTacitType("SampleClassWithParameter") shouldBeEqualTo true
+            hasTacitType("SampleClass") shouldBeEqualTo false
+            hasTacitTypeOf(SampleClassWithParameter::class) shouldBeEqualTo true
+            hasTacitTypeOf(SampleClass::class) shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `property-has-implicit-complex-type`() {
+        // given
+        val sut = getSnippetFile("property-has-implicit-complex-type")
+            .properties()
+            .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTacitType("SampleClassWithParameter") shouldBeEqualTo true
+            hasTacitType("SampleClass") shouldBeEqualTo false
+            hasTacitTypeOf(SampleClassWithParameter::class) shouldBeEqualTo true
+            hasTacitTypeOf(SampleClass::class) shouldBeEqualTo false
+        }
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/koproperty/snippet/forkotacittypeprovider/", fileName)
+}
