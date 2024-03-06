9962a522ffefbb23e234f649331e373ab2775aa0
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-03-06 23:06:23 +0100

    add aditional tests in konsist-declaration-tester

---
+++ test-projects/konsist-declaration-tester/app/src/integrationTest/kotlin/com/lemonappdev/konsist/declaration/kopackage/KoPackageDeclarationForKoPackageMatchingPathProviderTest.kt
@@ -0,0 +1,33 @@
+package com.lemonappdev.konsist.declaration.kopackage
+
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.helper.ext.toOsSeparator
+import com.lemonappdev.konsist.helper.util.PathProvider
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoPackageDeclarationForKoPackageMatchingPathProviderTest {
+    @Test
+    fun `package-with-matching-file-path`() {
+        // given
+        val sut =  Konsist
+            .scopeFromFile("${PathProvider.appMainSourceSetProjectDirectory}/sample/AppClass.kt".toOsSeparator())
+            .packages
+            .first()
+
+        // then
+        sut.hasMatchingPath shouldBeEqualTo true
+    }
+
+    @Test
+    fun `package-without-matching-file-path`() {
+        // given
+        val sut =  Konsist
+            .scopeFromFile("${PathProvider.appMainSourceSetProjectDirectory}/sample/AppClassWithPackageNotMatchingPath.kt".toOsSeparator())
+            .packages
+            .first()
+
+        // then
+        sut.hasMatchingPath shouldBeEqualTo false
+    }
+}
