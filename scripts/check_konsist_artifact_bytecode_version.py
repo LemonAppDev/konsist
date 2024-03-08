2bb1d693233d8e1682d10ff82ce68f676194d471
Igor Wojda <igor.wojda@gmail.com>
2024-03-08 15:19:28 +0100

    KON-604 Update Artifact Verification (#901)
    * Rename script and fail when no files
    
    * Simplify script
    
    * Upd name

--- scripts/check_konsist_artifact_bytecode_version.py
+++ scripts/check_konsist_artifact_bytecode_version.py
@@ -16,25 +16,21 @@ print("build_dir", build_dir)
 
 def get_bytecode_version(class_file):
     result = subprocess.run(["javap", "-verbose", class_file], capture_output=True, text=True)
+
     for line in result.stdout.splitlines():
         if "major version" in line:
             return line.split(":")[-1].strip()
 
 
-def main():
+def check_artifact_bytecode(desired_java_version, desired_bytecode_version):
     jar_path = get_artifact_path("jar")
-    # Java 8 == bytecode version 52.0 (defined in the local.javalibrary.gradle.kts)
-    # https://javaalmanac.io/bytecode/versions/
-    desired_java_version = "8"
-    desired_bytecode_version = "52"
 
-    print(
-        f"Verify if the first class in {jar_path} is compiled to bytecode {desired_bytecode_version} (Java {desired_java_version})")
+    print( f"Verify if the first class in {jar_path} is compiled to bytecode {desired_bytecode_version} (Java {desired_java_version})" )
 
     checked = False  # Variable to track if we've already checked a .class file
 
     try:
-        # Unzip the jar file to the temporary directory
+        # Unzip the get_artifact_path jar file to the temporary directory
         subprocess.run(["unzip", "-qq", jar_path, "-d", build_dir], check=True)
 
         # Walk the directory to find all .class files
@@ -55,12 +51,14 @@ def main():
         if checked:
             print(f"SUCCESS: First class has correct bytecode version: {desired_bytecode_version}")
         else:
-            print("WARNING: No .class file found to check.")
+            print("ERROR: No .class file found to check the artifact.")
+            sys.exit(1)
 
     finally:
         # Clean up the temporary directory
         shutil.rmtree(build_dir)
 
 
-if __name__ == "__main__":
-    main()
+# Java 8 == bytecode version 52.0 (defined in the local.javalibrary.gradle.kts)
+# https://javaalmanac.io/bytecode/versions/
+check_artifact_bytecode("8", "52")
