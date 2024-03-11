bc5c6d752d9cd90df0b6b0ab61bc8eeee88e500f
Igor Wojda <igor.wojda@gmail.com>
2024-03-11 14:29:19 +0100

    Upd snippets

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koannotation/KoAnnotationDeclarationForKoFullyQualifiedNameProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koannotation/KoAnnotationDeclarationForKoFullyQualifiedNameProviderTest.kt
@@ -20,6 +20,10 @@ class KoAnnotationDeclarationForKoFullyQualifiedNameProviderTest {
         sut.fullyQualifiedName shouldBeEqualTo "com.lemonappdev.konsist.testdata.SampleAnnotation"
     }
 
+    /*
+    The "SampleAnnotation" contains "Annotation" in name.
+    Test makes sure that correct fully qualified name is returned.
+     */
     @Test
     fun `annotation-fully-qualified-name-when-other-import-contains-its-name`() {
         // given
