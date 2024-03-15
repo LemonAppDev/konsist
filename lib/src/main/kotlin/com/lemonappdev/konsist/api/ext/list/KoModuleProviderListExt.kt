f1d41dbb30206acfa63581a34a54415a02a11016
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-03-15 18:33:44 +0100

    Add ability to pass list and set to KoModuleProviderListExt.kt

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoModuleProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoModuleProviderListExt.kt
@@ -18,6 +18,28 @@ fun <T : KoModuleProvider> List<T>.withModule(
     }
 
 /**
+ * List containing declarations with module.
+ *
+ * @param names The module name(s) to include.
+ * @return A list containing declarations that reside in any of the specified modules.
+ */
+fun <T : KoModuleProvider> List<T>.withModule(names: Set<String>): List<T> =
+    filter {
+        when {
+            names.isEmpty() -> true
+            else -> names.any { module -> it.resideInModule(module) }
+        }
+    }
+
+/**
+ * List containing declarations with module.
+ *
+ * @param names The module name(s) to include.
+ * @return A list containing declarations that reside in any of the specified modules.
+ */
+fun <T : KoModuleProvider> List<T>.withModule(names: List<String>): List<T> = withModule(names.toSet())
+
+/**
  * List containing declarations without module.
  *
  * @param name The module name to exclude.
@@ -31,3 +53,25 @@ fun <T : KoModuleProvider> List<T>.withoutModule(
     filter {
         !it.resideInModule(name) && names.none { module -> it.resideInModule(module) }
     }
+
+/**
+ * List containing declarations without module.
+ *
+ * @param names The module name(s) to exclude.
+ * @return A list containing declarations that don't reside in any of the specified modules.
+ */
+fun <T : KoModuleProvider> List<T>.withoutModule(names: Set<String>): List<T> =
+    filterNot {
+        when {
+            names.isEmpty() -> true
+            else -> names.any { module -> it.resideInModule(module) }
+        }
+    }
+
+/**
+ * List containing declarations without module.
+ *
+ * @param names The module name(s) to exclude.
+ * @return A list containing declarations that don't reside in any of the specified modules.
+ */
+fun <T : KoModuleProvider> List<T>.withoutModule(names: List<String>): List<T> = withoutModule(names.toSet())
