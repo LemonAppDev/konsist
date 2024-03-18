7204ee7b8a05f517e5371995d30416a367128577
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-03-18 22:10:06 +0100

    Add ability to pass list and set to KoResideInPackageProviderListExt.kt

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoResideInPackageProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoResideInPackageProviderListExt.kt
@@ -18,6 +18,28 @@ fun <T : KoResideInPackageProvider> List<T>.withPackage(vararg names: String): L
     }
 
 /**
+ * List containing elements with package.
+ *
+ * @param names The package names to include.
+ * @return A list containing elements that reside in any of the specified packages (or any package if [names] is empty).
+ */
+fun <T : KoResideInPackageProvider> List<T>.withPackage(names: Set<String>): List<T> =
+    filter {
+        when {
+            names.isEmpty() -> (it as? KoPackageProvider)?.packagee != null
+            else -> names.any { packagee -> it.resideInPackage(packagee) }
+        }
+    }
+
+/**
+ * List containing elements with package.
+ *
+ * @param names The package names to include.
+ * @return A list containing elements that reside in any of the specified packages (or any package if [names] is empty).
+ */
+fun <T : KoResideInPackageProvider> List<T>.withPackage(names: List<String>): List<T> = withPackage(names.toSet())
+
+/**
  * List containing elements without package.
  *
  * @param names The package names to exclude.
@@ -30,3 +52,25 @@ fun <T : KoResideInPackageProvider> List<T>.withoutPackage(vararg names: String)
             else -> names.all { packagee -> it.resideOutsidePackage(packagee) }
         }
     }
+
+/**
+ * List containing elements without package.
+ *
+ * @param names The package names to exclude.
+ * @return A list containing elements that don't reside in any of the specified packages (or none package if [names] is empty).
+ */
+fun <T : KoResideInPackageProvider> List<T>.withoutPackage(names: Set<String>): List<T> =
+    filter {
+        when {
+            names.isEmpty() -> (it as? KoPackageProvider)?.packagee == null
+            else -> names.all { packagee -> it.resideOutsidePackage(packagee) }
+        }
+    }
+
+/**
+ * List containing elements without package.
+ *
+ * @param names The package names to exclude.
+ * @return A list containing elements that don't reside in any of the specified packages (or none package if [names] is empty).
+ */
+fun <T : KoResideInPackageProvider> List<T>.withoutPackage(names: List<String>): List<T> = withoutPackage(names.toSet())
