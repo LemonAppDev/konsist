189d43489e097820158972cb2074be789b73fbb1
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-03-22 10:43:30 +0100

    add extensions for KoFullyQualifiedNameProviderListExt.kt

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoFullyQualifiedNameProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/KoFullyQualifiedNameProviderListExt.kt
@@ -18,6 +18,29 @@ fun <T : KoFullyQualifiedNameProvider> List<T>.withFullyQualifiedName(
     }
 
 /**
+ * List containing declarations with the fully qualified name.
+ *
+ * @param names The names to include.
+ * @return A list containing declarations with the specified fully qualified names.
+ */
+fun <T : KoFullyQualifiedNameProvider> List<T>.withFullyQualifiedName(names: Set<String>): List<T> =
+    filter {
+        when {
+            names.isEmpty() -> true
+            else -> names.any { fullyQualifiedName -> it.fullyQualifiedName == fullyQualifiedName }
+        }
+    }
+
+/**
+ * List containing declarations with the fully qualified name.
+ *
+ * @param names The names to include.
+ * @return A list containing declarations with the specified fully qualified names.
+ */
+fun <T : KoFullyQualifiedNameProvider> List<T>.withFullyQualifiedName(names: List<String>): List<T> =
+    withFullyQualifiedName(names.toSet())
+
+/**
  * List containing declarations without fully qualified name.
  *
  * @param name The name to exclude.
@@ -31,3 +54,26 @@ fun <T : KoFullyQualifiedNameProvider> List<T>.withoutFullyQualifiedName(
     filter {
         it.fullyQualifiedName != name && names.none { fullyQualifiedName -> it.fullyQualifiedName == fullyQualifiedName }
     }
+
+/**
+ * List containing declarations without fully qualified name.
+ *
+ * @param names The names to exclude.
+ * @return A list containing declarations without the specified fully qualified names.
+ */
+fun <T : KoFullyQualifiedNameProvider> List<T>.withoutFullyQualifiedName(names: Set<String>): List<T> =
+    filter {
+        when {
+            names.isEmpty() -> false
+            else -> names.none { fullyQualifiedName -> it.fullyQualifiedName == fullyQualifiedName }
+        }
+    }
+
+/**
+ * List containing declarations without fully qualified name.
+ *
+ * @param names The names to exclude.
+ * @return A list containing declarations without the specified fully qualified names.
+ */
+fun <T : KoFullyQualifiedNameProvider> List<T>.withoutFullyQualifiedName(names: List<String>): List<T> =
+    withoutFullyQualifiedName(names.toSet())
