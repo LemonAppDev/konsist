d083a6a620ce8925ea9ca7225907843a6f40471e
Jean-Baptiste Mille <jibidus@gmail.com>
2024-04-30 16:21:34 +0200

    KON-581 Remove konsist-starter- directory prefix in smaple projects (#1031)
    * Fix CI jobs on macos-latest after github actions image migration
    
    macos-latest image from github actions was migrated from
    [macos-14](https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md) to
    [macos-14-arm64](https://github.com/actions/runner-images/blob/main/images/macos/macos-14-arm64-Readme.md)
    (see [README of github actions
    runner-images](https://github.com/actions/runner-images/commit/84c158e8b8f516478f045f9712856e6c65c526b3)).
    
    Java 8 is installed in macos-14, but not in macos-14-arm64, and all jobs on macos
    images require java 8 and java 21.
    
    * KON-581 Remove konsist-starter- directory prefix in sample projects

---
+++ samples/starter-projects/android-gradle-kotlin-kotest/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "android-gradle-kotlin-kotest"
+include(":app", ":konsistTest")
