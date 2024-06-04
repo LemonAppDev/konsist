e5a06a0cf2f7bcf94d456f52c357a792f2fcb505
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-06-04 21:57:15 +0200

    fix code

--- scripts/check_kttxt_snippets.py
+++ scripts/check_kttxt_snippets.py
@@ -164,8 +164,11 @@ def get_all_kttxt_files():
     for root, dirs, files in os.walk(project_root):
         for file in files:
             file_abs_path = os.path.abspath(os.path.join(root, file))
-            if file.endswith('.kttxt') and documentation_snippets_path not in file_abs_path and documentation_snippets_path not in root:
-                kttxt_temp_file_paths.append(os.path.join(root, file))
+            if file.endswith('.kttxt'):
+                # Use os.path.relpath to get the path relative to project_root
+                relative_path = os.path.relpath(file_abs_path, project_root)
+                if documentation_snippets_path not in relative_path:
+                    kttxt_temp_file_paths.append(os.path.join(root, file))
 
     kt_temp_file_paths = [get_kt_temp_file_from_kttxt_file(path) for path in kttxt_temp_file_paths]
     return kt_temp_file_paths
