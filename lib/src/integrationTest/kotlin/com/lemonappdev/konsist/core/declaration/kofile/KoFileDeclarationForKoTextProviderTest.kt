bfc2b073d3cca8c5fb84586109bc2e29cafdd04b
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-06-05 14:22:25 +0200

    add tests for KoImport and KoFile

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofile/KoFileDeclarationForKoTextProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofile/KoFileDeclarationForKoTextProviderTest.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.core.declaration.kofile
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
@@ -24,6 +25,66 @@ class KoFileDeclarationForKoTextProviderTest {
                 
                 """.trimIndent(),
             )
+    }
+
+    @Test
+    fun `file-has-text-with-prefix`() {
+        // given
+        val sut =
+            getSnippetFile("file-has-text-with-prefix")
+                .files
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextStartingWith("fun sample") shouldBeEqualTo true
+            hasTextStartingWith("wrong-prefix") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `file-has-text-with-suffix`() {
+        // given
+        val sut =
+            getSnippetFile("file-has-text-with-suffix")
+                .files
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextEndingWith("\n") shouldBeEqualTo true
+            hasTextEndingWith("wrong-suffix") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `file-has-text-containing-text`() {
+        // given
+        val sut =
+            getSnippetFile("file-has-text-containing-text")
+                .files
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextContaining("SampleText") shouldBeEqualTo true
+            hasTextContaining("not-containing") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `file-has-text-matching-regex`() {
+        // given
+        val sut =
+            getSnippetFile("file-has-text-matching-regex")
+                .files
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextMatching(Regex("^[a-zA-Z[^0-9]]+")) shouldBeEqualTo true
+            hasTextMatching(Regex("[0-9]+")) shouldBeEqualTo false
+        }
     }
 
     private fun getSnippetFile(fileName: String) =
--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koimport/KoImportDeclarationForKoTextProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koimport/KoImportDeclarationForKoTextProviderTest.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.core.declaration.koimport
 
 import com.lemonappdev.konsist.TestSnippetProvider.getSnippetKoScope
+import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
@@ -16,6 +17,67 @@ class KoImportDeclarationForKoTextProviderTest {
         // then
         sut.text shouldBeEqualTo "import com.lemonappdev.konsist.testdata.SampleClass"
     }
+
+    @Test
+    fun `import-has-text-with-prefix`() {
+        // given
+        val sut =
+            getSnippetFile("import-has-text-with-prefix")
+                .imports
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextStartingWith("import com.lemonappdev") shouldBeEqualTo true
+            hasTextStartingWith("wrong-prefix") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `import-has-text-with-suffix`() {
+        // given
+        val sut =
+            getSnippetFile("import-has-text-with-suffix")
+                .imports
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextEndingWith("testdata.SampleClass") shouldBeEqualTo true
+            hasTextEndingWith("wrong-suffix") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `import-has-text-containing-text`() {
+        // given
+        val sut =
+            getSnippetFile("import-has-text-containing-text")
+                .imports
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextContaining(" com.") shouldBeEqualTo true
+            hasTextContaining("not-containing") shouldBeEqualTo false
+        }
+    }
+
+    @Test
+    fun `import-has-text-matching-regex`() {
+        // given
+        val sut =
+            getSnippetFile("import-has-text-matching-regex")
+                .imports
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            hasTextMatching(Regex("^[a-zA-Z[^0-9]]+")) shouldBeEqualTo true
+            hasTextMatching(Regex("[0-9]+")) shouldBeEqualTo false
+        }
+    }
+
 
     private fun getSnippetFile(fileName: String) = getSnippetKoScope("core/declaration/koimport/snippet/forkotextprovider/", fileName)
 }
