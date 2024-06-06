9cc832405e4f415260838481a98b6e362d1460bb
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-06-06 14:23:21 +0200

    upd tests

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofile/KoFileDeclarationForKoTextProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/kofile/KoFileDeclarationForKoTextProviderTest.kt
@@ -15,74 +15,22 @@ class KoFileDeclarationForKoTextProviderTest {
                 .first()
 
         // then
-        sut
-            .text
-            .shouldBeEqualTo(
+        assertSoftly(sut) {
+            text.shouldBeEqualTo(
                 """
                 fun sampleFunction() {
                     "SampleText"
                 }
                 
-                """.trimIndent(),
+                """.trimIndent()
             )
-    }
-
-    @Test
-    fun `file-has-text-with-prefix`() {
-        // given
-        val sut =
-            getSnippetFile("file-has-text-with-prefix")
-                .files
-                .first()
-
-        // then
-        assertSoftly(sut) {
-            hasTextStartingWith("fun sample") shouldBeEqualTo true
-            hasTextStartingWith("wrong-prefix") shouldBeEqualTo false
-        }
-    }
-
-    @Test
-    fun `file-has-text-with-suffix`() {
-        // given
-        val sut =
-            getSnippetFile("file-has-text-with-suffix")
-                .files
-                .first()
-
-        // then
-        assertSoftly(sut) {
-            hasTextEndingWith("\n") shouldBeEqualTo true
-            hasTextEndingWith("wrong-suffix") shouldBeEqualTo false
-        }
-    }
-
-    @Test
-    fun `file-has-text-containing-text`() {
-        // given
-        val sut =
-            getSnippetFile("file-has-text-containing-text")
-                .files
-                .first()
-
-        // then
-        assertSoftly(sut) {
-            hasTextContaining("SampleText") shouldBeEqualTo true
-            hasTextContaining("not-containing") shouldBeEqualTo false
-        }
-    }
-
-    @Test
-    fun `file-has-text-matching-regex`() {
-        // given
-        val sut =
-            getSnippetFile("file-has-text-matching-regex")
-                .files
-                .first()
-
-        // then
-        assertSoftly(sut) {
-            hasTextMatching(Regex("^[a-zA-Z[^0-9]]+")) shouldBeEqualTo true
+            hasTextStartingWith("fun sampleF") shouldBeEqualTo true
+            hasTextStartingWith("Other") shouldBeEqualTo false
+            hasTextEndingWith("}\n") shouldBeEqualTo true
+            hasTextEndingWith("other") shouldBeEqualTo false
+            hasTextContaining("Function() {") shouldBeEqualTo true
+            hasTextContaining("anno") shouldBeEqualTo false
+            hasTextMatching(Regex("^[^@]*\$")) shouldBeEqualTo true
             hasTextMatching(Regex("[0-9]+")) shouldBeEqualTo false
         }
     }
