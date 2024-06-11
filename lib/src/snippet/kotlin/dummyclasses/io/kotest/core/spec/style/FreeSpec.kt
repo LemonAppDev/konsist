653e616d8dcd3d239345c51791c6eaf5cc01e036
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-06-11 13:41:42 +0200

    KON-625 Create script to verify that all snippets with extension `.ktdoc` have valid Kotlin code (#1122)
    * init
    
    * add script
    
    * beck to previous script
    
    * check snippets
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * Empty-Commit
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * fix snippets
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * upd code
    
    * create working code
    
    * upd code
    
    * check if exist
    
    * add mockk classes
    
    * ry extract
    
    * upd github checks
    
    * upd import
    
    * upd import
    
    * upd code
    
    * upd code
    
    * upd code
    
    * extract common methods from check_kttest_snippets.py and turn on tests
    
    * extract common methods from check_kttest_snippets.py and turn on tests
    
    * fix temporary check
    
    * upd
    
    * turn on the rest github checks
    
    * fix detekt
    
    * upd github script
    
    * upd github script

---
+++ lib/src/snippet/kotlin/dummyclasses/io/kotest/core/spec/style/FreeSpec.kt
@@ -0,0 +1,15 @@
+package io.kotest.core.spec.style
+
+open class FreeSpec(val body: FreeSpec.() -> Unit = {}) {
+    val testCase = TestCase()
+
+    operator fun String.invoke(value: () -> Unit) {}
+}
+
+class TestCase {
+    val name: TestName = TestName()
+}
+
+class TestName {
+    val testName: String = ""
+}
--- scripts/check_kttest_snippets.py
+++ scripts/check_kttest_snippets.py
@@ -6,13 +6,13 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import multiprocessing
 import tempfile
 import time
+from common import (project_root, print_and_flush, clean, ensure_files_exist, count_files_in_directory, print_relative_file_paths, get_all_file_paths)
 
 multiprocessing.set_start_method('fork')
 
 # Variables ============================================================================================================
 error_occurred = False
 script_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(script_dir)
 kt_temp_files_dir = tempfile.mkdtemp()
 test_data_jar_file_path = os.path.join(kt_temp_files_dir, "test-data.jar")
 nested_test_data_jar_file_path = os.path.join(kt_temp_files_dir, "nested_test-data.jar")
@@ -21,11 +21,6 @@ success = "SUCCESS"
 failed = "FAILED"
 
 # Methods =============================================================================================================
-# Function to print messages and flush the output
-def print_and_flush(message):
-    print(message)
-    sys.stdout.flush()
-
 # Function to compile the test data JAR file
 def compile_test_data_jar():
     global error_occurred
@@ -168,11 +163,6 @@ def compile_kotlin_files(kotlin_files):
             if result == "FAILED":
                 error_occurred = True
 
-# Function to clean up temporary files
-def clean():
-    shutil.rmtree(kt_temp_files_dir)
-    subprocess.run(["git", "clean", "-f"])
-
 # Function to get the .kt file path from a .kttest file path
 def get_kt_temp_file_from_kttest_file(kttest_snippet_file_path):
     # Check if the file path starts with the project root
@@ -196,19 +186,6 @@ def get_all_kttest_files():
 
     return kttest_temp_file_paths
 
-# Function to ensure that all files in a list exist
-def ensure_files_exist(file_paths):
-    for file_path in file_paths:
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"The file {file_path} does not exist.")
-
-# Function to count the total number of files in a directory
-def count_files_in_directory(directory):
-    total_files = 0
-    for root, dirs, files in os.walk(directory):
-        total_files += len(files)
-    return total_files
-
 # Function to list all files in a directory
 def list_files(directory):
     file_list = []
@@ -216,20 +193,6 @@ def list_files(directory):
         for file in files:
             file_list.append(os.path.join(root, file))
     return file_list
-
-# Function to print relative file paths
-def print_relative_file_paths(file_list):
-    for file in file_list:
-        print(os.path.relpath(file, project_root))
-
-# Function to get all file paths in a directory
-def get_all_file_paths(directory):
-    file_paths = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_paths.append(file_path)
-    return file_paths
 
 # Script ===============================================================================================================
 if __name__ == '__main__':
