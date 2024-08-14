0fe72881a7ab13949d2325a7fd717ef106066e85
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-08-14 16:17:18 +0200

    clean code

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/combined/KoClassAndObjectDeclarationCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/declaration/combined/KoClassAndObjectDeclarationCore.kt
@@ -1,23 +1,13 @@
 package com.lemonappdev.konsist.core.declaration.combined
 
 import com.lemonappdev.konsist.api.declaration.combined.KoClassAndObjectDeclaration
-import com.lemonappdev.konsist.core.declaration.KoParentDeclarationCore
-import com.lemonappdev.konsist.core.provider.KoChildProviderCore
 import com.lemonappdev.konsist.core.provider.KoInitBlockProviderCore
 import com.lemonappdev.konsist.core.provider.KoParentClassProviderCore
-import com.lemonappdev.konsist.core.provider.modifier.KoActualModifierProviderCore
 import com.lemonappdev.konsist.core.provider.modifier.KoDataModifierProviderCore
-import com.lemonappdev.konsist.core.provider.modifier.KoExpectModifierProviderCore
-import com.lemonappdev.konsist.core.provider.modifier.KoSealedModifierProviderCore
 
 internal interface KoClassAndObjectDeclarationCore :
     KoClassAndObjectDeclaration,
     KoClassAndInterfaceAndObjectDeclarationCore,
-    KoParentDeclarationCore,
-    KoActualModifierProviderCore,
-    KoChildProviderCore,
-    KoExpectModifierProviderCore,
-    KoSealedModifierProviderCore,
     KoDataModifierProviderCore,
     KoInitBlockProviderCore,
     KoParentClassProviderCore
