b88c4d17a09474fe652cc777538e1eca4271aa0e
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-09 12:16:48 +0200

    add more logs

--- scripts/create_release.py
+++ scripts/create_release.py
@@ -51,6 +51,8 @@ def choose_release_option():
     Returns: The chosen option (1 or 2).
     """
 
+    print_yellow_message(f"Method: choose_release_option()")
+
     print_success_message(f"Which release option do you choose? Write:")
     print_magenta_message(f"1 - if you want to create \"Main Release - Upgrade Minor\"")
     print_blue_message(f"2 - if you want to create \"Hotfix Release - Upgrade Patch\"")
@@ -883,39 +885,39 @@ def create_release():
 
     chosen_option = choose_release_option()
 
-    # if chosen_option == 1:
-    #     change_branch_to_develop_and_and_merge_main()
-    #     base_branch = "develop"
-    # else:
-    #     change_branch_to_main()
-    #     base_branch = "main"
+    if chosen_option == 1:
+        change_branch_to_develop_and_and_merge_main()
+        base_branch = "develop"
+    else:
+        change_branch_to_main()
+        base_branch = "main"
 
     old_konsist_version = get_old_konsist_version()
     new_konsist_version = get_new_konsist_version(chosen_option, old_konsist_version)
 
     check_for_uncommitted_changes()
 
-    # release_branch_title = create_release_branch(new_konsist_version, base_branch)
-    #
-    # replace_konsist_version(old_konsist_version, new_konsist_version, files_with_version_to_change)
-    #
+    release_branch_title = create_release_branch(new_konsist_version, base_branch)
+
+    replace_konsist_version(old_konsist_version, new_konsist_version, files_with_version_to_change)
+
     check_if_exist_files_with_deprecated_annotation(api_directory, new_konsist_version)
-    #
-    # test_3rd_party_projects_using_local_artifacts(old_konsist_version, new_konsist_version)
-    #
-    # create_pull_request_to_main(new_konsist_version)
-    #
-    # check_github_checks(release_branch_title)
-    #
-    # merge_release_pr(release_branch_title)
-    #
-    # create_github_release(new_konsist_version)
-    #
-    # update_version_in_konsist_documentation(konsist_documentation_repository_address, old_konsist_version, new_konsist_version)
-    #
-    # update_snippets_in_konsist_documentation()
-    #
-    # change_branch_to_develop_and_and_merge_main()
+
+    test_3rd_party_projects_using_local_artifacts(old_konsist_version, new_konsist_version)
+
+    create_pull_request_to_main(new_konsist_version)
+
+    check_github_checks(release_branch_title)
+
+    merge_release_pr(release_branch_title)
+
+    create_github_release(new_konsist_version)
+
+    update_version_in_konsist_documentation(konsist_documentation_repository_address, old_konsist_version, new_konsist_version)
+
+    update_snippets_in_konsist_documentation()
+
+    change_branch_to_develop_and_and_merge_main()
 
 # Script ===============================================================================================================
 create_release()
