e081a4b630ed3bebd35b30cdc5fe9af767254c40
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-17 10:37:24 +0200

    add more tests in KoTypeDeclarationForKoTypeProviderTest

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/type/kotype/KoTypeDeclarationForKoTypeProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/type/kotype/KoTypeDeclarationForKoTypeProviderTest.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.core.declaration.type.kotype
 
 import com.lemonappdev.konsist.TestSnippetProvider
+import io.kotest.matchers.equals.shouldBeEqual
 import org.amshove.kluent.assertSoftly
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.params.ParameterizedTest
@@ -12,9 +13,17 @@ class KoTypeDeclarationForKoTypeProviderTest {
     @MethodSource("provideValues")
     fun `kotlin-type`(
         fileName: String,
+        isClass: Boolean,
+        isObject: Boolean,
+        isInterface: Boolean,
+        isTypeAlias: Boolean,
+        isImportAlias: Boolean,
         isKotlinType: Boolean,
         isKotlinBasicType: Boolean,
         isKotlinCollectionType: Boolean,
+        isFunctionType: Boolean,
+        isGenericType: Boolean,
+        isExternalType: Boolean,
     ) {
         // given
         val sut =
@@ -28,9 +37,17 @@ class KoTypeDeclarationForKoTypeProviderTest {
 
         // then
         assertSoftly(sut) {
+            it?.isClass shouldBeEqualTo isClass
+            it?.isObject shouldBeEqualTo isObject
+            it?.isInterface shouldBeEqualTo isInterface
+            it?.isTypeAlias shouldBeEqualTo isTypeAlias
+            it?.isImportAlias shouldBeEqualTo isImportAlias
             it?.isKotlinType shouldBeEqualTo isKotlinType
             it?.isKotlinBasicType shouldBeEqualTo isKotlinBasicType
             it?.isKotlinCollectionType shouldBeEqualTo isKotlinCollectionType
+            it?.isFunctionType shouldBeEqualTo isFunctionType
+            it?.isGenericType shouldBeEqualTo isGenericType
+            it?.isExternalType shouldBeEqualTo isExternalType
         }
     }
 
@@ -42,24 +59,232 @@ class KoTypeDeclarationForKoTypeProviderTest {
         @JvmStatic
         fun provideValues() =
             listOf(
-                arguments("nullable-kotlin-basic-type-is-kotlin-type", true, true, false),
-                arguments("not-nullable-kotlin-basic-type-is-kotlin-type", true, true, false),
-                arguments("nullable-kotlin-collection-type-is-kotlin-type", true, false, true),
-                arguments("not-nullable-kotlin-collection-type-is-kotlin-type", true, false, true),
-                arguments("nullable-class-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-class-type-is-not-kotlin-type", false, false, false),
-                arguments("nullable-interface-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-interface-type-is-not-kotlin-type", false, false, false),
-                arguments("nullable-object-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-object-type-is-not-kotlin-type", false, false, false),
-                arguments("nullable-function-type-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-function-type-type-is-not-kotlin-type", false, false, false),
-                arguments("nullable-import-alias-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-import-alias-type-is-not-kotlin-type", false, false, false),
-                arguments("nullable-typealias-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-typealias-type-is-not-kotlin-type", false, false, false),
-                arguments("nullable-external-type-is-not-kotlin-type", false, false, false),
-                arguments("not-nullable-external-type-is-not-kotlin-type", false, false, false),
+                arguments(
+                    "nullable-kotlin-basic-type-is-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-kotlin-basic-type-is-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+//                arguments("nullable-kotlin-collection-type-is-kotlin-type", false, false, false, false, false, true, false, true, false, false, false),
+//                arguments("not-nullable-kotlin-collection-type-is-kotlin-type", false, false, false, false, false, true, false, true, false, false, false),
+                arguments(
+                    "nullable-class-type-is-not-kotlin-type",
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-class-type-is-not-kotlin-type",
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "nullable-interface-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-interface-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "nullable-object-type-is-not-kotlin-type",
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-object-type-is-not-kotlin-type",
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "nullable-function-type-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-function-type-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false
+                ),
+                arguments(
+                    "nullable-import-alias-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-import-alias-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "nullable-typealias-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "not-nullable-typealias-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    true,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false
+                ),
+                arguments(
+                    "nullable-external-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true
+                ),
+                arguments(
+                    "not-nullable-external-type-is-not-kotlin-type",
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                    true
+                ),
             )
     }
 }
