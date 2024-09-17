228bb5680461fbb873a39f1b7c5c5481e9bb70c5
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-17 15:35:41 +0200

    rename

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/type/kogenerictype/KoGenericTypeDeclarationForKoModuleProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/type/kogenerictype/KoGenericTypeDeclarationForKoModuleProviderTest.kt
@@ -1,6 +1,6 @@
 package com.lemonappdev.konsist.core.declaration.type.kogenerictype
 
 class KoGenericTypeDeclarationForKoModuleProviderTest {
-    // Konsist koFunctionType.moduleName and koFunctionType.resideInModule() tests
+    // Konsist koGenericType.moduleName and koGenericType.resideInModule() tests
     // are implemented in the test\konsist-declaration-tester project.
 }
--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/type/kogenerictype/KoGenericTypeDeclarationForKoSourceSetProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/type/kogenerictype/KoGenericTypeDeclarationForKoSourceSetProviderTest.kt
@@ -1,6 +1,6 @@
 package com.lemonappdev.konsist.core.declaration.type.kogenerictype
 
 class KoGenericTypeDeclarationForKoSourceSetProviderTest {
-    // Konsist koFunctionType.sourceSetName and koFunctionType.resideInSourceSet() tests
+    // Konsist koGenericType.sourceSetName and koGenericType.resideInSourceSet() tests
     // are implemented in the test\konsist-declaration-tester project.
 }
