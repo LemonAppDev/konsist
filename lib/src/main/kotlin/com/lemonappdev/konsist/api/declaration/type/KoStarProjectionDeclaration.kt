223d36da0401fa639a2b1955f6b9920926de7177
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-20 12:59:51 +0200

    add KoStarProjectionDeclaration

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/type/KoStarProjectionDeclaration.kt
@@ -0,0 +1,17 @@
+package com.lemonappdev.konsist.api.declaration.type
+
+import com.lemonappdev.konsist.api.provider.KoContainingDeclarationProvider
+import com.lemonappdev.konsist.api.provider.KoContainingFileProvider
+import com.lemonappdev.konsist.api.provider.KoLocationProvider
+import com.lemonappdev.konsist.api.provider.KoModuleProvider
+import com.lemonappdev.konsist.api.provider.KoPathProvider
+import com.lemonappdev.konsist.api.provider.KoSourceSetProvider
+
+interface KoStarProjectionDeclaration :
+    KoBaseTypeDeclaration,
+    KoContainingFileProvider,
+    KoContainingDeclarationProvider,
+    KoLocationProvider,
+    KoPathProvider,
+    KoModuleProvider,
+    KoSourceSetProvider
