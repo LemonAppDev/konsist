4026d2b9e0bbc37c14253b8499f4d002ba78c607
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-24 15:06:35 +0200

    add more properties and functions in KoGenericTypeDeclarationProvider and KoGenericTypeDeclarationProviderListExt

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoGenericTypeDeclarationProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoGenericTypeDeclarationProvider.kt
@@ -1,6 +1,7 @@
 package com.lemonappdev.konsist.api.provider
 
 import com.lemonappdev.konsist.api.declaration.type.KoTypeDeclaration
+import kotlin.reflect.KClass
 
 interface KoGenericTypeDeclarationProvider : KoBaseProvider {
     /**
@@ -21,6 +22,8 @@ interface KoGenericTypeDeclarationProvider : KoBaseProvider {
      */
     val typeArguments: List<KoTypeDeclaration>
 
+    val numTypeArguments: Int
+
     /**
      * A list of type declarations representing all type arguments, including nested ones.
      *
@@ -30,4 +33,66 @@ interface KoGenericTypeDeclarationProvider : KoBaseProvider {
      * 2) In `List<Set<String>>`, the list will contain `Set` and `String`.
      */
     val typeArgumentsFlatten: List<KoTypeDeclaration>
+
+    val numTypeArgumentsFlatten: Int
+
+    fun hasGenericType(predicate: (KoTypeDeclaration) -> Boolean): Boolean
+
+    fun hasGenericTypeOf(kClass: KClass<*>): Boolean
+
+    fun countTypeArguments(predicate: (KoTypeDeclaration) -> Boolean): Int
+
+    fun hasTypeArgumentWithName(
+        name: String,
+        vararg names: String,
+    ): Boolean
+
+    fun hasTypeArgumentWithName(names: Collection<String>): Boolean
+
+    fun hasTypeArgumentsWithAllNames(
+        name: String,
+        vararg names: String,
+    ): Boolean
+
+    fun hasTypeArgumentsWithAllNames(names: Collection<String>): Boolean
+
+    fun hasTypeArgumentOf(name: KClass<*>, vararg names: KClass<*>, ): Boolean
+
+    fun hasTypeArgumentOf(names: Collection<KClass<*>>): Boolean
+
+    fun hasAllTypeArgumentsOf(name: KClass<*>, vararg names: KClass<*>, ): Boolean
+
+    fun hasAllTypeArgumentsOf(names: Collection<KClass<*>>, ): Boolean
+
+    fun hasTypeArgument(predicate: (KoTypeDeclaration) -> Boolean): Boolean
+
+    fun hasAllTypeArguments(predicate: (KoTypeDeclaration) -> Boolean): Boolean
+
+    fun countTypeArgumentsFlatten(predicate: (KoTypeDeclaration) -> Boolean): Int
+
+    fun hasTypeArgumentFlattenWithName(
+        name: String,
+        vararg names: String,
+    ): Boolean
+
+    fun hasTypeArgumentFlattenWithName(names: Collection<String>): Boolean
+
+    fun hasTypeArgumentsFlattenWithAllNames(
+        name: String,
+        vararg names: String,
+    ): Boolean
+
+    fun hasTypeArgumentsFlattenWithAllNames(names: Collection<String>): Boolean
+
+    fun hasTypeArgumentFlattenOf(name: KClass<*>, vararg names: KClass<*>, ): Boolean
+
+    fun hasTypeArgumentFlattenOf(names: Collection<KClass<*>>): Boolean
+
+    fun hasAllTypeArgumentsFlattenOf(name: KClass<*>, vararg names: KClass<*>, ): Boolean
+
+    fun hasAllTypeArgumentsFlattenOf(names: Collection<KClass<*>>, ): Boolean
+
+    fun hasTypeArgumentFlatten(predicate: (KoTypeDeclaration) -> Boolean): Boolean
+
+    fun hasAllTypeArgumentsFlatten(predicate: (KoTypeDeclaration) -> Boolean): Boolean
 }
