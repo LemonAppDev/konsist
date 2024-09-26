cca2d0cccc54f9aea717b6c8feb87514569b3ead
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-26 11:09:03 +0200

    save sample-eternal-library only as jar

--- projects/helper-projects/sample-external-library/src/main/kotlin/com/lemonappdev/konsist/externalsample/SampleExternalClassWithParameter.kt
+++
@@ -1,3 +0,0 @@
-package com.lemonappdev.konsist.externalsample
-
-open class SampleExternalClassWithParameter(val param: String)
--- projects/helper-projects/sample-external-library/src/main/kotlin/com/lemonappdev/konsist/externalsample/SampleExternalGenericClassWithParameter.kt
+++
@@ -1,3 +0,0 @@
-package com.lemonappdev.konsist.externalsample
-
-open class SampleExternalGenericClassWithParameter<T>(val param: String)
