a2277f77ea924930c38dd703b1f4ebafc6d303dc
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-26 13:43:42 +0200

    add kdocs

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoSourceDeclarationProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoSourceDeclarationProvider.kt
@@ -2,6 +2,13 @@ package com.lemonappdev.konsist.api.provider
 
 import com.lemonappdev.konsist.api.declaration.KoBaseDeclaration
 
+/**
+ * Provides access to the source declaration of a declaration.
+ */
 interface KoSourceDeclarationProvider : KoBaseProvider {
+
+    /**
+     * The source declaration associated with this declaration.
+     */
     val sourceDeclaration: KoBaseDeclaration
 }
