b75f7cfd263fafbe48be28dc4395a5315910c1c7
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-09-26 10:40:20 +0200

    clean code

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoIsMutableTypeProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoIsMutableTypeProviderCore.kt
@@ -1,9 +1,6 @@
 package com.lemonappdev.konsist.core.provider
 
 import com.lemonappdev.konsist.api.provider.KoIsMutableTypeProvider
-import com.lemonappdev.konsist.api.provider.KoIsTopLevelProvider
-import org.jetbrains.kotlin.psi.KtTypeParameterListOwner
-import org.jetbrains.kotlin.psi.psiUtil.isTopLevelKtOrJavaMember
 
 internal interface KoIsMutableTypeProviderCore :
     KoIsMutableTypeProvider,
