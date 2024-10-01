8e9012c1e5c133b1a459b7b9aee03d88fffb1e82
Igor Wojda <igor.wojda@gmail.com>
2024-10-01 07:28:09 +0200

    Deprecate KoModifier.FUN

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/modifier/KoFunModifierProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/modifier/KoFunModifierProvider.kt
@@ -5,11 +5,13 @@ import com.lemonappdev.konsist.api.provider.KoBaseProvider
 /**
  * An interface representing a Kotlin declaration that provides information about whether it has `fun` modifier.
  */
+@Deprecated("Will be removed in version 0.19.0", ReplaceWith("koScope.functions()"))
 interface KoFunModifierProvider :
     KoBaseProvider,
     KoModifierProvider {
     /**
      * Determines whatever declaration has a `fun` modifier.
      */
+    @Deprecated("Will be removed in version 0.19.0", ReplaceWith("koScope.functions()"))
     val hasFunModifier: Boolean
 }
