bd444d1dc6ec28ed5d3211083eeb0aebe00c311c
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-02 15:09:44 +0200

    add kdocs

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoTypeArgumentDeclaration.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoTypeArgumentDeclaration.kt
@@ -4,11 +4,28 @@ import com.lemonappdev.konsist.api.declaration.type.KoTypeDeclaration
 import com.lemonappdev.konsist.api.provider.KoBaseProvider
 import com.lemonappdev.konsist.api.provider.KoNameProvider
 
+/**
+ * Represents a type argument declaration.
+ */
 interface KoTypeArgumentDeclaration :
     KoBaseDeclaration,
     KoBaseProvider,
     KoNameProvider {
+    /**
+     * A list of type arguments associated with this declaration, if any.
+     * If there are no type arguments, this returns `null`.
+     *
+     * Example:
+     * In `List<String>`, `String` is a type argument for `List`.
+     */
     val typeArguments: List<KoTypeArgumentDeclaration>?
 
+    /**
+     * The source type declaration associated with this type argument.
+     * This represents the original type that this type argument refers to.
+     *
+     * Example:
+     * In `List<String>`, `List` is the source type declaration for the `String` type argument.
+     */
     val sourceDeclaration: KoTypeDeclaration
 }
