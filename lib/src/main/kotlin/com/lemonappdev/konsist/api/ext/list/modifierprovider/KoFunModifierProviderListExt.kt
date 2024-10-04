fdc9a004d4c736c6ddb8392db287481026441f59
Igor Wojda <igor.wojda@gmail.com>
2024-10-04 09:05:22 +0200

    Revert "KON-653 Deprecate `KoModifier.FUN` (#1404)"
    This reverts commit 85b3fd77f8c16d868acc9db00a49da114d382511.

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/modifierprovider/KoFunModifierProviderListExt.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/ext/list/modifierprovider/KoFunModifierProviderListExt.kt
@@ -7,7 +7,6 @@ import com.lemonappdev.konsist.api.provider.modifier.KoFunModifierProvider
  *
  * @return A list containing declarations with the `fun` modifier.
  */
-@Deprecated("Will be removed in version 0.19.0", ReplaceWith("koScope.functions()"))
 fun <T : KoFunModifierProvider> List<T>.withFunModifier(): List<T> = filter { it.hasFunModifier }
 
 /**
@@ -15,5 +14,4 @@ fun <T : KoFunModifierProvider> List<T>.withFunModifier(): List<T> = filter { it
  *
  * @return A list containing declarations without the `fun` modifier.
  */
-@Deprecated("Will be removed in version 0.19.0", ReplaceWith("koScope.functions()"))
 fun <T : KoFunModifierProvider> List<T>.withoutFunModifier(): List<T> = filterNot { it.hasFunModifier }
