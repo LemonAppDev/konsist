e1303410d6974b37fcfd77e3ec83d1eb674e8a3a
Igor Wojda <igor.wojda@gmail.com>
2024-10-08 22:45:14 +0200

    Extract tests to KdocKonsistTest

---
+++ lib/src/konsistTest/kotlin/com/lemonappdev/konsist/api/KdocKonsistTest.kt
@@ -0,0 +1,31 @@
+package com.lemonappdev.konsist.api
+
+import com.lemonappdev.konsist.api.ext.provider.hasValidKDocParamTags
+import com.lemonappdev.konsist.api.ext.provider.hasValidKDocReturnTag
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.jupiter.api.Test
+
+class KdocKonsistTest {
+    private val apiPackageScope = Konsist.scopeFromPackage("com.lemonappdev.konsist.api..", sourceSetName = "main")
+
+    @Test
+    fun `every api function has valid KDoc`() {
+        apiPackageScope
+            .functions()
+            .assertTrue { it.hasValidKDocParamTags() && it.hasValidKDocReturnTag() }
+    }
+
+    @Test
+    fun `every api property has KDoc`() {
+        apiPackageScope
+            .properties()
+            .assertTrue { it.hasKDoc }
+    }
+
+    @Test
+    fun `every api declaration has KDoc`() {
+        apiPackageScope
+            .classesAndInterfacesAndObjects()
+            .assertTrue { it.hasKDoc }
+    }
+}
