4f732a0744409b1e091967df0d442a1a6d35dc7c
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-10 19:48:39 +0200

    add kdocs

--- lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoSourceDeclaration.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/declaration/KoSourceDeclaration.kt
@@ -7,6 +7,14 @@ import com.lemonappdev.konsist.api.provider.KoTextProvider
 import com.lemonappdev.konsist.api.provider.KoTypeDeclarationProvider
 import com.lemonappdev.konsist.api.provider.KoTypeProvider
 
+/**
+ * Represents a source declaration in the Kotlin codebase.
+ *
+ * A source declaration refers to the underlying object or entity being referenced in the source code.
+ * For example, in an import statement like `import com.samplepackage.SampleClass`, the source declaration
+ * would be the `SampleClass` instance itself, providing access to its properties, name, type, and other
+ * relevant information.
+ */
 interface KoSourceDeclaration :
     KoBaseDeclaration,
     KoBaseProvider,
--- lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoStarProjectionProvider.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/api/provider/KoStarProjectionProvider.kt
@@ -1,5 +1,16 @@
 package com.lemonappdev.konsist.api.provider
 
+/**
+ * Represents a provider that indicates whether a declaration is a star projection.
+ *
+ * A star projection (`*`) is a wildcard used in Kotlin's generic type system to represent
+ * any type, providing flexibility in generic contexts by allowing the use of unknown types
+ * or types that can be substituted by any type argument.
+ *
+ * Example usage:
+ * - A star projection is often seen in generic collections like `List<*>`, where the
+ *   exact type is not known or not important.
+ */
 interface KoStarProjectionProvider : KoBaseProvider {
     /**
      * Determines whether the source declaration is a star projection.
