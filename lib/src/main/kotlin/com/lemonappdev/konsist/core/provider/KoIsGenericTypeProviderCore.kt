c2ad9d4a7a2140b13b74fcab6432a9b694918224
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-17 22:01:08 +0200

    fix tests

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoIsGenericTypeProviderCore.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoIsGenericTypeProviderCore.kt
@@ -5,19 +5,17 @@ import com.lemonappdev.konsist.api.provider.KoIsGenericTypeProvider
 
 internal interface KoIsGenericTypeProviderCore :
     KoIsGenericTypeProvider,
-    KoSourceAndAliasTypeProviderCore,
     KoBaseProviderCore {
     override val isGenericType: Boolean
         get() {
-            val regex = "\\w+<[a-zA-Z*<>]+>".toRegex()
+            val regex = "\\w+<[a-zA-Z*<>, ]+>".toRegex()
 
-            val type =
-                if ((this as? KoTypeDeclaration)?.isTypeAlias == true) {
-                    bareSourceType
-                } else {
-                    sourceType
-                }
+            val type = if ((this as? KoTypeDeclaration)?.isTypeAlias == true) {
+                (this as? KoTypeDeclaration)?.bareSourceType
+            } else {
+                (this as? KoTypeDeclaration)?.name
+            }
 
-            return regex.matches(type)
+            return type?.let { regex.matches(it) } ?: false
         }
 }
