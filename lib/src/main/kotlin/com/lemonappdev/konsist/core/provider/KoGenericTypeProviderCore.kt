131c1ba33b660087939c0a883ca044c300578aa9
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-18 10:05:48 +0200

    revert old KoGenericTypeProvider

---
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/provider/KoGenericTypeProviderCore.kt
@@ -0,0 +1,24 @@
+package com.lemonappdev.konsist.core.provider
+
+import com.lemonappdev.konsist.api.declaration.type.KoTypeDeclaration
+import com.lemonappdev.konsist.api.provider.KoGenericTypeProvider
+
+@Deprecated("Will be removed in version 0.18.0", ReplaceWith("KoIsGenericTypeProviderCore"))
+internal interface KoGenericTypeProviderCore :
+    KoGenericTypeProvider,
+    KoSourceAndAliasTypeProviderCore,
+    KoBaseProviderCore {
+    override val isGenericType: Boolean
+        get() {
+            val regex = "\\w+<[^<>]+>".toRegex()
+
+            val type =
+                if ((this as? KoTypeDeclaration)?.isTypeAlias == true) {
+                    bareSourceType
+                } else {
+                    sourceType
+                }
+
+            return regex.matches(type)
+        }
+}
