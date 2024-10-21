c421fc087823771545d1aa95e54ae40b6e13d68f
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-21 20:25:57 +0200

    Add KoParentDeclarationForKoTextProviderTest

---
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koparent/KoParentDeclarationForKoTextProviderTest.kt
@@ -0,0 +1,695 @@
+package com.lemonappdev.konsist.core.declaration.koparent
+
+import com.lemonappdev.konsist.TestSnippetProvider
+import com.lemonappdev.konsist.api.ext.list.parents
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+class KoParentDeclarationForKoTextProviderTest {
+    @Test
+    fun `class-with-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parent-class-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleSuperClass()"
+    }
+
+    @Test
+    fun `class-with-generic-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-generic-parent-class-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperClass<Int>()"
+    }
+
+    @Test
+    fun `class-with-parametrized-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parametrized-parent-class-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParametrizedSuperClass(\"param\")"
+    }
+
+    @Test
+    fun `class-with-parametrized-and-generic-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parametrized-and-generic-parent-class-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParametrizedSuperClass<Int>(\"param\")"
+    }
+
+    @Test
+    fun `class-with-parent-interface-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parent-interface-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleSuperInterface"
+    }
+
+    @Test
+    fun `class-with-generic-parent-interface-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-generic-parent-interface-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperInterface<Int>"
+    }
+
+    @Test
+    fun `class-with-parent-by-delegation-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parent-by-delegation-from-file")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleSuperInterface by sampleProperty"
+    }
+
+    @Test
+    fun `class-with-multiline-parent-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-multiline-parent-from-file")
+                .classes()
+                .first()
+                .parents()
+                .first()
+
+        // then
+        val text = """
+            SomeParentClass(
+                "some parent constructor param here"
+            )
+        """.trimIndent()
+
+        sut.text shouldBeEqualTo text
+    }
+
+    @Test
+    fun `class-with-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parent-class-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParentClass()"
+    }
+
+    @Test
+    fun `class-with-generic-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-generic-parent-class-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleCollection1<Int>()"
+    }
+
+    @Test
+    fun `class-with-parametrized-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parametrized-parent-class-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleClassWithParameter(\"param\")"
+    }
+
+    @Test
+    fun `class-with-parametrized-and-generic-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parametrized-and-generic-parent-class-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericClassWithParameter<Int>(\"param\")"
+    }
+
+    @Test
+    fun `class-with-parent-interface-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parent-interface-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleInterface"
+    }
+
+    @Test
+    fun `class-with-generic-parent-interface-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-generic-parent-interface-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperInterface<Int>"
+    }
+
+    @Test
+    fun `class-with-parent-by-delegation-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parent-by-delegation-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleInterface by sampleProperty"
+    }
+
+    @Test
+    fun `class-with-multiline-parent-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-multiline-parent-from-import")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        val text = """
+            SampleClassWithParameter(
+                "some parent constructor param here"
+            )
+        """.trimIndent()
+
+        sut.text shouldBeEqualTo text
+    }
+
+    @Test
+    fun `class-with-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-external-parent-class")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalClass()"
+    }
+
+    @Test
+    fun `class-with-generic-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-generic-external-parent-class")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericClass<Int>()"
+    }
+
+    @Test
+    fun `class-with-parametrized-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parametrized-external-parent-class")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalClassWithParameter(\"param\")"
+    }
+
+    @Test
+    fun `class-with-parametrized-and-generic-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-parametrized-and-generic-external-parent-class")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericClassWithParameter<Int>(\"param\")"
+    }
+
+    @Test
+    fun `class-with-external-parent-interface`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-external-parent-interface")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalInterface"
+    }
+
+    @Test
+    fun `class-with-generic-external-parent-interface`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-generic-external-parent-interface")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericInterface<Int>"
+    }
+
+    @Test
+    fun `class-with-external-parent-by-delegation`() {
+        // given
+        val sut =
+            getSnippetFile("class-with-external-parent-by-delegation")
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalInterface by sampleProperty"
+    }
+
+    @Test
+    fun `interface-with-parent-interface-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("interface-with-parent-interface-from-file")
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleSuperInterface"
+    }
+
+    @Test
+    fun `interface-with-generic-parent-interface-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("interface-with-generic-parent-interface-from-file")
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperInterface<Int>"
+    }
+
+    @Test
+    fun `interface-with-parent-interface-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("interface-with-parent-interface-from-import")
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParentInterface"
+    }
+
+    @Test
+    fun `interface-with-generic-parent-interface-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("interface-with-generic-parent-interface-from-import")
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperInterface<Int>"
+    }
+
+    @Test
+    fun `interface-with-external-parent-interface`() {
+        // given
+        val sut =
+            getSnippetFile("interface-with-external-parent-interface")
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalInterface"
+    }
+
+    @Test
+    fun `interface-with-generic-external-parent-interface`() {
+        // given
+        val sut =
+            getSnippetFile("interface-with-generic-external-parent-interface")
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericInterface<Int>"
+    }
+
+    @Test
+    fun `object-with-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parent-class-from-file")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleSuperClass()"
+    }
+
+    @Test
+    fun `object-with-generic-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-generic-parent-class-from-file")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperClass<Int>()"
+    }
+
+    @Test
+    fun `object-with-parametrized-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parametrized-parent-class-from-file")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParametrizedSuperClass(\"param\")"
+    }
+
+    @Test
+    fun `object-with-parametrized-and-generic-parent-class-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parametrized-and-generic-parent-class-from-file")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParametrizedSuperClass<Int>(\"param\")"
+    }
+
+    @Test
+    fun `object-with-parent-interface-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parent-interface-from-file")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleSuperInterface"
+    }
+
+    @Test
+    fun `object-with-generic-parent-interface-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-generic-parent-interface-from-file")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperInterface<Int>"
+    }
+
+    @Test
+    fun `object-with-multiline-parent-from-file`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-multiline-parent-from-file")
+                .objects()
+                .first()
+                .parents()
+                .first()
+
+        // then
+        val text = """
+            SomeParentClass(
+                "some parent constructor param here"
+            )
+        """.trimIndent()
+
+        sut.text shouldBeEqualTo text
+    }
+
+    @Test
+    fun `object-with-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parent-class-from-import")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleParentClass()"
+    }
+
+    @Test
+    fun `object-with-generic-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-generic-parent-class-from-import")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleCollection1<Int>()"
+    }
+
+    @Test
+    fun `object-with-parametrized-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parametrized-parent-class-from-import")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleClassWithParameter(\"param\")"
+    }
+
+    @Test
+    fun `object-with-parametrized-and-generic-parent-class-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parametrized-and-generic-parent-class-from-import")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericClassWithParameter<Int>(\"param\")"
+    }
+
+    @Test
+    fun `object-with-parent-interface-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parent-interface-from-import")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleInterface"
+    }
+
+    @Test
+    fun `object-with-generic-parent-interface-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-generic-parent-interface-from-import")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleGenericSuperInterface<Int>"
+    }
+
+    @Test
+    fun `object-with-multiline-parent-from-import`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-multiline-parent-from-import")
+                .objects()
+                .first()
+                .parents()
+                .first()
+
+        // then
+        val text = """
+            SampleClassWithParameter(
+                "some parent constructor param here"
+            )
+        """.trimIndent()
+
+        sut.text shouldBeEqualTo text
+    }
+
+    @Test
+    fun `object-with-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-external-parent-class")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalClass()"
+    }
+
+    @Test
+    fun `object-with-generic-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-generic-external-parent-class")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericClass<Int>()"
+    }
+
+    @Test
+    fun `object-with-parametrized-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parametrized-external-parent-class")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalClassWithParameter(\"param\")"
+    }
+
+    @Test
+    fun `object-with-parametrized-and-generic-external-parent-class`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-parametrized-and-generic-external-parent-class")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericClassWithParameter<Int>(\"param\")"
+    }
+
+    @Test
+    fun `object-with-external-parent-interface`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-external-parent-interface")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalInterface"
+    }
+
+    @Test
+    fun `object-with-generic-external-parent-interface`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-generic-external-parent-interface")
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        sut.text shouldBeEqualTo "SampleExternalGenericInterface<Int>"
+    }
+
+    @Test
+    fun `object-with-multiline-external-parent`() {
+        // given
+        val sut =
+            getSnippetFile("object-with-multiline-external-parent")
+                .objects()
+                .first()
+                .parents()
+                .first()
+
+        // then
+        val text = """
+            SampleExternalClassWithParameter(
+                "some parent constructor param here"
+            )
+        """.trimIndent()
+
+        sut.text shouldBeEqualTo text
+    }
+
+    private fun getSnippetFile(fileName: String) =
+        TestSnippetProvider.getSnippetKoScope("core/declaration/koparent/snippet/forkotextprovider/", fileName)
+}
