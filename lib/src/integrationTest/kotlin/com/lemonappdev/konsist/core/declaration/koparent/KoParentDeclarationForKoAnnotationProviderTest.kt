6239c327e723c30a40aa68da2e65cfaf39d2e5e6
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-25 09:54:14 +0200

    upd snippets

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koparent/KoParentDeclarationForKoAnnotationProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koparent/KoParentDeclarationForKoAnnotationProviderTest.kt
@@ -2,6 +2,7 @@ package com.lemonappdev.konsist.core.declaration.koparent
 
 import com.lemonappdev.konsist.TestSnippetProvider
 import com.lemonappdev.konsist.api.ext.list.parents
+import com.lemonappdev.konsist.testdata.NonExistingAnnotation
 import com.lemonappdev.konsist.testdata.SampleAnnotation
 import com.lemonappdev.konsist.testdata.SampleAnnotation1
 import com.lemonappdev.konsist.testdata.SampleAnnotation2
@@ -51,6 +52,125 @@ class KoParentDeclarationForKoAnnotationProviderTest {
             hasAllAnnotationsOf(SampleAnnotation1::class, SampleAnnotation2::class) shouldBeEqualTo false
             hasAllAnnotationsOf(listOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo false
             hasAllAnnotationsOf(setOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo false
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideClassesForTwoAnnotations")
+    fun `class-parent-has-two-annotations`(fileName: String) {
+        // given
+        val sut =
+            getSnippetFile(fileName)
+                .classes()
+                .parents()
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            numAnnotations shouldBeEqualTo 2
+            countAnnotations { type -> type.hasNameStartingWith("Sample") } shouldBeEqualTo 2
+            countAnnotations { type -> type.name == "SampleAnnotation1" } shouldBeEqualTo 1
+            hasAnnotations() shouldBeEqualTo true
+            hasAnnotationOf(emptyList()) shouldBeEqualTo true
+            hasAnnotationOf(emptySet()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptyList()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptySet()) shouldBeEqualTo true
+            hasAnnotationWithName("SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationWithName("OtherAnnotation") shouldBeEqualTo false
+            hasAnnotationWithName("SampleAnnotation1", "OtherAnnotation") shouldBeEqualTo true
+            hasAnnotationWithName("com.lemonappdev.konsist.testdata.SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationWithName("com.lemonappdev.konsist.testdata.NonExistingAnnotation") shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                ).shouldBeEqualTo(true)
+            hasAnnotationWithName(listOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationWithName(listOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("com.lemonappdev.konsist.testdata.NonExistingAnnotation")) shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotationWithName(setOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationWithName(setOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("com.lemonappdev.konsist.testdata.NonExistingAnnotation")) shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    setOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotationsWithAllNames("SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationsWithAllNames("SampleAnnotation1", "SampleAnnotation2") shouldBeEqualTo true
+            hasAnnotationsWithAllNames("SampleAnnotation1", "OtherAnnotation") shouldBeEqualTo false
+            hasAnnotationsWithAllNames("com.lemonappdev.konsist.testdata.SampleAnnotation1") shouldBeEqualTo true
+            it
+                .hasAnnotationsWithAllNames(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                ).shouldBeEqualTo(false)
+            it
+                .hasAnnotationsWithAllNames(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation2",
+                ).shouldBeEqualTo(true)
+
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1", "SampleAnnotation2")) shouldBeEqualTo true
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationsWithAllNames(listOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            it
+                .hasAnnotationsWithAllNames(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(false)
+            it
+                .hasAnnotationsWithAllNames(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation2",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotation { type -> type.name == "SampleAnnotation1" } shouldBeEqualTo true
+            hasAnnotation { type -> type.name == "OtherAnnotation1" } shouldBeEqualTo false
+            hasAllAnnotations { type -> !type.hasArguments() } shouldBeEqualTo true
+            hasAllAnnotations { type -> type.hasNameEndingWith("tion1") } shouldBeEqualTo false
+            hasAnnotationOf(emptyList()) shouldBeEqualTo true
+            hasAnnotationOf(emptySet()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptyList()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptySet()) shouldBeEqualTo true
+            hasAnnotationOf(SampleAnnotation1::class) shouldBeEqualTo true
+            hasAnnotationOf(NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAnnotationOf(SampleAnnotation1::class, NonExistingAnnotation::class) shouldBeEqualTo true
+            hasAnnotationOf(listOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAnnotationOf(listOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAnnotationOf(listOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo true
+            hasAnnotationOf(setOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAnnotationOf(setOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAnnotationOf(setOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(SampleAnnotation1::class) shouldBeEqualTo true
+            hasAllAnnotationsOf(NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAllAnnotationsOf(SampleAnnotation1::class, SampleAnnotation2::class) shouldBeEqualTo true
+            hasAllAnnotationsOf(SampleAnnotation1::class, NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(listOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(setOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo false
         }
     }
 
@@ -144,67 +264,80 @@ class KoParentDeclarationForKoAnnotationProviderTest {
         @JvmStatic
         fun provideClassesForNoAnnotation() =
             listOf(
-                arguments("class-with-parent-class-from-file"),
-                arguments("class-with-generic-parent-class-from-file"),
-                arguments("class-with-parametrized-parent-class-from-file"),
-                arguments("class-with-parametrized-and-generic-parent-class-from-file"),
-                arguments("class-with-parent-interface-from-file"),
-                arguments("class-with-generic-parent-interface-from-file"),
-                arguments("class-with-parent-by-delegation-from-file"),
-                arguments("class-with-multiline-parent-from-file"),
-                arguments("class-with-parent-class-from-import"),
-                arguments("class-with-generic-parent-class-from-import"),
-                arguments("class-with-parametrized-parent-class-from-import"),
-                arguments("class-with-parametrized-and-generic-parent-class-from-import"),
-                arguments("class-with-parent-interface-from-import"),
-                arguments("class-with-generic-parent-interface-from-import"),
-                arguments("class-with-parent-by-delegation-from-import"),
-                arguments("class-with-multiline-parent-from-import"),
-                arguments("class-with-external-parent-class"),
-                arguments("class-with-generic-external-parent-class"),
-                arguments("class-with-parametrized-external-parent-class"),
-                arguments("class-with-parametrized-and-generic-external-parent-class"),
-                arguments("class-with-external-parent-interface"),
-                arguments("class-with-generic-external-parent-interface"),
-                arguments("class-with-external-parent-by-delegation"),
+                arguments("class-with-parent-class-without-annotation"),
+                arguments("class-with-parametrized-and-generic-parent-class-without-annotation"),
+                arguments("class-with-parent-interface-without-annotation"),
+                arguments("class-with-generic-parent-interface-without-annotation"),
+                arguments("class-with-parent-by-delegation-without-annotation"),
+                arguments("class-with-external-parent-class-without-annotation"),
+                arguments("class-with-parametrized-and-generic-external-parent-class-without-annotation"),
+                arguments("class-with-external-parent-interface-without-annotation"),
+                arguments("class-with-generic-external-parent-interface-without-annotation"),
+                arguments("class-with-external-parent-by-delegation-without-annotation"),
+            )
+
+        @Suppress("unused")
+        @JvmStatic
+        fun provideClassesForTwoAnnotations() =
+            listOf(
+                arguments("class-with-parent-class-with-two-annotations"),
+                arguments("class-with-parametrized-and-generic-parent-class-with-two-annotations"),
+                arguments("class-with-parent-interface-with-two-annotations"),
+                arguments("class-with-generic-parent-interface-with-two-annotations"),
+                arguments("class-with-parent-by-delegation-with-two-annotations"),
+                arguments("class-with-external-parent-class-with-two-annotations"),
+                arguments("class-with-parametrized-and-generic-external-parent-class-with-two-annotations"),
+                arguments("class-with-external-parent-interface-with-two-annotations"),
+                arguments("class-with-generic-external-parent-interface-with-two-annotations"),
+                arguments("class-with-external-parent-by-delegation-with-two-annotations"),
             )
 
         @Suppress("unused")
         @JvmStatic
         fun provideInterfacesForNoAnnotation() =
             listOf(
-                arguments("interface-with-parent-interface-from-file"),
-                arguments("interface-with-generic-parent-interface-from-file"),
-                arguments("interface-with-parent-interface-from-import"),
-                arguments("interface-with-generic-parent-interface-from-import"),
-                arguments("interface-with-external-parent-interface"),
-                arguments("interface-with-generic-external-parent-interface"),
+                arguments("interface-with-parent-interface-without-annotation"),
+                arguments("interface-with-generic-parent-interface-without-annotation"),
+                arguments("interface-with-external-parent-interface-without-annotation"),
+                arguments("interface-with-generic-external-parent-interface-without-annotation"),
+            )
+
+        @Suppress("unused")
+        @JvmStatic
+        fun provideInterfacesForTwoAnnotations() =
+            listOf(
+                arguments("interface-with-parent-interface-with-two-annotations"),
+                arguments("interface-with-generic-parent-interface-with-two-annotations"),
+                arguments("interface-with-external-parent-interface-with-two-annotations"),
+                arguments("interface-with-generic-external-parent-interface-with-two-annotations"),
             )
 
         @Suppress("unused")
         @JvmStatic
         fun provideObjectsForNoAnnotation() =
             listOf(
-                arguments("object-with-parent-class-from-file"),
-                arguments("object-with-generic-parent-class-from-file"),
-                arguments("object-with-parametrized-parent-class-from-file"),
-                arguments("object-with-parametrized-and-generic-parent-class-from-file"),
-                arguments("object-with-parent-interface-from-file"),
-                arguments("object-with-generic-parent-interface-from-file"),
-                arguments("object-with-multiline-parent-from-file"),
-                arguments("object-with-parent-class-from-import"),
-                arguments("object-with-generic-parent-class-from-import"),
-                arguments("object-with-parametrized-parent-class-from-import"),
-                arguments("object-with-parametrized-and-generic-parent-class-from-import"),
-                arguments("object-with-parent-interface-from-import"),
-                arguments("object-with-generic-parent-interface-from-import"),
-                arguments("object-with-multiline-parent-from-import"),
-                arguments("object-with-external-parent-class"),
-                arguments("object-with-generic-external-parent-class"),
-                arguments("object-with-parametrized-external-parent-class"),
-                arguments("object-with-parametrized-and-generic-external-parent-class"),
-                arguments("object-with-external-parent-interface"),
-                arguments("object-with-generic-external-parent-interface"),
+                arguments("object-with-parent-class-without-annotation"),
+                arguments("object-with-parametrized-and-generic-parent-class-without-annotation"),
+                arguments("object-with-parent-interface-without-annotation"),
+                arguments("object-with-generic-parent-interface-without-annotation"),
+                arguments("object-with-external-parent-class-without-annotation"),
+                arguments("object-with-parametrized-and-generic-external-parent-class-without-annotation"),
+                arguments("object-with-external-parent-interface-without-annotation"),
+                arguments("object-with-generic-external-parent-interface-without-annotation"),
+            )
+
+        @Suppress("unused")
+        @JvmStatic
+        fun provideObjectsForTwoAnnotations() =
+            listOf(
+                arguments("object-with-parent-class-with-two-annotations"),
+                arguments("object-with-parametrized-and-generic-parent-class-with-two-annotations"),
+                arguments("object-with-parent-interface-with-two-annotations"),
+                arguments("object-with-generic-parent-interface-with-two-annotations"),
+                arguments("object-with-external-parent-class-with-two-annotations"),
+                arguments("object-with-parametrized-and-generic-external-parent-class-with-two-annotations"),
+                arguments("object-with-external-parent-interface-with-two-annotations"),
+                arguments("object-with-generic-external-parent-interface-with-two-annotations"),
             )
     }
 }
