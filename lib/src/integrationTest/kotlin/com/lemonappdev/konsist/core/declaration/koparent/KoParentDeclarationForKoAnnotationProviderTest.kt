abae772ed77ce622b56a8402a2fa5f47075a6490
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-10-25 10:09:39 +0200

    upd snippets

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koparent/KoParentDeclarationForKoAnnotationProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koparent/KoParentDeclarationForKoAnnotationProviderTest.kt
@@ -216,6 +216,125 @@ class KoParentDeclarationForKoAnnotationProviderTest {
     }
 
     @ParameterizedTest
+    @MethodSource("provideInterfacesForTwoAnnotations")
+    fun `interface-parent-has-two-annotations`(fileName: String) {
+        // given
+        val sut =
+            getSnippetFile(fileName)
+                .interfaces()
+                .parents()
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            numAnnotations shouldBeEqualTo 2
+            countAnnotations { type -> type.hasNameStartingWith("Sample") } shouldBeEqualTo 2
+            countAnnotations { type -> type.name == "SampleAnnotation1" } shouldBeEqualTo 1
+            hasAnnotations() shouldBeEqualTo true
+            hasAnnotationOf(emptyList()) shouldBeEqualTo true
+            hasAnnotationOf(emptySet()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptyList()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptySet()) shouldBeEqualTo true
+            hasAnnotationWithName("SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationWithName("OtherAnnotation") shouldBeEqualTo false
+            hasAnnotationWithName("SampleAnnotation1", "OtherAnnotation") shouldBeEqualTo true
+            hasAnnotationWithName("com.lemonappdev.konsist.testdata.SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationWithName("com.lemonappdev.konsist.testdata.NonExistingAnnotation") shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                ).shouldBeEqualTo(true)
+            hasAnnotationWithName(listOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationWithName(listOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("com.lemonappdev.konsist.testdata.NonExistingAnnotation")) shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotationWithName(setOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationWithName(setOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("com.lemonappdev.konsist.testdata.NonExistingAnnotation")) shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    setOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotationsWithAllNames("SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationsWithAllNames("SampleAnnotation1", "SampleAnnotation2") shouldBeEqualTo true
+            hasAnnotationsWithAllNames("SampleAnnotation1", "OtherAnnotation") shouldBeEqualTo false
+            hasAnnotationsWithAllNames("com.lemonappdev.konsist.testdata.SampleAnnotation1") shouldBeEqualTo true
+            it
+                .hasAnnotationsWithAllNames(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                ).shouldBeEqualTo(false)
+            it
+                .hasAnnotationsWithAllNames(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation2",
+                ).shouldBeEqualTo(true)
+
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1", "SampleAnnotation2")) shouldBeEqualTo true
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationsWithAllNames(listOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            it
+                .hasAnnotationsWithAllNames(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(false)
+            it
+                .hasAnnotationsWithAllNames(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation2",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotation { type -> type.name == "SampleAnnotation1" } shouldBeEqualTo true
+            hasAnnotation { type -> type.name == "OtherAnnotation1" } shouldBeEqualTo false
+            hasAllAnnotations { type -> !type.hasArguments() } shouldBeEqualTo true
+            hasAllAnnotations { type -> type.hasNameEndingWith("tion1") } shouldBeEqualTo false
+            hasAnnotationOf(emptyList()) shouldBeEqualTo true
+            hasAnnotationOf(emptySet()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptyList()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptySet()) shouldBeEqualTo true
+            hasAnnotationOf(SampleAnnotation1::class) shouldBeEqualTo true
+            hasAnnotationOf(NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAnnotationOf(SampleAnnotation1::class, NonExistingAnnotation::class) shouldBeEqualTo true
+            hasAnnotationOf(listOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAnnotationOf(listOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAnnotationOf(listOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo true
+            hasAnnotationOf(setOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAnnotationOf(setOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAnnotationOf(setOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(SampleAnnotation1::class) shouldBeEqualTo true
+            hasAllAnnotationsOf(NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAllAnnotationsOf(SampleAnnotation1::class, SampleAnnotation2::class) shouldBeEqualTo true
+            hasAllAnnotationsOf(SampleAnnotation1::class, NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(listOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(setOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo false
+        }
+    }
+
+    @ParameterizedTest
     @MethodSource("provideObjectsForNoAnnotation")
     fun `object-parent-has-no-annotation`(fileName: String) {
         // given
@@ -253,6 +372,125 @@ class KoParentDeclarationForKoAnnotationProviderTest {
             hasAllAnnotationsOf(SampleAnnotation1::class, SampleAnnotation2::class) shouldBeEqualTo false
             hasAllAnnotationsOf(listOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo false
             hasAllAnnotationsOf(setOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo false
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideObjectsForTwoAnnotations")
+    fun `object-parent-has-two-annotations`(fileName: String) {
+        // given
+        val sut =
+            getSnippetFile(fileName)
+                .objects()
+                .parents()
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            numAnnotations shouldBeEqualTo 2
+            countAnnotations { type -> type.hasNameStartingWith("Sample") } shouldBeEqualTo 2
+            countAnnotations { type -> type.name == "SampleAnnotation1" } shouldBeEqualTo 1
+            hasAnnotations() shouldBeEqualTo true
+            hasAnnotationOf(emptyList()) shouldBeEqualTo true
+            hasAnnotationOf(emptySet()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptyList()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptySet()) shouldBeEqualTo true
+            hasAnnotationWithName("SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationWithName("OtherAnnotation") shouldBeEqualTo false
+            hasAnnotationWithName("SampleAnnotation1", "OtherAnnotation") shouldBeEqualTo true
+            hasAnnotationWithName("com.lemonappdev.konsist.testdata.SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationWithName("com.lemonappdev.konsist.testdata.NonExistingAnnotation") shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                ).shouldBeEqualTo(true)
+            hasAnnotationWithName(listOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationWithName(listOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(listOf("com.lemonappdev.konsist.testdata.NonExistingAnnotation")) shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotationWithName(setOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationWithName(setOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationWithName(setOf("com.lemonappdev.konsist.testdata.NonExistingAnnotation")) shouldBeEqualTo false
+            it
+                .hasAnnotationWithName(
+                    setOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotationsWithAllNames("SampleAnnotation1") shouldBeEqualTo true
+            hasAnnotationsWithAllNames("SampleAnnotation1", "SampleAnnotation2") shouldBeEqualTo true
+            hasAnnotationsWithAllNames("SampleAnnotation1", "OtherAnnotation") shouldBeEqualTo false
+            hasAnnotationsWithAllNames("com.lemonappdev.konsist.testdata.SampleAnnotation1") shouldBeEqualTo true
+            it
+                .hasAnnotationsWithAllNames(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                ).shouldBeEqualTo(false)
+            it
+                .hasAnnotationsWithAllNames(
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                    "com.lemonappdev.konsist.testdata.SampleAnnotation2",
+                ).shouldBeEqualTo(true)
+
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1")) shouldBeEqualTo true
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1", "SampleAnnotation2")) shouldBeEqualTo true
+            hasAnnotationsWithAllNames(listOf("SampleAnnotation1", "OtherAnnotation")) shouldBeEqualTo false
+            hasAnnotationsWithAllNames(listOf("com.lemonappdev.konsist.testdata.SampleAnnotation1")) shouldBeEqualTo true
+            it
+                .hasAnnotationsWithAllNames(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.NonExistingAnnotation",
+                    ),
+                ).shouldBeEqualTo(false)
+            it
+                .hasAnnotationsWithAllNames(
+                    listOf(
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation1",
+                        "com.lemonappdev.konsist.testdata.SampleAnnotation2",
+                    ),
+                ).shouldBeEqualTo(true)
+            hasAnnotation { type -> type.name == "SampleAnnotation1" } shouldBeEqualTo true
+            hasAnnotation { type -> type.name == "OtherAnnotation1" } shouldBeEqualTo false
+            hasAllAnnotations { type -> !type.hasArguments() } shouldBeEqualTo true
+            hasAllAnnotations { type -> type.hasNameEndingWith("tion1") } shouldBeEqualTo false
+            hasAnnotationOf(emptyList()) shouldBeEqualTo true
+            hasAnnotationOf(emptySet()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptyList()) shouldBeEqualTo true
+            hasAllAnnotationsOf(emptySet()) shouldBeEqualTo true
+            hasAnnotationOf(SampleAnnotation1::class) shouldBeEqualTo true
+            hasAnnotationOf(NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAnnotationOf(SampleAnnotation1::class, NonExistingAnnotation::class) shouldBeEqualTo true
+            hasAnnotationOf(listOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAnnotationOf(listOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAnnotationOf(listOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo true
+            hasAnnotationOf(setOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAnnotationOf(setOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAnnotationOf(setOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(SampleAnnotation1::class) shouldBeEqualTo true
+            hasAllAnnotationsOf(NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAllAnnotationsOf(SampleAnnotation1::class, SampleAnnotation2::class) shouldBeEqualTo true
+            hasAllAnnotationsOf(SampleAnnotation1::class, NonExistingAnnotation::class) shouldBeEqualTo false
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(listOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(listOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(setOf(NonExistingAnnotation::class)) shouldBeEqualTo false
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class, SampleAnnotation2::class)) shouldBeEqualTo true
+            hasAllAnnotationsOf(setOf(SampleAnnotation1::class, NonExistingAnnotation::class)) shouldBeEqualTo false
         }
     }
 
