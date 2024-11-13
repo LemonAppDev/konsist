6f761682131f3bf84f8d872a73de377dde155035
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-11-13 13:08:31 +0100

    update toHyperlink logic

--- lib/src/main/kotlin/com/lemonappdev/konsist/core/util/HyperlinkUtil.kt
+++ lib/src/main/kotlin/com/lemonappdev/konsist/core/util/HyperlinkUtil.kt
@@ -3,8 +3,14 @@ package com.lemonappdev.konsist.core.util
 import java.io.File
 
 object HyperlinkUtil {
+    private const val FILE_PREFIX = "file://"
+
     fun toHyperlink(path: String): String {
         val absolutePath = File(path).absolutePath
-        return "file://$absolutePath"
+        return if (!absolutePath.startsWith(FILE_PREFIX)) {
+            "$FILE_PREFIX$absolutePath"
+        } else {
+            absolutePath
+        }
     }
 }
