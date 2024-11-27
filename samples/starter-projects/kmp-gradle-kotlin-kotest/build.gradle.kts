75ad27a8ce6e48eae79da077408ca4eb39d22f27
Igor Wojda <igor.wojda@gmail.com>
2024-11-27 16:13:31 +0100

    Revert "Update kotlin monorepo to v2.1.0 (#1590)"
    This reverts commit 24e2265a795bce9b971c5b73a034189fff969dac.

--- samples/starter-projects/kmp-gradle-kotlin-kotest/build.gradle.kts
+++ samples/starter-projects/kmp-gradle-kotlin-kotest/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "2.1.0"
+    kotlin("multiplatform") version "2.0.21"
     id("io.kotest.multiplatform") version "6.0.0-LOCAL"
     application
     id("com.android.application")
--- samples/starter-projects/spring-gradle-kotlin-kotest/build.gradle.kts
+++ samples/starter-projects/spring-gradle-kotlin-kotest/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "2.1.0"
+    kotlin("plugin.spring") version "2.0.21"
     `jvm-test-suite`
 }
 
