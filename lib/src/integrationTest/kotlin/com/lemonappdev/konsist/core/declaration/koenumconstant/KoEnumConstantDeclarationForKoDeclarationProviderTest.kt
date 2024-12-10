2958995eba2d64a4e2957a73d11c9e21290233d7
NataliaPeterwas <111683562+nataliapeterwas@users.noreply.github.com>
2024-12-10 15:59:52 +0100

    add more tests

--- lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koenumconstant/KoEnumConstantDeclarationForKoDeclarationProviderTest.kt
+++ lib/src/integrationTest/kotlin/com/lemonappdev/konsist/core/declaration/koenumconstant/KoEnumConstantDeclarationForKoDeclarationProviderTest.kt
@@ -44,7 +44,7 @@ class KoEnumConstantDeclarationForKoDeclarationProviderTest {
             hasDeclarations() shouldBeEqualTo true
             hasDeclaration { (it as KoNameProvider).name == "sampleProperty" } shouldBeEqualTo true
             hasDeclaration { (it as KoNameProvider).name == "otherProperty" } shouldBeEqualTo false
-            hasAllDeclarations { (it as KoNameProvider).hasNameContaining("") } shouldBeEqualTo true
+            hasAllDeclarations { (it as KoNameProvider).hasNameContaining("ample") } shouldBeEqualTo true
             hasAllDeclarations { (it as KoNameProvider).hasNameStartingWith("sample") } shouldBeEqualTo false
             declarations()
                 .filterIsInstance<KoNameProvider>()
@@ -58,6 +58,193 @@ class KoEnumConstantDeclarationForKoDeclarationProviderTest {
                 )
         }
     }
+
+    @Test
+    fun `enum-constant-contains-nested-and-local-declarations includeLocal=true`() {
+        // given
+        val sut =
+            getSnippetFile("enum-constant-contains-nested-and-local-declarations")
+                .classes()
+                .enumConstants
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            numDeclarations(includeLocal = true, includeNested = false) shouldBeEqualTo 6
+
+            countDeclarations(
+                includeLocal = true,
+                includeNested = false
+            ) { (it as KoNameProvider).hasNameStartingWith("sampleLocal") }
+                .shouldBeEqualTo(2)
+
+            hasDeclarations(includeLocal = true, includeNested = false) shouldBeEqualTo true
+            hasDeclaration(
+                includeLocal = true,
+                includeNested = false
+            ) { (it as KoNameProvider).name == "sampleLocalProperty" }
+                .shouldBeEqualTo(true)
+
+            hasDeclaration(
+                includeLocal = true,
+                includeNested = false
+            ) { (it as KoNameProvider).name == "otherLocalProperty" }
+                .shouldBeEqualTo(false)
+
+            hasAllDeclarations(
+                includeLocal = true,
+                includeNested = false
+            ) { (it as KoNameProvider).hasNameContaining("ample") }
+                .shouldBeEqualTo(true)
+
+            hasAllDeclarations(includeLocal = true, includeNested = false) {
+                (it as KoNameProvider).hasNameStartingWith(
+                    "sample"
+                )
+            }
+                .shouldBeEqualTo(false)
+
+            declarations(includeLocal = true, includeNested = false)
+                .filterIsInstance<KoNameProvider>()
+                .map { it.name }
+                .shouldBeEqualTo(
+                    listOf(
+                        "sampleProperty",
+                        "sampleFunction",
+                        "sampleLocalProperty",
+                        "sampleLocalFunction",
+                        "SampleLocalClass",
+                        "SampleInnerClass",
+                    ),
+                )
+        }
+    }
+
+    @Test
+    fun `enum-constant-contains-nested-and-local-declarations includeNested=true`() {
+        // given
+        val sut =
+            getSnippetFile("enum-constant-contains-nested-and-local-declarations")
+                .classes()
+                .enumConstants
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            numDeclarations(includeLocal = false, includeNested = true) shouldBeEqualTo 6
+
+            countDeclarations(
+                includeLocal = false,
+                includeNested = true
+            ) { (it as KoNameProvider).hasNameStartingWith("sampleNested") }
+                .shouldBeEqualTo(2)
+
+            hasDeclarations(includeLocal = false, includeNested = true) shouldBeEqualTo true
+            hasDeclaration(
+                includeLocal = false,
+                includeNested = true
+            ) { (it as KoNameProvider).name == "sampleNestedProperty" }
+                .shouldBeEqualTo(true)
+
+            hasDeclaration(
+                includeLocal = false,
+                includeNested = true
+            ) { (it as KoNameProvider).name == "otherNestedProperty" }
+                .shouldBeEqualTo(false)
+
+            hasAllDeclarations(
+                includeLocal = false,
+                includeNested = true
+            ) { (it as KoNameProvider).hasNameContaining("ample") }
+                .shouldBeEqualTo(true)
+
+            hasAllDeclarations(includeLocal = false, includeNested = true) {
+                (it as KoNameProvider).hasNameStartingWith(
+                    "sample"
+                )
+            }
+                .shouldBeEqualTo(false)
+
+            declarations(includeLocal = false, includeNested = true)
+                .filterIsInstance<KoNameProvider>()
+                .map { it.name }
+                .shouldBeEqualTo(
+                    listOf(
+                        "sampleProperty",
+                        "sampleFunction",
+                        "SampleInnerClass",
+                        "sampleNestedProperty",
+                        "sampleNestedFunction",
+                        "SampleNestedClass",
+                    ),
+                )
+        }
+    }
+
+    @Test
+    fun `enum-constant-contains-nested-and-local-declarations includeLocal=true and includeNested=true`() {
+        // given
+        val sut =
+            getSnippetFile("enum-constant-contains-nested-and-local-declarations")
+                .classes()
+                .enumConstants
+                .first()
+
+        // then
+        assertSoftly(sut) {
+            numDeclarations(includeLocal = true, includeNested = true) shouldBeEqualTo 9
+
+            countDeclarations(
+                includeLocal = true,
+                includeNested = true
+            ) { (it as KoNameProvider).hasNameStartingWith("sample") }
+                .shouldBeEqualTo(6)
+
+            hasDeclarations(includeLocal = true, includeNested = true) shouldBeEqualTo true
+            hasDeclaration(
+                includeLocal = true,
+                includeNested = true
+            ) { (it as KoNameProvider).name == "sampleLocalProperty" }
+                .shouldBeEqualTo(true)
+
+            hasDeclaration(
+                includeLocal = true,
+                includeNested = true
+            ) { (it as KoNameProvider).name == "otherLocalProperty" }
+                .shouldBeEqualTo(false)
+
+            hasAllDeclarations(
+                includeLocal = true,
+                includeNested = true
+            ) { (it as KoNameProvider).hasNameContaining("ample") }
+                .shouldBeEqualTo(true)
+
+            hasAllDeclarations(includeLocal = true, includeNested = true) {
+                (it as KoNameProvider).hasNameStartingWith(
+                    "sample"
+                )
+            }
+                .shouldBeEqualTo(false)
+
+            declarations(includeLocal = true, includeNested = true)
+                .filterIsInstance<KoNameProvider>()
+                .map { it.name }
+                .shouldBeEqualTo(
+                    listOf(
+                        "sampleProperty",
+                        "sampleFunction",
+                        "sampleLocalProperty",
+                        "sampleLocalFunction",
+                        "SampleLocalClass",
+                        "SampleInnerClass",
+                        "sampleNestedProperty",
+                        "sampleNestedFunction",
+                        "SampleNestedClass",
+                    ),
+                )
+        }
+    }
+
 
     private fun getSnippetFile(fileName: String) =
         getSnippetKoScope("core/declaration/koenumconstant/snippet/forkodeclarationprovider/", fileName)
