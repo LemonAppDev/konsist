e9e14276e91bdb07792c61e48db83be06ea950d4
Igor Wojda <igor.wojda@gmail.com>
2025-09-22 12:23:37 +0200

    Restore java Version

--- buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
+++ buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
@@ -3,8 +3,10 @@ plugins {
 }
 
 java {
+    // Make Konsist artifact compatible with Java 1.8
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        // Java 8 == bytecode version 52.0
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
 
     // Generated sources.jar for the library jar
