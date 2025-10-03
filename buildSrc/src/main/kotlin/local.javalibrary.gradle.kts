1b3e68caefc12278ca43e36e3f6086c4e0b158ce
Igor Wojda <igor.wojda@gmail.com>
2025-10-03 08:18:49 +0200

    Upgrade Junit to 6.0.0

--- buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
+++ buildSrc/src/main/kotlin/local.javalibrary.gradle.kts
@@ -3,10 +3,9 @@ plugins {
 }
 
 java {
-    // Make Konsist artifact compatible with Java 11
     toolchain {
-        // Java 11 == bytecode version 55.0
-        languageVersion.set(JavaLanguageVersion.of(11))
+        // Java 17 == bytecode version 61.0
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 
     // Generated sources.jar for the library jar
